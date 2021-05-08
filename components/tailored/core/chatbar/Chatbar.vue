80a6b8f89580c1074b43605d5d85dab53930c923
RetroPronghorn <retropronghorn@gmail.com>
2021-05-07 23:52:59 -0400

    feat(Chatbar): Bootstrapped chatbar

---
+++ components/tailored/core/chatbar/Chatbar.vue
@@ -0,0 +1,20 @@
+<template src="./Chatbar.html"></template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      text: '',
+      maxChars: 256,
+    }
+  },
+  computed: {
+    charlimit() {
+      return this.$data.text.length > this.$data.maxChars
+    },
+  },
+})
+</script>
+
+<style scoped lang="less" src="./Chatbar.less"></style>
