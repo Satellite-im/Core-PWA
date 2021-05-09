214c365200a50436f0b8e9b64a4cfa6c86bc06b6
RetroPronghorn <retropronghorn@gmail.com>
2021-05-09 15:08:48 -0400

    feat(Conversation): Bootstrapped basic conversation

---
+++ components/typography/Text/Text.vue
@@ -0,0 +1,17 @@
+<template src="./Text.html"></template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    /**
+     * Text to be included
+     */
+    text: {
+      type: String,
+      default: 'Missing prop `text`.',
+    },
+  },
+})
+</script>
+<style scoped lang="less" src="./Text.less"></style>
