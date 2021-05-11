ea060dfc0ad9294823cce1badda7be1b4cb28298
Kathy Chan <kathykemopaw@gmail.com>
2021-05-10 21:47:16 -0700

    add comments

--- components/mixins/Swipe.js
+++ components/mixins/Swipe.js
@@ -3,6 +3,7 @@ export const mobileSwipe = {
     // can add functions here on start-up if needed
   },
   methods: {
+    // swipe handler for settings
     settingsSwipeHandler(direction) {
       if (this.$route.name?.includes('settings') && this.$device.isMobile) {
         if (direction === 'left') {
