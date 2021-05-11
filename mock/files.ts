66bad57590a050292ebcae01c447f1fd7ab389ce
RetroPronghorn <retropronghorn@gmail.com>
2021-05-11 16:54:32 -0700

    update

---
+++ mock/files.ts
@@ -0,0 +1,113 @@
+export const files = {
+  name: 'Root',
+  children: [
+    {
+      name: 'Favorites',
+      children: [
+        {
+          name: 'Some File',
+          type: 'image/png',
+          size: 8203451,
+          location: 'https://ifps.io/ipfs/12jfa9sdf01234',
+        },
+        {
+          name: 'Some Zip',
+          type: 'archive/zip',
+          size: 8201233451,
+          location: 'https://ifps.io/ipfs/12jfffsa9sdf01234',
+        },
+      ],
+    },
+    {
+      name: 'Assets',
+      children: [
+        {
+          name: 'Typography',
+          children: [
+            {
+              name: 'Satellite',
+              children: [
+                {
+                  name: 'Some File',
+                  type: 'image/png',
+                  size: 8203451,
+                  location: 'https://ifps.io/ipfs/12jfa9sdf01234',
+                },
+                {
+                  name: 'Some Zip',
+                  type: 'archive/zip',
+                  size: 8201233451,
+                  location: 'https://ifps.io/ipfs/12jfffsa9sdf01234',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          name: 'Satellite',
+          children: [
+            {
+              name: 'Some File',
+              type: 'image/png',
+              size: 8203451,
+              location: 'https://ifps.io/ipfs/12jfa9sdf01234',
+            },
+            {
+              name: 'Some Zip',
+              type: 'archive/zip',
+              size: 8201233451,
+              location: 'https://ifps.io/ipfs/12jfffsa9sdf01234',
+            },
+          ],
+        },
+        {
+          name: 'Gaming',
+          children: [
+            {
+              name: 'Some File',
+              type: 'image/png',
+              size: 8203451,
+              location: 'https://ifps.io/ipfs/12jfa9sdf01234',
+            },
+          ],
+        },
+        {
+          name: 'Some File',
+          type: 'image/png',
+          size: 8203451,
+          location: 'https://ifps.io/ipfs/12jfa9sdf01234',
+        },
+        {
+          name: 'Some Zip',
+          type: 'archive/zip',
+          size: 8201233451,
+          location: 'https://ifps.io/ipfs/12jfffsa9sdf01234',
+        },
+        {
+          name: 'Some File',
+          type: 'image/png',
+          size: 8203451,
+          location: 'https://ifps.io/ipfs/12jfa9sdf01234',
+        },
+        {
+          name: 'Some File',
+          type: 'image/png',
+          size: 8203451,
+          location: 'https://ifps.io/ipfs/12jfa9sdf01234',
+        },
+        {
+          name: 'Some File',
+          type: 'image/png',
+          size: 8203451,
+          location: 'https://ifps.io/ipfs/12jfa9sdf01234',
+        },
+        {
+          name: 'Some File',
+          type: 'image/png',
+          size: 8203451,
+          location: 'https://ifps.io/ipfs/12jfa9sdf01234',
+        },
+      ],
+    },
+  ],
+}
