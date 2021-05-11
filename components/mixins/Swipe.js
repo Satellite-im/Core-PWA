c406a548c7f5a92899ff1003236fbc89d94f6257
Kathy Chan <kathykemopaw@gmail.com>
2021-05-10 21:46:20 -0700

    fix format for files

--- components/mixins/Swipe.js
+++ components/mixins/Swipe.js
@@ -1,18 +1,17 @@
 export const mobileSwipe = {
-    created: function () {
-    //can add functions here on start-up if needed
+  created: () => {
+    // can add functions here on start-up if needed
+  },
+  methods: {
+    settingsSwipeHandler(direction) {
+      if (this.$route.name?.includes('settings') && this.$device.isMobile) {
+        if (direction === 'left') {
+          this.sidebar = false
+        }
+        if (direction === 'right') {
+          this.sidebar = true
+        }
+      }
     },
-    methods: {
-        settingsSwipeHandler (direction) {
-            if (this.$route.name?.includes('settings') && this.$device.isMobile) {
-
-            if (direction === 'left') {
-                this.sidebar = false
-            }
-            if (direction === 'right') {
-                this.sidebar = true
-            }
-            }
-        }  
-    }
+  },
 }
