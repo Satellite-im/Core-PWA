81e270f90c3fab89b12ffbb4ae27c8e2f7132e6e
Kathy Chan <kathykemopaw@gmail.com>
2021-05-12 17:08:27 -0700

    add README

--- components/mixins/Swipe/Swipe.ts
+++ components/mixins/Swipe/Swipe.ts
@@ -6,13 +6,13 @@ export const mobileSwipe = {
       settingsSwipeHandler (currThis: any) {
         return function (direction: any) {
           if (direction === 'left') {
-                  currThis.sidebar = false
-                }
-                if (direction === 'right') {
-                  currThis.sidebar = true
-                }
+            currThis.sidebar = false
+          }
+          if (direction === 'right') {
+            currThis.sidebar = true
+          }
         }
+      }
     }
-  }
 }
   
