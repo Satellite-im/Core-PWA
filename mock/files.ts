dac844c5fc63c6135f7ed9003ff9e9d220e6fb52
Retro <retro@Retros-MacBook-Pro.local>
2021-05-12 22:16:51 -0700

    feat(Files): Started bootstrapping files page

--- mock/files.ts
+++ mock/files.ts
@@ -1,17 +1,41 @@
-export const files = {
-  name: 'Root',
+export const Files = {
+  name: 'root',
+  modified: 1620871715645,
   children: [
     {
-      name: 'Favorites',
+      name: 'Uploads',
+      modified: 1620871715645,
       children: [
         {
           name: 'Some File',
+          modified: 1620871715645,
           type: 'image/png',
           size: 8203451,
           location: 'https://ifps.io/ipfs/12jfa9sdf01234',
         },
         {
           name: 'Some Zip',
+          modified: 1620871715645,
+          type: 'archive/zip',
+          size: 8201233451,
+          location: 'https://ifps.io/ipfs/12jfffsa9sdf01234',
+        },
+      ],
+    },
+    {
+      name: 'Favorites',
+      modified: 1620871715645,
+      children: [
+        {
+          name: 'Some File',
+          modified: 1620871715645,
+          type: 'image/png',
+          size: 8203451,
+          location: 'https://ifps.io/ipfs/12jfa9sdf01234',
+        },
+        {
+          name: 'Some Zip',
+          modified: 1620871715645,
           type: 'archive/zip',
           size: 8201233451,
           location: 'https://ifps.io/ipfs/12jfffsa9sdf01234',
@@ -20,21 +44,26 @@ export const files = {
     },
     {
       name: 'Assets',
+      modified: 1620871715645,
       children: [
         {
           name: 'Typography',
+          modified: 1620871715645,
           children: [
             {
               name: 'Satellite',
+              modified: 1620871715645,
               children: [
                 {
-                  name: 'Some File',
+                  name: 'Some File as',
+                  modified: 1620871715645,
                   type: 'image/png',
                   size: 8203451,
                   location: 'https://ifps.io/ipfs/12jfa9sdf01234',
                 },
                 {
-                  name: 'Some Zip',
+                  name: 'Some Zip ff',
+                  modified: 1620871715645,
                   type: 'archive/zip',
                   size: 8201233451,
                   location: 'https://ifps.io/ipfs/12jfffsa9sdf01234',
@@ -45,15 +74,18 @@ export const files = {
         },
         {
           name: 'Satellite',
+          modified: 1620871715645,
           children: [
             {
-              name: 'Some File',
+              name: 'Some File 9',
+              modified: 1620871715645,
               type: 'image/png',
               size: 8203451,
               location: 'https://ifps.io/ipfs/12jfa9sdf01234',
             },
             {
-              name: 'Some Zip',
+              name: 'Some Zip f',
+              modified: 1620871715645,
               type: 'archive/zip',
               size: 8201233451,
               location: 'https://ifps.io/ipfs/12jfffsa9sdf01234',
@@ -62,9 +94,11 @@ export const files = {
         },
         {
           name: 'Gaming',
+          modified: 1620871715645,
           children: [
             {
-              name: 'Some File',
+              name: 'Some File 1',
+              modified: 1620871715645,
               type: 'image/png',
               size: 8203451,
               location: 'https://ifps.io/ipfs/12jfa9sdf01234',
@@ -72,42 +106,62 @@ export const files = {
           ],
         },
         {
-          name: 'Some File',
+          name: 'Some File 0',
+          modified: 1620871715645,
           type: 'image/png',
           size: 8203451,
           location: 'https://ifps.io/ipfs/12jfa9sdf01234',
         },
         {
-          name: 'Some Zip',
+          name: 'Some Zip 1',
+          modified: 1620871715645,
           type: 'archive/zip',
           size: 8201233451,
           location: 'https://ifps.io/ipfs/12jfffsa9sdf01234',
         },
         {
-          name: 'Some File',
+          name: 'Some File 1',
+          modified: 1620871715645,
           type: 'image/png',
           size: 8203451,
           location: 'https://ifps.io/ipfs/12jfa9sdf01234',
         },
         {
-          name: 'Some File',
+          name: 'Some File 2',
+          modified: 1620871715645,
           type: 'image/png',
           size: 8203451,
           location: 'https://ifps.io/ipfs/12jfa9sdf01234',
         },
         {
-          name: 'Some File',
+          name: 'Some File 3',
+          modified: 1620871715645,
           type: 'image/png',
           size: 8203451,
           location: 'https://ifps.io/ipfs/12jfa9sdf01234',
         },
         {
-          name: 'Some File',
+          name: 'Some File 4',
+          modified: 1620871715645,
           type: 'image/png',
           size: 8203451,
           location: 'https://ifps.io/ipfs/12jfa9sdf01234',
         },
       ],
+    },
+    {
+      name: 'Some File',
+      modified: 1620871715645,
+      type: 'image/png',
+      size: 8203451,
+      location: 'https://ifps.io/ipfs/12jfa9sdf01234',
+    },
+    {
+      name: 'Some Zip',
+      modified: 1620871715645,
+      type: 'archive/zip',
+      size: 8201233451,
+      location: 'https://ifps.io/ipfs/12jfffsa9sdf01234',
     },
   ],
 }
