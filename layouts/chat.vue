f90a7e7dc38e4f9fc079adab9f19b0d14c8d2b32
Kathy Chan <kathykemopaw@gmail.com>
2021-05-17 19:21:57 -0700

    add mobile swipe option for chat

--- layouts/chat.vue
+++ layouts/chat.vue
@@ -2,8 +2,11 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import { mobileSwipe } from '../components/mixins/Swipe/Swipe'
+
 
 export default Vue.extend({
+  mixins: [mobileSwipe],
   name: 'Chat',
   middleware: 'authenticated',
   data() {
