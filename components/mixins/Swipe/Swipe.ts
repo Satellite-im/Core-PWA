29969def7c89e854a530bfea228f1c086dbdbe6e
Jason Panay <jason@Jasons-MacBook-Pro.local>
2021-05-19 16:43:13 -0400

    Swipe.ts typo fix

--- components/mixins/Swipe/Swipe.ts
+++ components/mixins/Swipe/Swipe.ts
@@ -1,18 +1,18 @@
 export const mobileSwipe = {
-    created: () => {
-      // can add functions here on start-up if needed
-    },
-    methods: {
-      sidebarSwipeHandler (currThis: any) {
-        return function (direction: any) {
-          if (direction === 'left') {
-            currThis.sidebar = false
-          }
-          if (direction === 'right') {
-            currThis.sidebar = true
-          }
+  created: () => {
+    // can add functions here on start-up if needed
+  },
+  methods: {
+    sidebarSwipeHandler (currThis: any) {
+      return function (direction: any) {
+        if (direction === 'left') {
+          currThis.sidebar = false
+        }
+        if (direction === 'right') {
+          currThis.sidebar = true
         }
       }
-    },
+    }
   },
 }
+
