c6d119ea47d123af7026a3f5219446afe7f985cc
Jason Panay <jason@Jasons-MacBook-Pro.local>
2021-05-26 02:04:48 -0400

    Emoji picker added (emoji-mart-vue lib)

--- components/tailored/core/chatbar/Chatbar.vue
+++ components/tailored/core/chatbar/Chatbar.vue
@@ -5,14 +5,26 @@ import Vue from 'vue'
 
 import FileUpload from './fileupload/FileUpload.vue'
 
+// @ts-ignore
+import data from 'emoji-mart-vue-fast/data/all.json'
+import 'emoji-mart-vue-fast/css/emoji-mart.css'
+// @ts-ignore
+import { Picker, EmojiIndex } from 'emoji-mart-vue-fast'
+let emojiIndex = new EmojiIndex(data)
+
+
 export default Vue.extend({
   components: {
     FileUpload,
+    Picker,
   },
   data() {
     return {
       text: '',
       maxChars: 256,
+      emojiIndex: emojiIndex,
+      emojiPos: { x: 0, y: 0 },
+      showEmojiPicker: false,
     }
   },
   computed: {
@@ -23,6 +35,33 @@ export default Vue.extend({
       return this.$data.text.length > this.$data.maxChars
     },
   },
+  methods: {
+    toggleEmojiPicker(e: any) {
+      this.$data.showEmojiPicker = !this.$data.showEmojiPicker
+      if (!this.$data.showEmojiPicker) {
+        let div = this.$refs.emojiPicker
+        // @ts-ignore
+        div.$el.style.opacity = 0
+      } else {
+        let divChecker = setInterval(() => {
+          let div = this.$refs.emojiPicker
+          if (div) {
+            // @ts-ignore
+            let divProps = div.$el.getBoundingClientRect()
+            this.$data.emojiPos.y = e.y - divProps.height - 24
+            this.$data.emojiPos.x = e.x - divProps.width + 36
+            // @ts-ignore
+             div.$el.style.opacity = 1
+            clearInterval(divChecker)
+          }
+        }, 10)
+      }
+    },
+    addEmoji(emoji: any) {
+      this.$data.text += emoji.native
+      this.$data.showEmojiPicker = false
+    }
+  }
 })
 </script>
 
