05186070fdde1e616cb585f229c86e0c906db959
RetroPronghorn <retropronghorn@gmail.com>
2021-05-28 19:03:50 -0400

    feat(Loader): Added logo Loader

---
+++ components/ui/Loaders/LogoLoader/LogoLoader.vue
@@ -0,0 +1,14 @@
+<template src="./LogoLoader.html"></template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    size: {
+      type: Number,
+      default: 64,
+    },
+  },
+})
+</script>
+<style scoped lang="less" src="./LogoLoader.less"></style>
--- store/ui/state.ts
+++ store/ui/state.ts
@@ -1,13 +1,17 @@
 interface UIState {
   contextMenuStatus: boolean
   contextMenuValues: Array<object>
+  quickProfile: Object | Boolean
   contextMenuPosition: Object
+  quickProfilePosition: Object
 }
 
 const InitalUIState: UIState = {
   contextMenuStatus: false,
+  quickProfile: false,
   contextMenuValues: [],
-  contextMenuPosition: { x: 0, y: 0 }
+  contextMenuPosition: { x: 0, y: 0 },
+  quickProfilePosition: { x: 0, y: 0 },
 }
 
 export default InitalUIState
