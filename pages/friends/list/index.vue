de1adf539d906208803279e734ebade48479662a
RetroPronghorn <retropronghorn@gmail.com>
2021-05-31 22:56:17 -0400

    feat(UI Tweaks): Tweaked UI a bit for Mobile

---
+++ pages/friends/list/index.vue
@@ -0,0 +1,12 @@
+<template src="./FriendsList.html"></template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'FriendsList',
+  layout: 'friends',
+})
+</script>
+
+<style scoped lang="less" src="./FriendsList.less"></style>
