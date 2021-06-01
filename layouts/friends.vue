e3ceea36abe012cd9101c55ab430eefc81369a52
RetroPronghorn <retropronghorn@gmail.com>
2021-05-31 22:56:17 -0400

    feat(UI Tweaks): Tweaked UI a bit for Mobile

---
+++ layouts/friends.vue
@@ -0,0 +1,33 @@
+<template src="./friends/Friends.html"></template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mobileSwipe } from '../components/mixins/Swipe/Swipe'
+
+export default Vue.extend({
+  name: 'ChatLayout',
+  mixins: [mobileSwipe],
+  middleware: 'authenticated',
+  data() {
+    return {
+      sidebar: true,
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.scrollToBottom()
+    }, 400)
+  },
+  updated() {
+    this.scrollToBottom()
+  },
+  methods: {
+    scrollToBottom() {
+      const container = (this.$refs.chat as Vue).$el as Element
+      container.scrollTop = container.scrollHeight
+    },
+  },
+})
+</script>
+
+<style lang="less" src="./friends/Friends.less"></style>
