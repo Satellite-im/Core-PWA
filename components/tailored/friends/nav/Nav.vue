f87218bcf19b4d0e3808342b51812a7c0c352575
RetroPronghorn <retropronghorn@gmail.com>
2021-06-07 22:13:54 -0400

    feat(Friends): Added friends UI

---
+++ components/tailored/friends/nav/Nav.vue
@@ -0,0 +1,18 @@
+<template src="./Nav.html"></template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    setRoute: {
+      type: Function,
+      default: () => () => {}
+    },
+    route: {
+      type: String,
+      default: 'active',
+    },
+  }
+})
+</script>
+<style scoped lang="less" src="./Nav.less"></style>
--- pages/friends/list/index.vue
+++ pages/friends/list/index.vue
@@ -3,9 +3,28 @@
 <script lang="ts">
 import Vue from 'vue'
 
+import { getAlphaSorted } from '~/utilities/friends'
+
+type Route = 'active' | 'requests' | 'blocked' | 'add'
+
 export default Vue.extend({
   name: 'FriendsList',
   layout: 'friends',
+  computed: {
+    alphaSortedFriends() {
+      return getAlphaSorted(this.$mock.friends)
+    },
+  },
+  data() {
+    return {
+      route: 'active',
+    }
+  },
+  methods: {
+    setRoute(route: Route) {
+      this.$data.route = route
+    }
+  }
 })
 </script>
 
