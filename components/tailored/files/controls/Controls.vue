1f444a64fabb5c19a19c07e14db4cbb478c6fba9
Kathy Chan <kathykemopaw@gmail.com>
2021-06-09 17:28:32 -0700

    add npmjs vue-js-modal

--- components/tailored/files/controls/Controls.vue
+++ components/tailored/files/controls/Controls.vue
@@ -1,6 +1,7 @@
 <template src="./Controls.html"></template>
 <script lang="ts">
 import Vue from 'vue'
+import vmodal from 'vue-js-modal'
 
 export default Vue.extend({
   props: {
@@ -11,16 +12,29 @@ export default Vue.extend({
   },
   data() {
     return {
-      showModal: false,
+      // showModal: false,
     }
+  },
+    mounted() {
+    // Updates time ago
+    console.log("mounted")
   },
   methods: {
     testAction() {
       console.log("meow")
-      console.log(this.showModal)
-      this.showModal = !this.showModal
+      // console.log(this.showModal)
+      // this.showModal = !this.showModal
+      this.show()
+
     },
+    show () {
+      this.$modal.show('my-first-modal');
+      },
+    hide () {
+        this.$modal.hide('my-first-modal');
+      }
   }
+
 })
 </script>
 <style scoped lang="less" src="./Controls.less"></style>
