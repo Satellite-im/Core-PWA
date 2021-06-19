fbbc3b761e7a1586d5178059c06ed1b955f16b73
RetroPronghorn <retropronghorn@gmail.com>
2021-06-18 23:25:30 -0400

    feat(Media): volume slider

---
+++ components/tailored/core/media/actions/volume/Volume.vue
@@ -0,0 +1,19 @@
+<template src="./Volume.html"></template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      showSlider: false,
+    }
+  },
+  methods: {
+    toggleSlider() {
+      this.$data.showSlider = !this.$data.showSlider
+    },
+  },
+})
+</script>
+
+<style scoped lang="less" src="./Volume.less"></style>
