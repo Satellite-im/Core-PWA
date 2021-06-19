08fa0354687610aff50ccc717345a07e35aa8c62
RetroPronghorn <retropronghorn@gmail.com>
2021-06-18 23:25:30 -0400

    feat(Media): volume slider

---
+++ components/tailored/core/media/actions/settings/Settings.vue
@@ -0,0 +1,8 @@
+<template src="./Settings.html"></template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style scoped lang="less" src="./Settings.less"></style>
---
+++ components/tailored/core/media/volume/Volume.vue
@@ -0,0 +1,8 @@
+<template src="./Volume.html"></template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+<style scoped lang="less" src="./Volume.less"></style>
