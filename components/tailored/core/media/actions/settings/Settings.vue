5b9868f0e9be57072b872539aef1352acb9a6e4d
RetroPronghorn <retropronghorn@gmail.com>
2021-06-21 14:43:46 -0400

    fix(Mobile): Fixes a lot of mobile UI bugs

--- components/tailored/core/media/actions/settings/Settings.vue
+++ components/tailored/core/media/actions/settings/Settings.vue
@@ -2,7 +2,18 @@
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  data() {
+    return {
+      menuOpen: false,
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.$data.menuOpen = !this.$data.menuOpen
+    },
+  },
+})
 </script>
 
 <style scoped lang="less" src="./Settings.less"></style>
