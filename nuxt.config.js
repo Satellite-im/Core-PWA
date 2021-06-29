13bdd07ae67c82ce72437cee2fc3e95169bac753
Chris Hogan <chris@hogan.io>
2021-06-29 14:15:08 -0600

    Set meta viewport to have minimum and maximum scale, this prevents mobile devices from auto zooming in on text inputs, throwing off the ui. Manual zooming not affected

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
