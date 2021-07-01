4f08b51191b6d33e32061c7402e15c85d48eabb8
pixwebie <nickjang234@gmail.com>
2021-07-01 13:48:01 -0400

    SA-28 completed

--- components/tailored/core/media/heading/Heading.vue
+++ components/tailored/core/media/heading/Heading.vue
@@ -2,7 +2,34 @@
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+import { mapState } from 'vuex'
+export default Vue.extend({
+  computed: {
+    ...mapState(['ui']),
+  },
+  mounted() {
+    this.handleFullscreen()
+    document.addEventListener('fullscreenchange', this.handleFullscreen)
+  },
+  destroyed() {
+    document.removeEventListener('fullscreenchange', this.handleFullscreen)
+  },
+  methods: {
+    toggleFullscreen() {
+      if (document.fullscreen) {
+        document.exitFullscreen()
+      } else {
+        const dynamicContent = document.querySelector('.dynamic-content')
+        if (dynamicContent) {
+          dynamicContent.requestFullscreen()
+        }
+      }
+    },
+    handleFullscreen() {
+      this.$store.commit('fullscreen', !!document.fullscreenElement)
+    },
+  },
+})
 </script>
 
 <style scoped lang="less" src="./Heading.less"></style>
