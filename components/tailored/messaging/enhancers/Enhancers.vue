bee6d005a6db7602fc99c271e24b6e77d1a9c252
RetroPronghorn <retropronghorn@gmail.com>
2021-07-02 23:41:55 -0400

    chore(Enchancers): Started work on chat enhancers sections

---
+++ components/tailored/messaging/enhancers/Enhancers.vue
@@ -0,0 +1,6 @@
+<template src="./Enhancers.html"></template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+<style scoped lang="less" src="./Enhancers.less"></style>
