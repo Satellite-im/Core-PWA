4e5bf74b573b60683521a22f11078a4a6bf8aa90
pixwebie <nickjang234@gmail.com>
2021-07-05 18:19:15 -0400

    SA-21 Create loading components added on every page

--- store/files/actions.ts
+++ store/files/actions.ts
@@ -4,6 +4,7 @@ export default {
   handler: () => {},
   // @ts-ignore
   async fetchFiles({ commit }) {
+    commit('loading', 'files')
     await new Promise((resolve) => setTimeout(resolve, 3000))
     commit('fetchFiles', Files)
     commit('load', 'files')
