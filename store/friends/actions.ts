3ffcdc02a893799722619f3fa9b0c122aa4fdebc
nickjjang <nickjang234@gmail.com>
2021-07-09 02:40:34 -0400

    SA34 fixed friends reloading every refresh

--- store/friends/actions.ts
+++ store/friends/actions.ts
@@ -3,13 +3,16 @@ import { Friends } from '~/mock/friends'
 
 interface FetchFriendsArguments {
   commit: Commit
+  state: any
 }
 export default {
   handler: () => {},
-  async fetchFriends({ commit }: FetchFriendsArguments) {
-    commit('loading', 'friends')
-    await new Promise((resolve) => setTimeout(resolve, 3000))
-    commit('fetchFriends', Friends)
-    commit('load', 'friends')
+  async fetchFriends({ commit, state }: FetchFriendsArguments) {
+    // commit('loading', 'friends')
+    if (!state.loading.friends) {
+      await new Promise((resolve) => setTimeout(resolve, 3000))
+      commit('fetchFriends', Friends)
+      commit('load', 'friends')
+    }
   },
 }
