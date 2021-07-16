a3010a65811963096b720e3817a4bb8461b20342
mtumiati <tumiatimanuel@gmail.com>
2021-07-16 09:53:56 +0200

    chore(SolanaManager): injected SolanaManager into vue prototype

--- plugins/local/classLoader.ts
+++ plugins/local/classLoader.ts
@@ -2,17 +2,21 @@
 
 import Vue from 'vue'
 import WebRTC from '~/utilities/WebRTC/WebRTC'
+import SolanaManager from '~/utilities/SolanaManager/SolanaManager'
 
 declare module 'vue/types/vue' {
   interface Vue {
     $WebRTC: typeof WebRTC
+    $SolanaManager: typeof SolanaManager
   }
 }
 
 declare module '@nuxt/types' {
   interface Context {
     $WebRTC: typeof WebRTC
+    $SolanaManager: typeof SolanaManager
   }
 }
 
 Vue.prototype.$WebRTC = new WebRTC()
+Vue.prototype.$SolanaManager = new SolanaManager()
