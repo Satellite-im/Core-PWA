d90b525aeda9c2a60e2494d8333ffd1bd8cef6e2
Kathy Chan <kathykemopaw@gmail.com>
2021-07-21 11:23:13 -0700

    code cleanup

--- pages/server/chat/index.vue
+++ pages/server/chat/index.vue
@@ -6,5 +6,16 @@ import Vue from 'vue'
 export default Vue.extend({
   name: 'ServerChat',
   layout: 'server',
+  data() {
+    return {
+      loading: true,
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.$data.loading = false
+    }, 3000)
+    this.$store.dispatch('fetchFriends')
+  },
 })
 </script>
