3b48e7f93922ed49f255d4a9585dcdb19e5fb723
mtumiati <tumiatimanuel@gmail.com>
2021-07-22 15:11:15 +0200

    feat(crypto): crypto class adapted for Solana ed25519 keypair

---
+++ types/crypto/crypto.ts
@@ -0,0 +1,13 @@
+export type HashableData =
+  | string
+  | Uint8Array
+  | ArrayBuffer
+  | Int8Array
+  | Int16Array
+  | Int32Array
+  | Uint16Array
+  | Uint32Array
+  | Uint8ClampedArray
+  | Float32Array
+  | Float64Array
+  | DataView
