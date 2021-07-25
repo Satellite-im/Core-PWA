33092c0b0b614e8b032a6877d9175245a2392d83
RetroPronghorn <retropronghorn@gmail.com>
2021-07-25 01:30:44 -0400

    fix(Profile): Fix profile popup for mobile

--- components/interactables/QuickProfile/QuickProfile.vue
+++ components/interactables/QuickProfile/QuickProfile.vue
@@ -18,21 +18,29 @@ export default Vue.extend({
       this.$store.commit('quickProfile', false)
     },
     handleOverflow() {
-      const quickProfile = this.$refs.quickProfile as HTMLElement
-      const position = this.$store.state.ui.quickProfilePosition
-      let clickX = position.x
-      let clickY = position.y
-      const widthOverflow =
-        clickX + quickProfile.clientWidth - window.innerWidth
-      const heightOverflow =
-        clickY + quickProfile.clientHeight - window.innerHeight
-      if (widthOverflow > -8) {
-        clickX -= quickProfile.clientWidth
-        this.$store.commit('setQuickProfilePosition', { x: clickX, y: clickY })
-      }
-      if (heightOverflow > -8) {
-        clickY -= heightOverflow + 12
-        this.$store.commit('setQuickProfilePosition', { x: clickX, y: clickY })
+      if (this.$device.isDesktop) {
+        const quickProfile = this.$refs.quickProfile as HTMLElement
+        const position = this.$store.state.ui.quickProfilePosition
+        let clickX = position.x
+        let clickY = position.y
+        const widthOverflow =
+          clickX + quickProfile.clientWidth - window.innerWidth
+        const heightOverflow =
+          clickY + quickProfile.clientHeight - window.innerHeight
+        if (widthOverflow > -8) {
+          clickX -= quickProfile.clientWidth
+          this.$store.commit('setQuickProfilePosition', {
+            x: clickX,
+            y: clickY,
+          })
+        }
+        if (heightOverflow > -8) {
+          clickY -= heightOverflow + 12
+          this.$store.commit('setQuickProfilePosition', {
+            x: clickX,
+            y: clickY,
+          })
+        }
       }
     },
   },
