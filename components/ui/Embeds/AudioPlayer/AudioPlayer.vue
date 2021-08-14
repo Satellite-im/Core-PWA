a89f2ae9ae057896f5ce167ec7f35e7da29d3b10
RetroPronghorn <retropronghorn@gmail.com>
2021-08-13 22:21:56 -0400

    chore(Tweaks): Added subtext to media embeds

--- components/ui/Embeds/AudioPlayer/AudioPlayer.vue
+++ components/ui/Embeds/AudioPlayer/AudioPlayer.vue
@@ -14,3 +14,15 @@ export default Vue.extend({
 })
 </script>
 <style lang="less" src="./AudioPlayer.less"></style>
+<style scoped lang="less">
+.is-text {
+  margin-left: -1rem;
+  font-size: @micro-text-size;
+  padding: 0;
+  line-height: 0.8;
+  font-family: @primary-font;
+  color: @text-muted;
+  display: inline-block;
+  text-align: left;
+}
+</style>
