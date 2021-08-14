ec1ef985168123c0acfdbdd7059e5e5b83ed55ce
RetroPronghorn <retropronghorn@gmail.com>
2021-08-13 21:28:43 -0400

    feat(Embeds): Added Audio & Video Embeds

---
+++ components/ui/Embeds/VideoPlayer/VideoPlayer.vue
@@ -0,0 +1,16 @@
+<template src="./VideoPlayer.html"></template>
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+
+import { VideoData } from './types'
+
+export default Vue.extend({
+  props: {
+    data: {
+      type: Object as PropType<VideoData> | undefined,
+      default: () => {},
+    },
+  },
+})
+</script>
+<style lang="less" src="./VideoPlayer.less"></style>
