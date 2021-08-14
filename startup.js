4ce226f71e19a6bd0dac550d3c3d7ab9a7c6b336
RetroPronghorn <retropronghorn@gmail.com>
2021-08-13 22:21:56 -0400

    chore(Tweaks): Added subtext to media embeds

---
+++ startup.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk')
+console.log(' ')
+console.log(chalk.bold(chalk.blue('Satellite.im - One')))
+console.log(
+  chalk.yellow(
+    'Prerelease Software, deploying your own instance of this software may not be ideal.'
+  )
+)
