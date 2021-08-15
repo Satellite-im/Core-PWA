725a69aa6c5e40fd7c2b4a6b883dbd71b3b5693c
RetroPronghorn <retropronghorn@gmail.com>
2021-08-15 18:03:35 -0400

    rollback

--- startup.js
+++ startup.js
@@ -3,6 +3,6 @@ console.log(' ')
 console.log(chalk.bold(chalk.blue('Satellite.im - One')))
 console.log(
   chalk.yellow(
-    'Prerelease Software, deploying your own instance of this software may not be ideal.'
+    'Pre-release Software, deploying your own instance of this software for production is not recomended.'
   )
 )
