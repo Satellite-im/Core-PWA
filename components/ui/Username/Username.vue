b45e4b25b85666e159f1bb4ae7a8ecb07c3ddc1f
Ivan Dossi <ivan.dossi@knobs.it>
2021-08-25 18:38:54 +0200

    feat(chat): Introducing user message badges

---
+++ components/ui/Username/Username.vue
@@ -0,0 +1,31 @@
+<template src="./Username.html"></template>
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import badges from '~/config/badges'
+
+import { Badge } from '@/types/ui/core'
+
+export default Vue.extend({
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+    badge: {
+      type: String,
+      required: false,
+    },
+  },
+  data() {
+    return {
+      badgeIcon: this.badge
+        ? {
+            style: badges[this.badge]?.[0],
+            name: badges[this.badge]?.[1],
+          }
+        : undefined,
+    }
+  },
+})
+</script>
+<style scoped lang="less" src="./Username.less"></style>
