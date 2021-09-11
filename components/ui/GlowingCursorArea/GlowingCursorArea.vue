4f41c4332b5378b386fda0ceb9b915c06660a31f
IvanM3x <87006044+IvanM3x@users.noreply.github.com>
2021-09-11 04:35:12 +0200

    chore(UI): applied  the glowing effect to the buttons (#128)
    * chore(UI): applied  the glowing effect to the buttons
    
    * chore(UI): effect disabled automatically on buttons that have the tooltip

---
+++ components/ui/GlowingCursorArea/GlowingCursorArea.vue
@@ -0,0 +1,33 @@
+<template src="./GlowingCursorArea.html"></template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    hide: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+  },
+  data() {
+    return { xChild: 0, yChild: 0, hideCursor: true }
+  },
+  computed: {
+    cursorPoint(): string {
+      return `transform: translateX(${this.xChild - 25}px) translateY(${
+        this.yChild - 25
+      }px) translateZ(0) translate3d(0, 0, 0);`
+    },
+  },
+  methods: {
+    moveCursor(e: Event | any) {
+      let bounds = e.target.getBoundingClientRect()
+
+      this.xChild = e.clientX - bounds.left
+      this.yChild = e.clientY - bounds.top
+    },
+  },
+})
+</script>
+<style scoped lang="less" src="./GlowingCursorArea.less"></style>
