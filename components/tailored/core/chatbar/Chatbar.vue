102ed793cd299c2f19c0eb70e6289768acc1cd10
RetroPronghorn <retropronghorn@gmail.com>
2021-09-11 01:06:19 -0400

    fix(Chatbar): Cleaned up chatbar usage enabling soft returns and enter to send.

--- components/tailored/core/chatbar/Chatbar.vue
+++ components/tailored/core/chatbar/Chatbar.vue
@@ -103,6 +103,12 @@ export default Vue.extend({
             }
           }
           break
+        case 'Enter':
+          if (!event.shiftKey) {
+            event.preventDefault()
+            this.sendMessage()
+          }
+          break
       }
       return true
     },
@@ -113,6 +119,9 @@ export default Vue.extend({
         isOwner: true,
       })
       this.value = ''
+      this.$nextTick(() => {
+        this.autoGrow()
+      })
     },
   },
 })
