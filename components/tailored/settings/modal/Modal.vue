d8e48234a39f1744822db9c847b69fd14769642a
RetroPronghorn <retropronghorn@gmail.com>
2021-09-13 00:26:59 -0400

    fix(Settings Modal): Fixed settings modal toggle looking weird

--- components/tailored/settings/modal/Modal.vue
+++ components/tailored/settings/modal/Modal.vue
@@ -36,3 +36,11 @@ export default Vue.extend({
 </script>
 
 <style scoped lang="less" src="./Modal.less"></style>
+<style lang="less">
+#settings.is-collapsed > div.sidebar.is-secondary-background > section > div.aside-menu.hidden-scroll > svg {
+  right: 0.9rem !important;
+}
+#settings.is-collapsed > div.sidebar.is-secondary-background > section > div.aside-menu.hidden-scroll > aside {
+  display: none;
+}
+</style>
