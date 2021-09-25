0b33775977774519bad17441610ae84f09ec4d58
Eric Lee <eric.lee6174@gmail.com>
2021-09-24 21:11:52 -0600

    feat(glyphs popup): add glyph item

---
+++ components/tailored/messaging/enhancers/glyphs/item/Item.vue
@@ -0,0 +1,33 @@
+<template src="./Item.html"></template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    width: {
+      type: Number,
+      default: 28,
+      required: false,
+    },
+    height: {
+      type: Number,
+      default: 28,
+      required: false,
+    },
+    src: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    getStyle() {
+      return {
+        'background-image': `url(${this.src})`,
+        width: `${this.width}px`,
+        height: `${this.height}px`,
+      }
+    },
+  },
+})
+</script>
+<style scoped lang="less" src="./Item.less"></style>
