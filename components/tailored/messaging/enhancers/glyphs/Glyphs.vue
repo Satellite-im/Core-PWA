a261d8efc228e6fea2ac1365202f5d1870b0dc06
Eric Lee <eric.lee6174@gmail.com>
2021-09-24 21:11:23 -0600

    feat(glyphs popup): add Glyphs component

---
+++ components/tailored/messaging/enhancers/glyphs/Glyphs.vue
@@ -0,0 +1,13 @@
+<template src="./Glyphs.html"></template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      selectedPack: null,
+    }
+  },
+})
+</script>
+<style scoped lang="less" src="./Glyphs.less"></style>
