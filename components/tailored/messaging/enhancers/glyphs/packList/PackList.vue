5bc97a62af003ef7b31b4cd157988c0732e9ea6f
Eric Lee <eric.lee6174@gmail.com>
2021-09-24 21:11:41 -0600

    feat(glyphs popup): add pack list

---
+++ components/tailored/messaging/enhancers/glyphs/packList/PackList.vue
@@ -0,0 +1,13 @@
+<template src="./PackList.html"></template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      selectedPack: null,
+    }
+  },
+})
+</script>
+<style scoped lang="less" src="./PackList.less"></style>
