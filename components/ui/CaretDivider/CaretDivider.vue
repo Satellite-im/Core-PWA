0c461e2845ed0524ded2674842d91f2ff92961e4
RetroPronghorn <retropronghorn@gmail.com>
2021-09-29 10:10:08 -0400

    feat(messaging): added caret divider

---
+++ components/ui/CaretDivider/CaretDivider.vue
@@ -0,0 +1,17 @@
+<template src="./CaretDivider.html"></template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    props: {
+        text: {
+            type: String,
+            default: 'Missing Text',
+        }
+    }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less" src="./CaretDivider.less"></style>
