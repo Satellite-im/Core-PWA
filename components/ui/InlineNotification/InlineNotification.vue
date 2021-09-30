d0f3bba38361b8428a83365d63a5bc7cde0b3058
RetroPronghorn <retropronghorn@gmail.com>
2021-09-30 00:44:28 -0400

    feat(Inline Notification): Added inline notification component

---
+++ components/ui/InlineNotification/InlineNotification.vue
@@ -0,0 +1,15 @@
+<template src="./InlineNotification.html"></template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    text: {
+      type: String,
+      default: 'Missing Text',
+      required: true,
+    },
+  },
+})
+</script>
+<style scoped lang="less" src="./InlineNotification.less"></style>
