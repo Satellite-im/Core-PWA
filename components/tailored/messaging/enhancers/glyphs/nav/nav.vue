5bb3deff850f2e9b0e9b74cf27f7f42f83b572cb
Eric Lee <eric.lee6174@gmail.com>
2021-09-29 22:20:26 -0600

    SA-133: Update styles of Nav and open marketplace dialog when click marketplace btn

---
+++ components/tailored/messaging/enhancers/glyphs/nav/nav.vue
@@ -0,0 +1,25 @@
+<template src="./nav.html"></template>
+<script lang="ts">
+import Vue from 'vue'
+import { mapState } from 'vuex'
+
+export default Vue.extend({
+  data() {
+    return {
+      selectedPack: null,
+    }
+  },
+  computed: {
+    ...mapState(['ui', 'search']),
+  },
+  methods: {
+    toggleMarketPlace() {
+      this.$store.commit('toggleModal', {
+        name: 'showMarketPlace',
+        state: !this.ui.modals.showMarketPlace,
+      })
+    },
+  },
+})
+</script>
+<style scoped lang="less" src="./nav.less"></style>
