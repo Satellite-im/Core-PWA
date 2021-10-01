a140aab1f4aa138882a357e786957397cac02364
RetroPronghorn <retropronghorn@gmail.com>
2021-10-01 00:15:23 -0400

    feat(Streaming): Added 'now live' screen share / streaming icon

--- store/media/state.ts
+++ store/media/state.ts
@@ -1,11 +1,13 @@
 interface MediaState {
   incomingCall: String
   activeCall: String
+  streaming: Boolean
 }
 
 const InitialMediaState = (): MediaState => ({
   incomingCall: 'Phoenix Kalindi',
   activeCall: '0x0',
+  streaming: true,
 })
 
 export default InitialMediaState
