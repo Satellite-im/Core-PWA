943959aa856e6664cacee03b00c74ebc06655733
Eric Lee <eric.lee6174@gmail.com>
2021-09-30 18:49:29 -0600

    feat(Glyphs): Add random glyph items for Most Used

--- components/tailored/messaging/enhancers/glyphs/pack/Pack.vue
+++ components/tailored/messaging/enhancers/glyphs/pack/Pack.vue
@@ -1,6 +1,5 @@
 <template src="./Pack.html"></template>
 <script lang="ts">
-
 // eslint-disable-next-line import/named
 import Vue, { PropType } from 'vue'
 import { Glyph } from '~/types/ui/glyph'
