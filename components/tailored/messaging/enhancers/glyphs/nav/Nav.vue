35f901c92dc30e3fcb75d800f8ae7ce904a905cd
Eric Lee <eric.lee6174@gmail.com>
2021-10-01 11:00:00 +0700

    Feat(Glyphs): Implement Glyphs Popover (#164)
    * feat(Glyph): add glyph type
    
    * feat(Glyph): add mock data
    
    * feat(glyphs popup): change global css for glyphs popup
    
    * feat(glyphs popup): rename glyphs mock data
    
    * feat(glyphs popup): add Glyphs component
    
    * feat(glyphs popup): add pack list
    
    * feat(glyphs popup): add glyph item
    
    * SA-133: Update styles of Nav and open marketplace dialog when click marketplace btn
    
    * feat(Glyphs): Add Nav component
    
    * feat(Glyphs): Add Search Box
    
    * feat(Glyphs): Add Pack Group Component
    
    * feat(Glyphs): Apply auto-scroll to Glyph Contents
    
    * feat(Glyphs): Add random glyph items for Most Used
    
    * feat(Glyphs): Fix mouseleave undefined console issues
    
    * feat(Glyphs): Add GlyphInfo component when hover item
    
    * feat(Glyphs): Set hovered data when mouseover on glyph item
    
    * feat(Glyphs): Filter glyphs when search text is changed & click nav item

---
+++ components/tailored/messaging/enhancers/glyphs/nav/Nav.vue
@@ -0,0 +1,25 @@
+<template src="./Nav.html"></template>
+<script lang="ts">
+import Vue from 'vue'
+import { mapState } from 'vuex'
+
+export default Vue.extend({
+  data() {
+    return {
+      selectedPack: null,
+    }
+  },
+  computed: {
+    ...mapState(['ui', 'search']),
+  },
+  methods: {
+    toggleMarketPlace() {
+      this.$store.commit('toggleModal', {
+        name: 'showMarketPlace',
+        state: !this.ui.modals.showMarketPlace,
+      })
+    },
+  },
+})
+</script>
+<style scoped lang="less" src="./Nav.less"></style>
