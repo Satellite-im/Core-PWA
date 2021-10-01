772b0aea5283f27d41365c6eb43ed2e5be07ec38
Eric Lee <eric.lee6174@gmail.com>
2021-09-30 21:57:01 -0600

    feat(Glyphs): Filter glyphs when search text is changed & click nav item

--- components/tailored/messaging/enhancers/glyphs/Glyphs.vue
+++ components/tailored/messaging/enhancers/glyphs/Glyphs.vue
@@ -5,9 +5,26 @@ import Vue from 'vue'
 export default Vue.extend({
   data() {
     return {
+      filteredGlyphs: this.$mock.glyphs,
       searchText: '',
       selectedPack: null,
     }
+  },
+  watch: {
+    searchText() {
+      this.filter(this.searchText)
+    },
+  },
+  methods: {
+    filter(filterValue: any) {
+      this.filteredGlyphs = Object.entries(this.$mock.glyphs).reduce(
+        (prev, [key, value]: [string, any]) => ({
+          ...prev,
+          ...(value?.name?.includes(filterValue) ? { [key]: value } : {}),
+        }),
+        {}
+      )
+    },
   },
 })
 </script>
