8c04672c91b20f13505408e4f021151beb801493
Eric Lee <eric.lee6174@gmail.com>
2021-09-30 18:08:24 -0600

    feat(Glyphs): Add Pack Group Component

---
+++ components/tailored/messaging/enhancers/glyphs/pack/Pack.vue
@@ -0,0 +1,22 @@
+<template src="./Pack.html"></template>
+<script lang="ts">
+
+// eslint-disable-next-line import/named
+import Vue, { PropType } from 'vue'
+import { Glyph } from '~/types/ui/glyph'
+
+export default Vue.extend({
+  props: {
+    pack: {
+      type: Object as PropType<Glyph>,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      isOpen: true,
+    }
+  },
+})
+</script>
+<style lang="less" src="./Pack.less"></style>
