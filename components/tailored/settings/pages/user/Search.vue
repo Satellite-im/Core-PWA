64ae8a592911464c9f9f148b91a964f665511904
Eric Lee <eric.lee6174@gmail.com>
2021-10-06 10:04:48 +0700

    fix(Invite Friend): Removing selected friends from "invite friend" dropdown in Create a Server (#171)

--- components/tailored/settings/pages/user/Search.vue
+++ components/tailored/settings/pages/user/Search.vue
@@ -1,6 +1,7 @@
 <template src="./Search.html"></template>
 
 <script lang="ts">
+// eslint-disable-next-line import/named
 import Vue, { PropType } from 'vue'
 
 // @ts-ignore
@@ -9,6 +10,17 @@ import { PlusIcon, XIcon } from 'vue-feather-icons'
 import { User } from '~/types/ui/user'
 import { InputStyle, InputSize } from '~/components/interactables/Input/types'
 import { Users } from '~/mock/users'
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    selected: Array<User>
+    dropDown: boolean
+    showDropDown: () => void
+    selection: number
+    selectUser: (user: User, event: Event) => void
+    filteredResult: Array<User>
+  }
+}
 
 export default Vue.extend({
   components: {
@@ -48,6 +60,17 @@ export default Vue.extend({
       selection: -1,
     }
   },
+  computed: {
+    filteredResult() {
+      return this.result.filter((user: User) => {
+        const isAlreadyExist = this.selected.find(
+          (selectedUser) => selectedUser.name === user.name
+        )
+        if (isAlreadyExist) return false
+        return true
+      })
+    },
+  },
   mounted() {
     const searchSlot = this.$refs.searchSlot as HTMLElement
     const searchResult = this.$refs.searchResult as HTMLElement
@@ -86,16 +109,7 @@ export default Vue.extend({
         return
       }
       event.stopPropagation()
-      const exists = this.selected.find((item, i) => {
-        if (item.name === user.name) {
-          this.selected.splice(i, 1)
-          return item
-        }
-        return null
-      })
-      if (!exists) {
-        this.selected.push(user)
-      }
+      this.selected.push(user)
       this.showDropDown()
       this.$emit('input', this.selected)
     },
@@ -113,7 +127,7 @@ export default Vue.extend({
         case 'Down':
         case 'ArrowDown':
           this.selection++
-          if (this.result.length <= this.selection) {
+          if (this.filteredResult.length <= this.selection) {
             this.selection = 0
           }
           break
@@ -121,11 +135,11 @@ export default Vue.extend({
         case 'ArrowUp':
           this.selection--
           if (this.selection < 0) {
-            this.selection = this.result.length - 1
+            this.selection = this.filteredResult.length - 1
           }
           break
         case 'Enter':
-          this.selectUser(this.result[this.selection], event)
+          this.selectUser(this.filteredResult[this.selection], event)
           break
       }
       this.showDropDown()
