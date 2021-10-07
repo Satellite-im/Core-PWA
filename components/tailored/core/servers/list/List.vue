96ce77cc0162310cdb8111992272e0513a155f02
RetroPronghorn <retropronghorn@gmail.com>
2021-10-07 05:41:12 -0400

    feat(Icons): Added custom satellite logo icon

--- components/tailored/core/servers/list/List.vue
+++ components/tailored/core/servers/list/List.vue
@@ -2,12 +2,13 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { SettingsIcon, PlusIcon } from 'satellite-lucide-icons'
+import { SettingsIcon, PlusIcon, SatelliteIcon } from 'satellite-lucide-icons'
 
 export default Vue.extend({
   components: {
     SettingsIcon,
     PlusIcon,
+    SatelliteIcon,
   },
   props: {
     unreads: {
