c685df3d4472d6398c0fae0f8bc70d344d39d2c2
Eric Lee <eric.lee6174@gmail.com>
2021-10-07 04:37:41 -0600

    feature(Glyph MarketPlace): Add Glyphs component

---
+++ components/tailored/marketplace/glyphs/Glyphs.vue
@@ -0,0 +1,16 @@
+<template src="./Glyphs.html"></template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { marketGlyphs } from '~/mock/marketplace'
+export default Vue.extend({
+  props: {},
+  data() {
+    return {
+      marketGlyphs,
+    }
+  },
+})
+</script>
+
+<style scoped lang="less" src="./Glyphs.less"></style>
