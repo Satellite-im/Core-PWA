3b7d633e80044a6ed9e1bcd4cb89c48f83daffaa
Eric Lee <eric.lee6174@gmail.com>
2021-10-13 06:40:51 -0600

    feat(Glyphs MarketPlace): Add Trendings

---
+++ components/tailored/marketplace/glyphs/trending/Trending.vue
@@ -0,0 +1,16 @@
+<template src="./Trending.html"></template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+
+export default Vue.extend({
+  props: {
+    list: {
+      type: Array as PropType<Array<any>>,
+      default: () => [],
+    },
+  },
+})
+</script>
+
+<style scoped lang="less" src="./Trending.less"></style>
