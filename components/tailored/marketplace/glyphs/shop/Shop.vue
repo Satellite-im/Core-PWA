ec440e7a763e900118a25f691a0dd494e081cf27
Eric Lee <eric.lee6174@gmail.com>
2021-10-18 07:48:46 -0600

    feat(Glyhs MarketPlace): Add Glyph Shop Section

---
+++ components/tailored/marketplace/glyphs/shop/Shop.vue
@@ -0,0 +1,17 @@
+<template src="./Shop.html"></template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { marketGlyphShopFilter, marketGlyphs } from '~/mock/marketplace'
+
+export default Vue.extend({
+  data() {
+    return {
+      marketGlyphShopFilter,
+      marketGlyphs,
+    }
+  },
+})
+</script>
+
+<style scoped lang="less" src="./Shop.less"></style>
