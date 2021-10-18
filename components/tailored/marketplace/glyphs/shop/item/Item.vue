27f387fc3deb27d6348686aed6b6dd4a08826bc5
Eric Lee <eric.lee6174@gmail.com>
2021-10-18 07:48:12 -0600

    feat(Glyhs MarketPlace): Add Glyph Shop Item Card

---
+++ components/tailored/marketplace/glyphs/shop/item/Item.vue
@@ -0,0 +1,23 @@
+<template src="./Item.html"></template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import { HeartIcon, ShoppingCartIcon } from 'satellite-lucide-icons'
+import { GlyphShopItem } from '~/types/marketplace/marketplace'
+
+export default Vue.extend({
+  components: {
+    HeartIcon,
+    ShoppingCartIcon,
+  },
+  props: {
+    item: {
+      type: Object as PropType<GlyphShopItem>,
+      default: () => {},
+    },
+  },
+  data() {},
+})
+</script>
+
+<style scoped lang="less" src="./Item.less"></style>
