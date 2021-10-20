f175f3d1b19b980796f542da7318979f893f0be9
Eric Lee <eric.lee6174@gmail.com>
2021-10-20 13:44:04 -0600

    feat(Glyph MarketPlace): Add Popular Glyph Component

---
+++ components/tailored/marketplace/glyphs/popular/Popular.vue
@@ -0,0 +1,27 @@
+<template src="./Popular.html"></template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { ChevronLeftIcon, ChevronRightIcon } from 'satellite-lucide-icons'
+import { GlyphMarketViewStatus } from '~/store/ui/types'
+
+export default Vue.extend({
+  components: {
+    ChevronLeftIcon,
+    ChevronRightIcon,
+  },
+  data() {
+    return {
+      popularGlyph: null,
+      popularIndex: 1,
+    }
+  },
+  mounted() {
+    this.popularGlyph = this.$mock.marketplace.marketGlyphs[this.popularIndex]
+  },
+  methods: {
+  },
+})
+</script>
+
+<style scoped lang="less" src="./Popular.less"></style>
