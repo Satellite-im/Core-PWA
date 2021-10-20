186d44fe6086aeadf32d8a0caddffb948fa4228f
Eric Lee <eric.lee6174@gmail.com>
2021-10-20 13:43:52 -0600

    feat(Glyph MarketPlace): Add Featured Glyph Component

---
+++ components/tailored/marketplace/glyphs/featured/Featured.vue
@@ -0,0 +1,33 @@
+<template src="./Featured.html"></template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { ChevronLeftIcon, ChevronRightIcon } from 'satellite-lucide-icons'
+import { GlyphMarketViewStatus } from '~/store/ui/types'
+
+export default Vue.extend({
+  components: {
+    ChevronLeftIcon,
+    ChevronRightIcon,
+  },
+  data() {
+    return {
+      featuredGlyph: null,
+      featuredIndex: 0,
+    }
+  },
+  mounted() {
+    this.featuredGlyph = this.$mock.marketplace.marketGlyphs[this.featuredIndex]
+  },
+  methods: {
+    shopAllStickers() {
+      this.$store.commit('setGlyphMarketplaceView', {
+        view: GlyphMarketViewStatus.SHOP_ALL,
+        shopId: null,
+      })
+    },
+  },
+})
+</script>
+
+<style scoped lang="less" src="./Featured.less"></style>
