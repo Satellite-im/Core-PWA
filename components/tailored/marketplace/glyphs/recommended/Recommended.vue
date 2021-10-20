103ce82f6f932469b8210fe89efedc2bde891818
Eric Lee <eric.lee6174@gmail.com>
2021-10-20 15:45:11 -0600

    feat(Glyph MarketPlace): Add recommended glyph marketplace component

---
+++ components/tailored/marketplace/glyphs/recommended/Recommended.vue
@@ -0,0 +1,31 @@
+<template src="./Recommended.html"></template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import { HeartIcon, ShoppingCartIcon } from 'satellite-lucide-icons'
+import { GlyphShopItem } from '~/types/marketplace/marketplace'
+import { GlyphMarketViewStatus } from '~/store/ui/types'
+
+export default Vue.extend({
+  components: {
+    HeartIcon,
+    ShoppingCartIcon,
+  },
+  props: {
+    item: {
+      type: Object as PropType<GlyphShopItem>,
+      default: null,
+    },
+  },
+  methods: {
+    goToShopItem() {
+      this.$store.commit('setGlyphMarketplaceView', {
+        view: GlyphMarketViewStatus.SHOP_DETAIL,
+        shopId: this.item.id,
+      })
+    },
+  },
+})
+</script>
+
+<style scoped lang="less" src="./Recommended.less"></style>
