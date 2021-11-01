098e797dcc371bcbc11b7bc66233d867308417d2
nickjjang <nickjang234@gmail.com>
2021-11-01 02:28:26 -0400

    Fix(100vh) issue not tested

--- layouts/chat.vue
+++ layouts/chat.vue
@@ -98,6 +98,13 @@ export default Vue.extend({
   mounted() {
     this.$Sounds.changeLevels(this.audio.volume / 100)
     this.$store.commit('ui/setTypingUser', this.$mock.users[0])
+
+    const appHeight = () => {
+      const doc = document.documentElement
+      doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+    }
+    window.addEventListener('resize', appHeight)
+    appHeight()
   },
 })
 </script>
