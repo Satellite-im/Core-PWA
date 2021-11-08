4311b04f775d87873a8535fbbb343da3beed3cca
jekrimo <jekrimo@gmail.com>
2021-11-08 14:48:04 -0700

    add error component

---
+++ components/ui/Popups/Error/Error.vue
@@ -0,0 +1,32 @@
+<template src="./Error.html"></template>
+
+<script>
+export default {
+  name: 'Error',
+  data () {
+    return {
+      error: false
+    }
+  },
+  methods: {
+    confirm () {
+      this.error = false
+    }
+  },
+  mounted () {
+    window.onerror = (message, source, lineno, colno, error) => {
+      this.error = {
+        message,
+        source,
+        lineno,
+        colno,
+        error,
+        userAgent: navigator.userAgent
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less" src="./Error.less"></style>
