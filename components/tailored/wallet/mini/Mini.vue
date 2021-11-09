89c3d484c0901f8c081da06d789fd6cead2ef4dd
RetroPronghorn <retropronghorn@gmail.com>
2021-11-09 00:48:03 -0500

    feat(Privacy): Added privacy page

---
+++ components/tailored/wallet/mini/Mini.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="mini-wallet">
+    <TailoredWalletMiniNav :route="route" />
+    <TailoredWalletMiniSend v-if="route === 'send'" />
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      route: 'send',
+    }
+  },
+})
+</script>
+<style scoped lang="less" src="./Mini.less"></style>
