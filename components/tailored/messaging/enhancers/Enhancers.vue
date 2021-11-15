c6ef75ad97e33e53a44878ca1ef3e1664e070d02
Eric Lee <eric.lee6174@gmail.com>
2021-11-15 01:52:07 -0700

    fix(Glyph): Fix 'After opening the Glyphs menu, user should be able to press it again to close it'

--- components/tailored/messaging/enhancers/Enhancers.vue
+++ components/tailored/messaging/enhancers/Enhancers.vue
@@ -99,9 +99,16 @@ export default Vue.extend({
      * @description Toggles enhancers by commiting the opposite of it's current value (this.ui.enhancers.show) to toggleEnhancers in state
      * @example v-on:click="toggleEnhancers"
      */
-    toggleEnhancers() {
+    toggleEnhancers(event: Event) {
       this.clickEvent()
+      /* Prevent toggle twice when toggle enhancer button is clicked (for mobile case ) */
+      const chatToggleEnhancer = document.getElementById('chat-enhancer-toggle')
       // @ts-ignore
+      if (!chatToggleEnhancer?.contains(event.target)) {
+        this.$store.commit('ui/toggleEnhancers', {
+          show: !this.ui.enhancers.show,
+        })
+      }
       this.$store.commit('ui/toggleEnhancers', {
         show: !this.ui.enhancers.show,
       })
