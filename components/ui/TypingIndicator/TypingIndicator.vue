877546927af93841b76388e970bf594f47a9f3b6
Jeff Kristian <jekrimo@gmail.com>
2021-11-17 08:43:04 -0700

    refactor(Typing Indicator): AP-37 - Typing indicator component now accepts an array of user names displaying up to 3 names, when 4+ names: displays many typing, and 0: nothing

--- components/ui/TypingIndicator/TypingIndicator.vue
+++ components/ui/TypingIndicator/TypingIndicator.vue
@@ -2,15 +2,37 @@
 
 <script lang="ts">
 import Vue from 'vue'
-
 export default Vue.extend({
+  name: 'Typing Indicator',
   props: {
-    userName: {
-      type: String,
-      default: '',
+    users: {
+      type: Array,
+      default: () => [
+],
       required: false,
     },
   },
+    /**
+     * @methods checkUsersTyping
+     * @description Checks the length of the users names array being fed to component
+     * returns a numeric value that the template checks in order to display the correct ui to the user
+     * @example <div id="text-container" v-if="checkUsersTyping() === 1">
+     *          </div>
+     */
+  methods: {
+    checkUsersTyping() {
+      if(this.users.length <= 3 && this.users.length != 0) {
+        return 1
+      } else if (this.users.length > 3){
+        return 0
+      } else {
+        return 2
+      }
+    }
+  },
+  mounted() {
+    this.checkUsersTyping();
+  }
 })
 </script>
 
