68ea6ecafc275747802d324fcf98d40db25616db
stavares843 <saratavares843@gmail.com>
2021-11-18 16:54:26 +0000

    chore - remove cypress placeholders

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,29 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
 Cypress.Commands.add('createAccount', () => {
     cy.visit('/')
     cy.get('#app > div > div > div:nth-child(2) > div > div:nth-child(1) > input').type('22,A9ZJ[F\t5g',{log:false}) //update for data-cy label - AP-51
