63a2f88e9fa40d2dc31cf8fbea74b4dfb2f58095
stavares843 <saratavares843@gmail.com>
2021-11-18 16:38:00 +0000

    chore - update test with data-cy label

--- cypress/integration/create-account-negative-tests.js
+++ cypress/integration/create-account-negative-tests.js
@@ -1,10 +1,10 @@
     it('Try to create account with PIN less than 5 digits', () => {
     cy.visit('/')
     cy.contains('Create Account Pin')
-    cy.get('#app > div > div > div:nth-child(2) > div > div:nth-child(2) > a').click() //update for data-cy label - AP-52
+    cy.get('[data-cy=add-input]').type('1')
+    cy.get('[data-cy=submit-input]').click()
     cy.contains('Pin must be at least 5 characters.')
     cy.reload()
-    cy.get('#app > div > div > div:nth-child(2) > div > div:nth-child(2) > a').click() //update for data-cy label - AP-52
-    cy.get('#app > div > div > div:nth-child(2) > div > div:nth-child(1) > input').type('1') //update for data-cy label - AP-51
+    cy.get('[data-cy=submit-input]').click()
     cy.contains('Pin must be at least 5 characters.')
 })
