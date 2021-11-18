4e2a1d3672df7e672745604d65e201148a1e43cb
stavares843 <saratavares843@gmail.com>
2021-11-18 18:38:25 +0000

    chore - add faker lib on commands file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,6 @@
 const faker = require('faker');
+const randomName = faker.internet.userName(name); // generate random name
+const randomStatus = faker.lorem.word(); // generate random status
 
 Cypress.Commands.add('createAccount', () => {
     cy.visit('/')
@@ -7,8 +9,8 @@ Cypress.Commands.add('createAccount', () => {
     cy.get('.is-primary > #custom-cursor-area').click()
     cy.contains('I Saved It').click()
     Cypress.on('uncaught:exception', (err, runnable) => false); // temporary until AP-48 gets fixed
-    cy.get('.input-container:nth-child(3) .input').type('name test') // add data-cy label - AP-53
-    cy.get('.input-container:nth-child(4) .input').type('status') // add data-cy label - AP-53
+    cy.get('.input-container:nth-child(3) .input').type(randomName) //add data-cy label - AP-53
+    cy.get('.input-container:nth-child(4) .input').type(randomStatus) // add data-cy label - AP-53
     cy.get('.create-server-btn > #custom-cursor-area').click();
 })
 
