d616326df13105ac24d7dff49762e9cbbac720a2
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2021-11-22 23:53:11 +0000

    test - add crypto test

---
+++ test/Crypto.spec.ts
@@ -0,0 +1,14 @@
+import Crypto from '../libraries/Crypto/Crypto.ts'
+
+describe('Logo', () => {
+  // create new instance of class
+  const crypto = new Crypto();
+
+  // test that signMessage method exists 
+  test("defines signMessage()", () => {
+    expect(typeof crypto.signMessage).toBe("function");
+  });
+  test("expect signmessage without signingKey to be null", () => {
+    expect(crypto.signMessage('dogs')).toBe(null);
+  });
+})
