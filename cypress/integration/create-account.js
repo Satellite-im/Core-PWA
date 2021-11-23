a1e3c3a0ff9f21450b8cfc59854268036e04ff03
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2021-11-23 19:49:26 +0000

    chore - add data-cy labels on test

--- cypress/integration/create-account.js
+++ cypress/integration/create-account.js
@@ -26,20 +26,20 @@ for (const command of ['visit', 'click', 'trigger', 'type', 'clear', 'reload', '
     cy.get('[data-cy=add-input]').type('22,A9ZJ[F\t5g',{log:false})
     cy.contains('Store Pin? (Less Secure)')
     cy.get('[data-cy=submit-input]').click()
-    cy.contains("We're going to create an account for you. On the next screen you'll see a set of words. Screenshot this or write it down. This is the only way to backup your account.")
+    cy.contains("We're going to create an account for you. On the next screen, you'll see a set of words. Screenshot this or write it down. This is the only way to backup your account.")
     cy.get('.is-primary > #custom-cursor-area').click()
     cy.contains('I Saved It').click()
     cy.contains('Customize how the world sees you, choose something memorable.', {timeout: 5000}).should('be.visible')
     cy.contains('Username')
     Cypress.on('uncaught:exception', (err, runnable) => false); // temporary until AP-48 gets fixed
-    cy.get('.input-container:nth-child(3) .input').type(randomName) //add data-cy label - AP-53
+    cy.get('[data-cy=username-input]').type(randomName)
     cy.contains('Status')
-    cy.get('.input-container:nth-child(4) .input').type(randomStatus) // add data-cy label - AP-53
+    cy.get('[data-cy=status-input]').type(randomStatus)
     const filepath = 'images/logo.png'
     cy.get('.is-outlined > #custom-cursor-area').click()
     cy.get('.input-file').attachFile(filepath)
     cy.contains('Crop').click()
-    cy.get('.create-server-btn > #custom-cursor-area').click()
+    cy.get('[data-cy=sign-in-button]').click()
     cy.contains('Linking Satellites...')
     cy.contains('Working on the space station', {timeout: 30000}).should('be.visible')
 })
