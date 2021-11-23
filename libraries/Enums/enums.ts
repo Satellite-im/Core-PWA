4dcf3d06f6319f77c2a6d373f94f13b714b69074
Jeff Kristian <jekrimo@gmail.com>
2021-11-23 13:30:58 -0700

    chore - update(enums): set up enum directory and start creation of types

---
+++ libraries/Enums/enums.ts
@@ -0,0 +1,6 @@
+import {KeybindingEnum} from '~/libraries/Enums/types/keybindings'
+import { PropCommonEnum } from '~/libraries/Enums/types/prop-common-events'
+export{
+    KeybindingEnum as KeybindingEnum,
+    PropCommonEnum as PropCommonEnum,
+}
