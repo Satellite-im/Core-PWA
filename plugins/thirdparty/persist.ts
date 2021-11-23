498ea5e32ef24b719fea0508d31bb7e883b11c09
nickjjang <nickjang234@gmail.com>
2021-11-23 14:59:26 -0500

    Fix(Login): Unlock should be persisted when store pin is enabled

--- plugins/thirdparty/persist.ts
+++ plugins/thirdparty/persist.ts
@@ -2,7 +2,7 @@
  * This plugin enables persistant storage to the state.
  */
 
-import { omit } from 'lodash'
+import { omit, remove } from 'lodash'
 import { VuexPersistence } from 'vuex-persist'
 
 // Add mutations here to blacklist saving to store
@@ -32,9 +32,13 @@ export default ({ store }: { store: any }) => {
   new VuexPersistence({
     key: 'Satellite-Store',
     reducer: (state: any) => {
+      let blackList = propertiesBlacklist;
+      if (state.accounts.storePin && !state.accounts.locked) {
+        blackList = [];
+      }
       // Lodash omit is not so performant, but it's actually fine
       // for blacklisting the state to be persisted
-      return omit(state, propertiesBlacklist)
+      return omit(state, blackList)
     },
     filter: (mutation) => {
       // Allows blacklisting of data we don't want stored
