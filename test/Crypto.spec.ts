bdf19ae0d6723912ca5213656066b605782aace7
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2021-11-24 06:25:47 +0000

    test - add cypress data-cy labels + update jest test (#301)
    * chore - update cypress version
    
    * chore - add data-cy labels
    
    * chore - add data-cy labels on test
    
    * chore - fix jest test
    
    * chore - update jest file name
    
    * chore - update test file
    
    * chore - minor update to test file
    
    Co-authored-by: Matt Wisniewski <retropronghorn@gmail.com>

--- test/Crypto.spec.ts
+++ test/Crypto.spec.ts
@@ -1,14 +1,15 @@
-import Crypto from '../libraries/Crypto/Crypto.ts'
+import Crypto from '../libraries/Crypto/Crypto'
+import 'ts-jest'
 
-describe('Logo', () => {
+describe('Crypto', () => {
   // create new instance of class
   const crypto = new Crypto()
 
   // test that signMessage method exists 
   test('defines signMessage()', () => {
     expect(typeof crypto.signMessage).toBe('function')
-  });
+  })
   test('expect signmessage without signingKey to be null', () => {
     expect(crypto.signMessage('dogs')).toBe(null)
-  });
+  })
 })
