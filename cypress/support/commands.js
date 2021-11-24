f790d4be27bc81d37ffd5ff60bc757a84e30b8e0
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2021-11-24 06:25:47 +0000

    test - add cypress data-cy labels + update jest test (#301)
    * chore - update cypress version
    
    * chore - add data-cy labels
    
    * chore - add data-cy labels on test
    
    * chore - fix jest test
    
    * chore - update jest file name
    
    * chore - update test file
    
    * chore - minor update to test file
    
    Co-authored-by: Matt Wisniewski <retropronghorn@gmail.com>

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -9,9 +9,9 @@ Cypress.Commands.add('createAccount', () => {
     cy.get('.is-primary > #custom-cursor-area').click()
     cy.contains('I Saved It').click()
     Cypress.on('uncaught:exception', (err, runnable) => false); // temporary until AP-48 gets fixed
-    cy.get('.input-container:nth-child(3) .input').type(randomName) //add data-cy label - AP-53
-    cy.get('.input-container:nth-child(4) .input').type(randomStatus) // add data-cy label - AP-53
-    cy.get('.create-server-btn > #custom-cursor-area').click();
+    cy.get('[data-cy=username-input]').type(randomName)
+    cy.get('[data-cy=status-input]').type(randomStatus)
+    cy.get('[data-cy=sign-in-button]').click()
 })
 
 import 'cypress-file-upload';
