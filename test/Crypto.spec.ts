7f45a33306250c2a20c75cde8b15dfb0206177b0
Matt Wisniewski <retropronghorn@gmail.com>
2021-11-24 00:25:35 -0500

    feat(MiniWallet): Added mini wallet component (#303)

--- test/Crypto.spec.ts
+++ test/Crypto.spec.ts
@@ -2,13 +2,13 @@ import Crypto from '../libraries/Crypto/Crypto.ts'
 
 describe('Logo', () => {
   // create new instance of class
-  const crypto = new Crypto();
+  const crypto = new Crypto()
 
   // test that signMessage method exists 
-  test("defines signMessage()", () => {
-    expect(typeof crypto.signMessage).toBe("function");
+  test('defines signMessage()', () => {
+    expect(typeof crypto.signMessage).toBe('function')
   });
-  test("expect signmessage without signingKey to be null", () => {
-    expect(crypto.signMessage('dogs')).toBe(null);
+  test('expect signmessage without signingKey to be null', () => {
+    expect(crypto.signMessage('dogs')).toBe(null)
   });
 })
