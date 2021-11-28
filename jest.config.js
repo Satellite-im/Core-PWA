af1ab5e16ae84f807dfcea45657ac8f637c1ece2
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2021-11-28 16:23:24 +0000

    chore: update jest coverage rules + update prettier rules (#322)
    * chore: update jest coverage rules
    
    * chore: update prettier rules and fix linting on jest config file
    
    * chore: remove not needed line from cypress test

--- jest.config.js
+++ jest.config.js
@@ -4,21 +4,32 @@ module.exports = {
     '**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
     '!**/node_modules/**',
+    '!**/*.config.js',
+    '!**/*.config.ts',
+    '!**/config.ts',
+    '!**/startup.js',
+    '!**/*.eslintrc.js'
   ],
   moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
     '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js'
   },
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/.nuxt',
+    '<rootDir>/cypress',
+    '<rootDir>/coverage',
+    '<rootDir>/plugins'
+  ],
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest'],
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest']
   },
   transformIgnorePatterns: [
     '/node_modules/',
     'node_modules/(?!@mylibrary/)',
-    '^.+\\.module\\.(css|sass|scss)$',
-  ],
+    '^.+\\.module\\.(css|sass|scss)$'
+  ]
 }
