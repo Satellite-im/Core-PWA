5f86ac3ee3b12846895254e01c31cf6177c0e3da
Eric Lee <eric.lee6174@gmail.com>
2021-11-29 03:51:52 -0700

    feat(import account): add input account component

---
+++ pages/setup/InputAccount/index.vue
@@ -0,0 +1,34 @@
+<template src="./InputAccount.html" />
+
+<script lang="ts">
+import Vue from 'vue'
+import { PlusCircleIcon } from 'satellite-lucide-icons'
+
+export default Vue.extend({
+  name: 'InputAccountScreen',
+  components: {
+    PlusCircleIcon
+  },
+  data() {
+    return {
+      error: '',
+      phraseInput: '',
+      phrases: []
+    }
+  },
+  methods: {
+    recoverAccount() {
+      /* recover account action will be implemented on bip39 service ticket */
+    },
+    isOdd(num: number) {
+      return num % 2
+    },
+    addPhrase() {
+      if (this.phrases.length < 12) this.phrases.push(this.phraseInput)
+      this.phraseInput = ''
+    }
+  }
+})
+</script>
+
+<style lang="less" scoped src="./InputAccount.less"></style>
