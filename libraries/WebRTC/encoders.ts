d2e4800122efca32d36f2dc8e51f0cf06475db13
Manuel Tumiati <36959970+iltumio@users.noreply.github.com>
2021-11-29 15:54:53 +0100

    feat(webrtc): add low level abstractions for handling p2p connections and audio/video calls (#324)
    * wip: WebRTC implementation
    
    * chore: better structure
    
    * chore: documented all methods
    
    * fix: prettier config
    
    * fix: replaced p2pt dependency with the internal one
    
    * Update encoders.ts
    
    * fix: typos in comments and comma dangle
    
    Co-authored-by: Matt Wisniewski <contact.mattdylan@gmail.com>
    Co-authored-by: Matt Wisniewski <retropronghorn@gmail.com>

---
+++ libraries/WebRTC/encoders.ts
@@ -0,0 +1,45 @@
+import * as t from 'io-ts'
+
+export const wireBaseMessage = t.type({
+  type: t.string,
+  payload: t.unknown,
+  sentAt: t.number,
+})
+
+export const wireIdentificationMessage = t.intersection([
+  wireBaseMessage,
+  t.type({
+    type: t.literal('IDENTIFICATION'),
+    payload: t.type({
+      peerId: t.string,
+    }),
+  }),
+])
+
+export const wireDataMessage = t.intersection([
+  wireBaseMessage,
+  t.type({
+    type: t.literal('DATA'),
+  }),
+])
+
+export const wireSignalMessage = t.intersection([
+  wireBaseMessage,
+  t.type({
+    type: t.literal('SIGNAL'),
+    payload: t.type({
+      peerId: t.string,
+      data: t.unknown,
+    }),
+  }),
+])
+
+export const wireRefuseConnectionMessage = t.intersection([
+  wireBaseMessage,
+  t.type({
+    type: t.literal('REFUSE'),
+    payload: t.type({
+      peerId: t.string,
+    }),
+  }),
+])
