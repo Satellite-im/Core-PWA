6302a7b3b10cdef2604cd70e75f11cb2b6cb6cd6
Eric Lee <eric.lee6174@gmail.com>
2021-12-01 11:38:05 -0700

    fix(chat): fix spacing issue on chatbar

--- components/tailored/core/chatbar/Chatbar.vue
+++ components/tailored/core/chatbar/Chatbar.vue
@@ -66,7 +66,7 @@ export default Vue.extend({
     hasCommand() {
       const parsedCommand = parseCommand(this.ui.chatbarContent)
       const currentCommand = commands.find(
-        (cmd) => cmd.name === parsedCommand.name.toLowerCase()
+        (cmd) => cmd.name === parsedCommand.name.toLowerCase(),
       )
       return currentCommand != null
     },
@@ -78,7 +78,7 @@ export default Vue.extend({
      */
     isValidCommand() {
       const currentText = parseCommand(
-        this.ui.chatbarContent
+        this.ui.chatbarContent,
       ).name.toLowerCase()
       const currentArgs = parseCommand(this.ui.chatbarContent).args
       const currentCommand = commands.find((c) => c.name === currentText)
@@ -188,7 +188,8 @@ export default Vue.extend({
      * @example v-on:click="sendMessage"
      */
     sendMessage() {
-      const isEmpty = !this.value.replace(/\s/g, '').replace(/&nbsp;/g, '').length
+      const isEmpty = !this.value.replace(/\s/g, '').replace(/&nbsp;/g, '')
+        .length
       if (!this.recipient || isEmpty) {
         return
       }
@@ -215,7 +216,7 @@ export default Vue.extend({
      * @method handleDrop
      * @description Allows the drag and drop of files into the chatbar
      * @param e Drop event data object
-     * @example v-on:drop="handleDrop" 
+     * @example v-on:drop="handleDrop"
      */
     handleDrop(e: any) {
       e.preventDefault()
@@ -230,7 +231,7 @@ export default Vue.extend({
     handlePaste(e: any) {
       e.stopPropagation()
       const clipboardItems = e.clipboardData.items
-      if(clipboardItems && clipboardItems.length) {
+      if (clipboardItems && clipboardItems.length) {
         this.handleUpload(clipboardItems)
       }
     },
@@ -251,7 +252,7 @@ export default Vue.extend({
         // @ts-ignore
         this.$refs['file-upload']?.handleFile(handleFileExpectEvent)
       }
-    }
+    },
   },
   watch: {
     '$store.state.ui.chatbarContent': function () {
