93b9df68273e6103be3a3a6602c75acd1cedaa68
nickjjang <nickjang234@gmail.com>
2021-12-06 17:18:15 -0500

    feat(callduration): shows the call duration (AP-6)

--- store/webrtc/state.ts
+++ store/webrtc/state.ts
@@ -2,6 +2,9 @@ import { WebRTCState } from './types'
 
 const InitialWebRTCState = (): WebRTCState => ({
   initialized: false,
+  activeStream: {
+    createdAt: Date.now(),
+  },
 })
 
 export default InitialWebRTCState
