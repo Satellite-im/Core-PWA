24f7c06bfa353ac81c73cec0e17019a30dfcf858
Joe McGrath <josephmcg01@gmail.com>
2021-12-06 16:18:50 +0900

    fix(upload): fix upload for large files nsfw check was hogging too much memory and crashing for large files. skips check above 8MB closes #91

--- config.ts
+++ config.ts
@@ -92,4 +92,8 @@ export const Config = {
   routingMiddleware: {
     prerequisitesCheckBypass: ['auth', 'setup'],
   },
+  uploadByteLimit: 1048576 * 8, // 8mb
+  regex: {
+    image: '^.*.(apng|avif|gif|jpg|jpeg|jfif|pjpeg|pjp|png|svg|webp)$',
+  },
 }
