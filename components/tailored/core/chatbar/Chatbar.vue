bf07fc08102697ecaf591270638e52e56922d1b0
davidchan1219 <nkristic0@gmail.com>
2021-12-08 08:48:39 -0700

    fixed layout/chat conflict

--- components/tailored/core/chatbar/Chatbar.vue
+++ components/tailored/core/chatbar/Chatbar.vue
@@ -85,7 +85,7 @@ export default Vue.extend({
     hasCommand() {
       const parsedCommand = parseCommand(this.ui.chatbarContent)
       const currentCommand = commands.find(
-        (cmd) => cmd.name === parsedCommand.name.toLowerCase(),
+        (cmd) => cmd.name === parsedCommand.name.toLowerCase()
       )
       return currentCommand != null
     },
@@ -97,7 +97,7 @@ export default Vue.extend({
      */
     isValidCommand() {
       const currentText = parseCommand(
-        this.ui.chatbarContent,
+        this.ui.chatbarContent
       ).name.toLowerCase()
       const currentArgs = parseCommand(this.ui.chatbarContent).args
       const currentCommand = commands.find((c) => c.name === currentText)
@@ -126,7 +126,7 @@ export default Vue.extend({
     },
     placeholder() {
       if (!this.hasCommand && this.$data.text === '') {
-        return this.$t('ui.talk')
+        return this.$t('global.talk')
       } else {
         return ''
       }
@@ -251,35 +251,36 @@ export default Vue.extend({
      * @example v-on:click="sendMessage"
      */
     sendMessage() {
-      const isEmpty = !this.value.replace(/\s/g, '').replace(/&nbsp;/g, '')
-        .length
-      if (!this.recipient || isEmpty) {
-        return
-      }
+      if(this.recipient) {
+        const isEmpty = !this.value.replace(/\s/g, '').replace(/&nbsp;/g, '').length
+        if (!this.recipient || isEmpty) {
+          return
+        }
 
-      if (this.ui.replyChatbarContent.from) {
-        this.$store.dispatch('textile/sendReplyMessage', {
-          to: this.recipient.textilePubkey,
-          text: this.value,
-          replyTo: this.ui.replyChatbarContent.messageID,
-        })
-      } else {
-        this.$store.dispatch('textile/sendTextMessage', {
-          to: this.recipient.textilePubkey,
-          text: this.value,
-        })
-      }
+        if (this.ui.replyChatbarContent.from) {
+          this.$store.dispatch('textile/sendReplyMessage', {
+            to: this.recipient.textilePubkey,
+            text: this.value,
+            replyTo: this.ui.replyChatbarContent.messageID,
+          })
+        } else {
+          this.$store.dispatch('textile/sendTextMessage', {
+            to: this.recipient.textilePubkey,
+            text: this.value,
+          })
+        }
 
-      const messageBox = this.$refs.messageuser as HTMLElement
-      // Clear Chatbar
-      messageBox.innerHTML = ''
-      this.value = ''
+        const messageBox = this.$refs.messageuser as HTMLElement
+        // Clear Chatbar
+        messageBox.innerHTML = ''
+        this.value = ''
+      }
     },
     /**
      * @method handleDrop
      * @description Allows the drag and drop of files into the chatbar
      * @param e Drop event data object
-     * @example v-on:drop="handleDrop"
+     * @example v-on:drop="handleDrop" 
      */
     handleDrop(e: any) {
       e.preventDefault()
@@ -292,11 +293,8 @@ export default Vue.extend({
      * @example v-on:paste="handlePaste"
      */
     handlePaste(e: any) {
-      e.stopPropagation()
-      const clipboardItems = e.clipboardData.items
-      if (clipboardItems && clipboardItems.length) {
-        this.handleUpload(clipboardItems)
-      }
+      e.preventDefault()
+      this.handleUpload(e.clipboardData.items)
     },
     /**
      * @method handleUpload
@@ -305,7 +303,6 @@ export default Vue.extend({
      * @example this.handleUpload(someEvent.itsData.items)
      */
     handleUpload(items: Array<object>) {
-      /* check if type is image */
       const arrOfFiles: File[] = [...items]
         .filter((f: any) => f.type.includes('image'))
         .map((f: any) => f.getAsFile())
@@ -315,7 +312,7 @@ export default Vue.extend({
         // @ts-ignore
         this.$refs['file-upload']?.handleFile(handleFileExpectEvent)
       }
-    },
+    }
   },
   watch: {
     'ui.chatbarContent': function () {
