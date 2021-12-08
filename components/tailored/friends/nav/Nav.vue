87bc2b9e1b521dad62faf37a36ff3d668cdd1862
Kathy Chan <kathykemopaw@gmail.com>
2021-12-08 11:40:43 -0800

    feat(friendReqCount): calculate correct padding and clean up for template literals

--- components/tailored/friends/nav/Nav.vue
+++ components/tailored/friends/nav/Nav.vue
@@ -17,20 +17,6 @@ export default Vue.extend({
     UserPlusIcon,
     UserXIcon,
   },
-  computed: {
-    ...mapState(['friends']),
-    /**
-     * makeFriendReqTag: generates the Request tag number on the Request Button
-     * depending on the number of ongoing friend requests, it will generate a different replyLength
-     */
-    makeFriendReqTag() {
-      const replyLength = this.friends.incomingRequests.length
-      if (replyLength > 100) {
-        return '99+'
-      }
-      return replyLength
-    },
-  },
   props: {
     setRoute: {
       type: Function,
@@ -39,6 +25,37 @@ export default Vue.extend({
     route: {
       type: String,
       default: 'active',
+    },
+  },
+  computed: {
+    ...mapState(['friends']),
+    /**
+     * friendRequestPadding: calculates how much padding to add to a button depending on friendRequest length
+     * depending on the number of ongoing friend requests, it will generate a different px amount for padding-right
+     */
+    friendRequestPadding() {
+      const currentIncomingRequests = this.friends.incomingRequests.length.toString()
+      const length = this.friends.incomingRequests.length.toString().length
+
+      if (currentIncomingRequests === '0') {
+        return 11
+      }
+
+      if (length >= 3) {
+        return 46
+      }
+      return 22 + length * 8
+    },
+    /**
+     * makeFriendReqTag: generates the Request tag number on the Request Button
+     * depending on the number of ongoing friend requests, it will generate a different replyLength
+     */
+    makeFriendReqTag() {
+      const replyLength = this.friends.incomingRequests.length
+      if (replyLength >= 100) {
+        return '99+'
+      }
+      return replyLength
     },
   },
 })
