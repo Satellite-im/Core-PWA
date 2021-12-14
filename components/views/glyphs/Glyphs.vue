47754046b505875bd61cc06fe713a83b12a51efe
Joe McGrath <josephmcg01@gmail.com>
2021-12-14 14:38:55 +0900

    remove unnecessary import

--- components/views/glyphs/Glyphs.vue
+++ components/views/glyphs/Glyphs.vue
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import { mapState } from 'vuex'
 import { marketGlyphs } from '~/mock/marketplace'
 import { GlyphMarketViewStatus } from '~/store/ui/types'
-import _, { find, findKey, shuffle } from 'lodash'
+import { find, findKey, shuffle } from 'lodash'
 import { ShoppingBagIcon } from 'satellite-lucide-icons'
 
 export default Vue.extend({
@@ -14,13 +14,13 @@ export default Vue.extend({
   computed: {
     ...mapState(['ui']),
     selectedPack() {
-      const key = _.findKey(this.$mock.glyphs, ({ name }) => {
+      const key = findKey(this.$mock.glyphs, ({ name }) => {
         return name === this.ui.glyphModalPack
       })
       return this.$mock.glyphs[key]
     },
     samplePack() {
-      return _.shuffle(this.selectedPack.stickerURLs).slice(1, 4)
+      return shuffle(this.selectedPack.stickerURLs).slice(1, 4)
     },
   },
   methods: {
@@ -36,7 +36,7 @@ export default Vue.extend({
         name: 'marketplace',
         state: !this.ui.modals.marketPlace,
       })
-      const marketInfo = _.find(marketGlyphs, ({ glyph }) => {
+      const marketInfo = find(marketGlyphs, ({ glyph }) => {
         return glyph.name === this.selectedPack.name
       })
       this.$store.commit('ui/setGlyphMarketplaceView', {
