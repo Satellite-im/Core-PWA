9f631aaea1323e1696dd4afe62587b1e59493ed4
RetroPronghorn <retropronghorn@gmail.com>
2021-12-14 19:28:05 -0500

    feat(Alerts): added alerts also refactored a few things

---
+++ components/views/navigation/server/sidebar/channels/VoiceUser/VoiceUser.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="voice-user">
+    <UiCircle type="random" :seed="user.address" :size="25" />
+    <TypographyText :text="user.name" class="voice-user-name" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VoiceUser',
+  props: ['user'],
+  data() {
+    return {
+      collapsed: false,
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.voice-user {
+  margin-left: 1rem;
+  padding: 0.25rem;
+  display: inline-flex;
+}
+.circle-icon {
+  width: 20px;
+  height: 20px;
+  float: left;
+}
+.voice-user-name {
+  padding-left: 5px;
+  font-size: 11pt;
+}
+</style>
---
+++ components/views/navigation/server/sidebar/channels/group/Group.vue
@@ -0,0 +1,38 @@
+<template src="./Group.html" />
+
+<script>
+import {
+  ChevronRightIcon,
+  ChevronDownIcon,
+  PlusIcon,
+} from 'satellite-lucide-icons'
+
+export default {
+  name: 'Group',
+  components: {
+    ChevronRightIcon,
+    ChevronDownIcon,
+    PlusIcon,
+  },
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['group', 'channels', 'setActiveChannel'],
+  data() {
+    return {
+      collapsed: false,
+    }
+  },
+  methods: {
+    /**
+     * @method toggleCollapse DocsTODO
+     * @description
+     * @example
+     */
+    toggleCollapse() {
+      this.collapsed = !this.collapsed
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less" src="./Group.less"></style>
