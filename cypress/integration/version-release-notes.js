44f161fa8cfee3f3abba6cfc26d6a7e8617de135
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2021-12-20 00:59:39 +0000

    test: check stuff on cypress plugin

--- cypress/integration/version-release-notes.js
+++ cypress/integration/version-release-notes.js
@@ -1,7 +1,18 @@
 it('Release notes appear when clicking on version number', () => {
   cy.visit('/')
+  cy.eyesOpen({
+    appName: 'Cypress test',
+    testName: 'Cypress test',
+  })
+
+  cy.eyesCheckWindow({
+    tag: 'Login Window',
+    target: 'window',
+    fully: true,
+  })
   cy.get('[data-cy=version]').click()
   cy.contains('Update')
   cy.contains('is Here!')
   cy.contains('Got It!').click()
+  cy.eyesClose()
 })
