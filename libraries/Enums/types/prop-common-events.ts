841d3fb21ce3ae262efac1a27da7788d449f172d
RetroPronghorn <retropronghorn@gmail.com>
2021-12-22 01:31:47 -0500

    feat(themes): started work on themes

--- libraries/Enums/types/prop-common-events.ts
+++ libraries/Enums/types/prop-common-events.ts
@@ -5,8 +5,11 @@ export enum PropCommonEnum {
     WAIT = "wait",
     FULFILLED = "fulfilled",
     CREATED_AT = "created_at",
+<<<<<<< HEAD
     TYPING = "TYPING",
     NOT_TYPING = "NOT_TYPING",
+=======
+>>>>>>> d23fdde0 (feat(themes): started work on themes)
 }
 
 export type PropCommon  = keyof typeof PropCommonEnum
