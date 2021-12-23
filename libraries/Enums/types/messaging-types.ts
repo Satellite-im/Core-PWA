0342060fa0460e3c553147a7c116b3cae1b2e46d
Eric Lee <eric.lee6174@gmail.com>
2021-12-23 02:33:44 -0700

    fix(reply): fix invalid message payload when reply

--- libraries/Enums/types/messaging-types.ts
+++ libraries/Enums/types/messaging-types.ts
@@ -1,6 +1,6 @@
 export enum MessagingTypesEnum {
-  REPLY = "reaction",
-  REACTION = "reply",
+  REPLY = "reply",
+  REACTION = "reaction",
 }
 
 export type MessagingType  = keyof typeof MessagingTypesEnum
