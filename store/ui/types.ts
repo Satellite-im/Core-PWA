fdd3ad24f8224b074ec419f88ba3b4b2c7cec2c2
Joe McGrath <33670767+josephmcg@users.noreply.github.com>
2021-12-28 05:20:19 +0900

    feat(glyphs): recently used glyphs functionality AP-341 (#769)

--- store/ui/types.ts
+++ store/ui/types.ts
@@ -1,3 +1,4 @@
+import { Glyph } from '~/types/ui/glyph'
 import { Channel } from '~/types/ui/server'
 
 export enum ThemeNames {
@@ -6,8 +7,8 @@ export enum ThemeNames {
 }
 
 export type Theme = {
-  name: ThemeNames,
-  class: String,
+  name: ThemeNames
+  class: String
 }
 
 export const Themes = [
@@ -54,9 +55,15 @@ export interface EnhancerInfo {
 }
 
 export interface EmojiUsage {
-  code: string,
-  count: number,
-  content: string,
+  code: string
+  count: number
+  content: string
+}
+
+export interface RecentGlyph {
+  pack: Glyph
+  url: string
+  count: number
 }
 
 export interface UIState {
@@ -94,10 +101,11 @@ export interface UIState {
     from: string
     payload: string
   }
-  recentReactions: Array<String>,
-  mostEmojiUsed: Array<EmojiUsage>,
+  recentReactions: Array<String>
+  mostEmojiUsed: Array<EmojiUsage>
+  recentGlyphs: Array<RecentGlyph>
   theme: {
-    base: Theme,
-    flair: String,
-  },
+    base: Theme
+    flair: String
+  }
 }
