183e291021a471be5efecbfa62595d19d5bdc28a
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2021-12-29 00:51:00 +0000

    chore: update

--- test/SearchUtil.test.ts
+++ test/SearchUtil.test.ts
@@ -36,31 +36,25 @@ describe("SearchUtil.default.getSearchOrderTypeList", () => {
 })
 
 describe("SearchUtil.default.filterSearchRecommendResult", () => {
-    /*test("0", () => {
-        let object: any = [{ name: undefined, address: undefined, avatar: undefined, key: "elio@example.com", value: "Elio" }]
-        let result: any = SearchUtil.default.filterSearchRecommendResult({ key1: object, key3: [], key0: [], key2: [] }, { command: search.SearchCommand.Mentions, value: "elio@example.com", index: 100, cursorStart: 2, cursorEnd: 1 })
-        expect(result).toMatchSnapshot()
-    })*/
-
-    test("1", () => {
+    test("0", () => {
         let object: any = [{ name: undefined, address: undefined, avatar: undefined, key: "elio@example.com", value: "elio@example.com" }, { name: undefined, address: undefined, avatar: undefined, key: "elio@example.com", value: "Elio" }, { name: undefined, address: undefined, avatar: undefined, key: "Dillenberg", value: "elio@example.com" }]
         let result: any = SearchUtil.default.filterSearchRecommendResult({ key1: object, key3: [], key0: [], key2: [] }, null)
         expect(result).toMatchSnapshot()
     })
 
-    test("2", () => {
+    test("1", () => {
         let object: any = [{ name: undefined, address: undefined, avatar: undefined, key: "Elio", value: "Dillenberg" }, { name: undefined, address: undefined, avatar: undefined, key: "Dillenberg", value: "Elio" }, { name: undefined, address: undefined, avatar: undefined, key: "Dillenberg", value: "Elio" }]
         let result: any = SearchUtil.default.filterSearchRecommendResult({ key1: object, key3: [], key0: [], key2: [] }, null)
         expect(result).toMatchSnapshot()
     })
 
-    test("3", () => {
+    test("2", () => {
         let object: any = [{ name: undefined, address: undefined, avatar: undefined, key: "elio@example.com", value: "elio@example.com" }]
         let result: any = SearchUtil.default.filterSearchRecommendResult({ key1: object, key3: [], key0: [], key2: [] }, null)
         expect(result).toMatchSnapshot()
     })
 
-    test("4", () => {
+    test("3", () => {
         let object: any = [{ name: "Jean-Philippe", address: "192.168.1.5", avatar: "https://cdn.fakercloud.com/avatars/joannefournier_128.jpg", key: "Elio", value: "Elio" }, { name: "Edmond", address: "192.168.1.5", avatar: "https://cdn.fakercloud.com/avatars/akmalfikri_128.jpg", key: "Dillenberg", value: "Elio" }, { name: "Edmond", address: "192.168.1.5", avatar: "https://cdn.fakercloud.com/avatars/akmalfikri_128.jpg", key: "Elio", value: "Elio" }, { name: "Edmond", address: "0.0.0.0", avatar: "https://cdn.fakercloud.com/avatars/weavermedia_128.jpg", key: "Dillenberg", value: "elio@example.com" }, { name: "Edmond", address: "192.168.1.5", avatar: "https://cdn.fakercloud.com/avatars/joannefournier_128.jpg", key: "Dillenberg", value: "Elio" }]
         let result: any = SearchUtil.default.filterSearchRecommendResult({ key0: object, key2: [], key4: [], key3: [], key1: [] }, null)
         expect(result).toMatchSnapshot()
@@ -102,31 +96,25 @@ describe("SearchUtil.default.filterSearchRecommendResult", () => {
         expect(result).toMatchSnapshot()
     })
 
-    /*test("1", () => {
-        let object: any = [{ name: "Pierre Edouard", address: "0.0.0.0", avatar: "https://cdn.fakercloud.com/avatars/zvchkelly_128.jpg", key: "Dillenberg", value: "Elio" }, { name: "Pierre Edouard", address: "192.168.1.5", avatar: "https://cdn.fakercloud.com/avatars/weavermedia_128.jpg", key: "Dillenberg", value: "elio@example.com" }, { name: "Anas", address: "0.0.0.0", avatar: "https://cdn.fakercloud.com/avatars/joannefournier_128.jpg", key: "Elio", value: "Dillenberg" }]
-        let result: any = SearchUtil.default.filterSearchRecommendResult({ key1: object, key0: [] }, { command: search.SearchCommand.After, value: "elio@example.com", index: 0, cursorStart: 0, cursorEnd: 100 })
-        expect(result).toMatchSnapshot()
-    })*/
-
-    test("2", () => {
+    test("1", () => {
         let object: any = [{ name: "Edmond", address: "0.0.0.0", avatar: undefined, key: "Elio", value: "elio@example.com" }, { name: "Michael", address: "0.0.0.0", avatar: undefined, key: "elio@example.com", value: "Elio" }]
         let result: any = SearchUtil.default.filterSearchRecommendResult({ key0: object, key2: [], key4: [], key3: [], key1: [] }, null)
         expect(result).toMatchSnapshot()
     })
 
-    test("3", () => {
+    test("2", () => {
         let object: any = [{ name: "George", address: "192.168.1.5", avatar: undefined, key: "Elio", value: "elio@example.com" }, { name: "Anas", address: "192.168.1.5", avatar: undefined, key: "Dillenberg", value: "Dillenberg" }]
         let result: any = SearchUtil.default.filterSearchRecommendResult({ key0: object, key2: [], key4: [], key3: [], key1: [] }, null)
         expect(result).toMatchSnapshot()
     })
 
-    test("4", () => {
+    test("3", () => {
         let object: any = [{ name: "Edmond", address: "192.168.1.5", avatar: "https://cdn.fakercloud.com/avatars/akmalfikri_128.jpg", key: "elio@example.com", value: "Elio" }, { name: "George", address: "0.0.0.0", avatar: "https://cdn.fakercloud.com/avatars/zvchkelly_128.jpg", key: "elio@example.com", value: "Dillenberg" }, { name: "Pierre Edouard", address: "192.168.1.5", avatar: "https://cdn.fakercloud.com/avatars/zvchkelly_128.jpg", key: "Elio", value: "elio@example.com" }]
         let result: any = SearchUtil.default.filterSearchRecommendResult({ key1: object, key0: [] }, null)
         expect(result).toMatchSnapshot()
     })
 
-    test("5", () => {
+    test("4", () => {
         let result: any = SearchUtil.default.filterSearchRecommendResult({ key0: [], key2: [], key4: [], key3: [], key1: [] }, null)
         expect(result).toMatchSnapshot()
     })
