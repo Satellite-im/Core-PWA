428e8886ca12c175675b1e8129567a03e7997e74
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2021-12-30 14:11:08 +0000

    Update StoreChatMutations.test.ts

--- test/StoreChatMutations.test.ts
+++ test/StoreChatMutations.test.ts
@@ -42,7 +42,6 @@ describe("mutations.default.setChatReply", () => {
     })
 })
 
-
 describe("mutations.default.setChatText", () => {
     test("0", () => {
         let object2: any = [{ userId: "12345", value: "elio@example.com" }, { userId: "9876", value: "Elio" }]
