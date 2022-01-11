0fee1a333567b910e115948c92286127e2cfd331
Chris Hogan <chris@hogan.io>
2022-01-11 16:05:49 -0700

    update(settings): update keybinds functionality AP-174 (#976)

---
+++ libraries/Enums/types/modifierKeys.ts
@@ -0,0 +1,10 @@
+export enum ModifierKeysEnum {
+    SHIFT = 'shift', 
+    CONTROL = 'control', 
+    ALT = 'alt', 
+    META = 'meta', 
+    TAB = 'tab', 
+    CAPSLOCK = 'capslock'
+}
+
+export type ModifierKeys = keyof typeof ModifierKeysEnum
