ba14d90ee219932a137ff051ec5c8e33c78c0335
drepram <caxvis@gmail.com>
2022-01-24 14:35:17 +0700

    add(jest): 100% coverage for logger unit test

--- utilities/Logger.test.ts
+++ utilities/Logger.test.ts
@@ -1,5 +1,5 @@
 import Logger from "../utilities/Logger"
-import { expect } from '@jest/globals'
+global.console.log = jest.fn()
 
 describe('Test class Logger', () => {
 
@@ -14,6 +14,26 @@ const level1 = undefined!
 // Method call
 const logger = new Logger(debug1)
  logger.log(tag1, desc1, data1, level1)
+
+ expect(console.log).toBeCalledTimes(1)
+})
+it('Logger-log with data', () => {
+// Arguments
+const debug1 = undefined!
+const tag1 = 'Oha'
+const desc1 = 'Oha'
+const data1 = {
+  peerId: "abc123",
+  typingState: "NOT_TYPING"
+}
+const level1 = undefined!
+
+// Method call
+const logger = new Logger(debug1)
+ logger.log(tag1, desc1, data1, level1)
+
+ expect(console.log).toBeCalledTimes(2) // Is called two times because the script will log the data if the data argument is not an empty object
+ expect(console.log).toBeCalledWith(data1)
 })
 
 it('Logger-debug', () => {
