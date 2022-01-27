86a13a1059722e0ae511c8b6e74894299258b662
Nick <81814644+nickjjang@users.noreply.github.com>
2022-01-28 00:04:27 +0900

    feat(profileModal): make reusable social account component (#1144)

---
+++ components/views/user/profile/SocialAccount/SocialAccount.vue
@@ -0,0 +1,63 @@
+<template src="./SocialAccount.html"></template>
+
+<script lang="ts">
+import {
+  GithubIcon,
+  FacebookIcon,
+  LinkedinIcon,
+  TwitterIcon,
+  YoutubeIcon,
+  VerifiedIcon,
+  ExternalLinkIcon,
+} from 'satellite-lucide-icons'
+import Vue, { PropType } from 'vue'
+import { SocialAccountTypesEnum } from '~/libraries/Enums/enums'
+
+export default Vue.extend({
+  components: {
+    GithubIcon,
+    FacebookIcon,
+    LinkedinIcon,
+    TwitterIcon,
+    YoutubeIcon,
+    VerifiedIcon,
+    ExternalLinkIcon,
+  },
+  props: {
+    fullName: {
+      type: String,
+      default: '',
+    },
+    accountType: {
+      type: String as PropType<SocialAccountTypesEnum>,
+      default: SocialAccountTypesEnum.FACEBOOK,
+    },
+    username: {
+      type: String,
+      default: '',
+    },
+    profileUrl: {
+      type: String,
+      default: '',
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      SocialAccountTypesEnum,
+      socialIcons: {
+        [SocialAccountTypesEnum.GITHUB]: GithubIcon,
+        [SocialAccountTypesEnum.FACEBOOK]: FacebookIcon,
+        [SocialAccountTypesEnum.LINKEDIN]: LinkedinIcon,
+        [SocialAccountTypesEnum.TWITTER]: TwitterIcon,
+        [SocialAccountTypesEnum.YOUTUBE]: YoutubeIcon,
+      },
+    }
+  },
+})
+</script>
+
+<style scoped lang="less" src="./SocialAccount.less"></style>
