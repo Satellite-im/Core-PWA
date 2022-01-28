de0049dbb141374ebb076621cef143203c9c37d3
Baymax <david.lee4015@gmail.com>
2022-01-27 18:11:17 -0700

    fix(sounds): change direct $sounds.playsound to vuex action call

---
+++ store/sounds/actions.ts
@@ -0,0 +1,17 @@
+import SoundManager, { Sounds } from '~/libraries/SoundManager/SoundManager'
+import { ActionsArguments } from '~/types/store/store'
+import { SoundsState } from './types'
+
+const $Sounds = new SoundManager()
+
+export default {
+  /**
+   * @method playSound
+   * @description play sound with its type
+   * @example playSound(Sounds.CALL)
+   */
+  playSound({ state }: ActionsArguments<SoundsState>, call: Sounds) {
+    const status = state[call]
+    if (status) $Sounds.playSound(call)
+  },
+}
