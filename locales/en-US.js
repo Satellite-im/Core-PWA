1c140b096c4acf555fd0cdeff2b8952cd645342a
Jeff Kristian <jekrimo@gmail.com>
2022-02-01 13:57:43 -0700

    chore(locales): remove locales submodule (#1183)

---
+++ locales/en-US.js
@@ -0,0 +1,591 @@
+export default {
+  global: {
+    name: 'Satellite.im',
+    encrypted: 'Encrypted',
+    unencrypted: 'Unencrypted',
+    upload: 'Upload',
+    upload_files: 'Upload Files',
+  },
+  ui: {
+    talk: 'Speak Freely...',
+    close: 'Close',
+    cancel: 'Cancel',
+    search: 'Search',
+    home: 'Home',
+    copied: 'ATTN: Copied to clipboard.',
+    unpin: 'Unpin',
+    send: 'Send',
+    emotes: 'Emoji',
+    glyphs: 'Glyphs',
+    fullscreen: 'Fullscreen',
+    exit_fullscreen: 'Exit fullscreen',
+    more: 'More',
+    are: 'are',
+    is: 'is',
+    live: 'Live',
+    edited: 'edited',
+  },
+  wallet: {
+    wallet: 'Wallet',
+    send_money: 'Send Money',
+    recent_transactions: 'Recent Transactions',
+    accounts: {
+      linked_accounts: 'Linked Accounts',
+      link_new_account_or_device: 'Link New Account / Device',
+      require_pin_auth: 'Require Pin Auth?',
+      security_suggestion:
+        'This is suggested for all users to increase financial security',
+    },
+    balance: {
+      balance: 'Balance',
+      add_funds: 'Add Funds',
+      cash_out: 'Cash Out',
+    },
+    mini_wallet: {
+      details: 'Details',
+      send_money: 'Send Money',
+      request_money: 'Request Money',
+      to: 'To: ',
+      from: 'From: ',
+      amount: 'Amount: ',
+      currency: 'Currency: ',
+      txid: 'TXID: ',
+    },
+  },
+  messaging: {
+    messages: 'Messages',
+    groups: 'Groups',
+    pin: 'Pin',
+    reply: 'Reply',
+    typing: 'typing',
+    many_typing: 'many users are typing',
+    new_messages: 'New Messages',
+    pinned: {
+      archived_messages: 'Archived Messages',
+      new: 'New',
+      old: 'Old',
+      files: 'Files',
+    },
+    edit: {
+      escape_to: 'escape to ',
+      cancel: 'cancel',
+      enter_to: ' • enter to ',
+      save: 'save',
+    },
+  },
+  controls: {
+    copy_id: 'Copy ID',
+    share_link: 'Share Link',
+    download: 'Download',
+    mic: 'Mic',
+    headphones: 'Audio',
+    video: 'Video',
+    call: 'Call',
+    mute: 'Mute',
+    toggle: 'Toggle',
+    deafen: 'Deafen',
+    crop: 'Crop',
+    edit: 'Edit',
+    end_stream: 'End Stream',
+    not_connected: 'Offline calling unavailable',
+    not_available: 'Not available yet',
+  },
+  popups: {
+    error: {
+      text: 'Text',
+      title: 'Error',
+      user_agent: 'User Agent',
+      source: 'Source',
+      line: 'Line',
+      confirm: 'Confirm',
+      contact:
+        'Oops! Something went wrong! Help us improve your experience by sending an error report',
+    },
+  },
+  files: {
+    files: 'Files',
+    view_original: 'View Original',
+  },
+  pages: {
+    files: {
+      refresh: 'Refresh',
+      new_folder: 'New Folder',
+      new_folder_prompt: 'New Folder:',
+      file_type: 'File Type',
+      browse: {
+        files: 'Files',
+        search: 'Search...',
+        name: 'Name',
+        modified: 'Modified',
+        type: 'Type',
+        encrypted: 'Encrypted',
+        size: 'Size',
+      },
+      aside: {
+        free_tier: 'Free Tier',
+        upgrade: 'Upgrade',
+        quick_access: 'Quick Access',
+        shared_items: 'Shared Items',
+      },
+      upload: {
+        close: 'Close',
+        scanning_image: 'Scanning Image...',
+        send: 'Send',
+        cancel: 'Cancel',
+      },
+    },
+    unlock: {
+      decrypt: 'Decrypt Account',
+      create: 'Create Account Pin',
+      placeholder: 'Enter Pin...',
+      store_pin: 'Store Pin? (Less Secure)',
+    },
+    loading: {
+      loading: 'Linking Satellites...',
+    },
+    disclaimer: {
+      title: 'Account Creation',
+      subtitle:
+        "We're going to create an account for you. On the next screen, you'll see a set of words. Screenshot this or write it down. This is the only way to backup your account.",
+      create: 'Create Account',
+      or: 'Or',
+      import: 'Import Account',
+    },
+    inputAccount: {
+      title: 'Import Account',
+      subtitle:
+        'Enter your 12 word passphrase in exactly the same order your recovery seed was generated.',
+      enter: 'Enter Passphrase',
+      recover: 'Recover Account',
+    },
+    phrase: {
+      title: 'Recovery Seed',
+      done: 'I Saved It',
+      write_down:
+        'Write this down in the order that they appear here. Having the correct order is very important when you are recovering your account.',
+    },
+    privacy: {
+      title: 'Privacy Settings',
+      subtitle:
+        'Choose which features to enable to best suit your privacy preferences.',
+      continue: 'Continue',
+      register: {
+        title: 'Register Username Publicly',
+        subtitle:
+          'Publicly associate your account ID with a human readable username. Anyone can see this association.',
+      },
+      pin: {
+        title: 'Store Account Pin',
+        subtitle:
+          "Store your account pin locally so you don't have to enter it manually every time. This is not recommended.",
+      },
+      activity: {
+        title: 'Display Current Activity',
+        subtitle:
+          "Allow Satellite to see what games you're playing and show them off on your profile so friends can jump in.",
+      },
+      embeds: {
+        title: 'Enable External Embeds',
+        subtitle:
+          'Allow Satellite to fetch data from external sites in order to expand links like Spotify, YouTube, and more.',
+      },
+    },
+    settings: {
+      settings: 'Settings',
+      motion: 'Motion',
+      never: 'Never',
+      always: 'Always',
+      aspect_ratio_label: 'Aspect Ratio Off?',
+      app_info: 'Application Info',
+      info: {
+        title: 'Application Info',
+        subtitle:
+          'Below is information that may be helpful to you when creating bug reports or developing on our application.',
+      },
+      changelog: 'Changelog',
+      developer: {
+        title: 'Developer Settings',
+        subtitle:
+          'Mock data and tweak settings for testing. This page is really just for internal use.',
+        identifier: 'Identifier',
+      },
+      notifications: {
+        title: 'Notifications Settings',
+        notes:
+          'Notifications work different on every platform. On Web, you need to request notifications. On Desktop and Android notifications are enabled by default.',
+        enabled: 'Enable or Disable Notifications',
+        labels: {
+          current_platform: 'Your current platform is:',
+        },
+        web: {
+          granted:
+            'In the web version of Satellite, you need to disable notifications in your browser for this site.',
+          denied:
+            "We cannot re-prompt you for notification permission in the web version of Satellite. You need to set notifications for this site to 'ask' or 'allow'",
+        },
+        send_label: 'Send Notification',
+        send_placeholder: 'Content you want to appear in local notification',
+        sounds: {
+          title: 'Sounds',
+          message: 'Message',
+          call: 'Call',
+          hangup: 'Hang Up',
+          mute: 'Mute',
+          unmute: 'Unmute',
+          deafen: 'Deafen',
+          undeafen: 'Undeafen',
+          upload: 'Upload',
+          connected: 'Connected',
+        },
+      },
+      audio: {
+        title: 'Audio',
+        sources: {
+          title: 'Audio Sources',
+          subtitle: 'Input and Output devices you want Satellite to use.',
+          permissionButtonText: 'Request Permission',
+          permissionRequestMessage: '',
+          permissionDeniedMessage:
+            "You have denied microphone access so we can't see what devices you have. Please allow microphone access in your browser.",
+          browserDoesNotSupportAudioOutChange:
+            'Your Browser does not allow setting audio out, so the default is used here. To change this, use a chrome based browser or update your default audio out in your OS.',
+          input: {
+            title: 'Audio Input',
+            subtitle:
+              "Select which input device you'd like people to hear your silky smooth voice from.",
+          },
+          output: {
+            title: 'Audio Output',
+            subtitle:
+              "Select the device that you'd like to deliver sound to your ear holes with.",
+          },
+        },
+        bitrate: {
+          title: 'Audio Bitrate',
+          subtitle:
+            'Higher bitrates will transmit better quality audio, lowering the bitrate can help with slower connection speeds.',
+        },
+        sampleSize: {
+          title: 'Sample Size',
+          subtitle:
+            'A higher sample size will give you a broader dynamic range of audio. Higher samples require better connections.',
+        },
+        volume: {
+          title: 'Output Volume',
+        },
+        inputVolume: {
+          title: 'Input Volume',
+        },
+        systemVolume: {
+          title: 'System Volume',
+        },
+        echo: {
+          title: 'Echo Cancellation',
+          subtitle:
+            'Enable to help cancel out some slight echo & feedback from your mic.',
+        },
+        noise: {
+          title: 'Noise Suppression',
+          subtitle: 'Enable to help hide background noise in your environment.',
+        },
+      },
+      video: {
+        title: 'Video',
+        sources: {
+          permissionDeniedMessage:
+            "You have denied camera access so we can't see what devices you have. Please allow camera access in your browser.",
+          input: {
+            title: 'Video Input',
+            subtitle:
+              "Select which video device you'd like to share your beautiful smile from.",
+          },
+        },
+      },
+      screen: {
+        title: 'Screen Share',
+        captureMouse: {
+          title: 'Capture Mouse',
+          subtitle: 'Would you like to share your mouse in screen shares?',
+        },
+      },
+      keybinds: {
+        title: 'Default Keybinds',
+        subtitle:
+          'A list of the default keybinds for quickly navigating and using Satellite.',
+        mute: 'Toggle Mute',
+        settings: 'Open Settings',
+        deafen: 'Toggle Deafen',
+        call: 'Call Active Chat',
+        clear: 'Clear',
+        save: 'Save',
+        cancel: 'Cancel',
+        reset_all: 'Reset All Keybinds',
+        systemHotkeyError:
+          'That is browser/system shortcut. Please input other keys.',
+        existHotkeyError: 'Key already bound',
+        modifierHotkeyError:
+          'Modifiers (Shift, Tab, Option, etc.) Must come before alphanumerics',
+        editHotkeyError: 'Character Not Allowed',
+      },
+      accounts: {
+        title: 'Accounts & Devices',
+        subtitle:
+          "Select with account you'd prefer to default transactions from.",
+        active: 'Active Account',
+        gas_price: 'Transaction Fee Limit',
+        devices: 'Connected Devices',
+        no_devices: 'No connected devices found.',
+        no_status: 'No status set.',
+      },
+      personalize: {
+        flair: 'Flair',
+        title: 'Personalize Satellite',
+        subtitle: 'Make it your own and choose custom colors & themes.',
+        theme: 'Color Theme',
+        language: 'Language',
+      },
+      realms: {
+        title: 'Realms',
+        subtitle:
+          "Change the realm Satellite lives in. Changing a realm will mean you can only communicate with others in the same realm as you. Please only change if you know what you're doing.",
+        chain: 'Realm',
+      },
+      network: {
+        title: 'Network',
+        subtitle:
+          "Change network options, please note that switching from 'Testnet' to 'Mainnet' can trigger errors and additional charges during the alpha period.",
+        network: 'Network',
+        allow_embeds: 'Allow Embeds',
+        allow_embeds_description:
+          'Have Satellite try to load videos and link previews. This sends a request to the URL someone shares with you to get more information/embed the content from the source and may allow the site to track you.',
+      },
+      storage: {
+        title: 'Storage',
+        subtitle:
+          'Control how your data is stored. You can export your local storage information as well as reset Satellite here. Be careful resetting your local storage will clear out your account.',
+        clear: {
+          title: 'Clear Storage',
+          subtitle:
+            'Reset Satellite. This will clear any saved accounts. Do not do this without backing up your account first.',
+          button: 'Clear Local Storage',
+        },
+        export: {
+          title: 'Export Storage',
+          subtitle:
+            "Export your local storage, it's your data you can do whatever you'd like with it.",
+          button: 'Export Local Storage',
+        },
+      },
+      profile: {
+        title: 'Profile',
+        subtitle: 'Update your profile photo, status, banners and more.',
+        info: {
+          title: 'Account Info',
+          subtitle:
+            'Below is a list of helpful information regarding your account.',
+          accountID: 'Account Identifier',
+          badges: 'Badges',
+          preferences: 'Preferences',
+          location: 'Location',
+          selectLocation: 'Select Location',
+          language: 'Language',
+          selectLanguage: 'Select Language',
+        },
+        phrase: {
+          title: 'Recovery Phrase',
+          subtitle:
+            'Do not share this phrase with anyone. This phrase is used to recover your account. Anyone with access to this has access to your account.',
+        },
+      },
+    },
+    chat: {
+      new_message: 'messages',
+      jump_to_current: 'View New Messages',
+      chat_now: 'chat now',
+      add_member: 'Add Member +',
+      switch_chat: 'Switch Chat',
+      start_watch_party: 'Watch Party',
+      new_chat: 'New Chat',
+      new_chat_description:
+        'Select one or more of your friends to chat in groups, or one-on-one.',
+      no_friends_yet: 'No Friends Yet',
+      no_friends_yet_text: "The world doesn't have to be so lonely.",
+      enhancers: {
+        glyphs: 'Glyphs',
+        emoji: 'Emoji',
+        gifs: 'Gifs',
+        search: 'Search...',
+      },
+    },
+  },
+  servers: {
+    new_server: 'New Community',
+    create: {
+      heading: 'Create a server',
+      photo_text:
+        "Give your server a face. Choose a memorable photo to show it's members.",
+      photo_button: 'Set photo',
+      server_name: 'Enter a server name',
+      server_name_placeholder: 'Server name...',
+      create_server: 'Create server',
+      select_friends: 'Invite your friends to this server',
+      server_name_error: 'Server name must be at least 5 characters.',
+      select_friends_placeholder: 'Search friends...',
+    },
+  },
+  conversation: {
+    encrypted: 'Messages are secured by end to end encryption.',
+    reply: 'reply | replies',
+    reply_to: 'Reply to',
+    multimedia: 'Multimedia content',
+    collapse: 'Collapse',
+  },
+  errors: {
+    accounts: {
+      pin_too_short: 'Pin must be at least 5 characters.',
+      invalid_pin: 'Pin does not match',
+      user_derivation_failed:
+        'We were unable to verify your passphrase. Please check it and try again.',
+      mnemonic_not_present: 'Problem with passphrase, please try again.',
+      file_too_large:
+        'File is too large, please upload a file smaller than 8MB.',
+    },
+    friends: {
+      request_already_sent: 'You have already sent a request to this user',
+      request_already_accepted: 'This user is already in your friends',
+      friend_info_not_found: 'Seems that this user is not registered',
+      textile_not_initialized: 'Your account is not ready. Try later',
+    },
+    chat: {
+      drop_file_count: 'Sorry, you can only upload 8 files at a time',
+      unable_preview: 'Unable to preview file',
+      upload_blocked: 'Upload Blocked',
+      contains_nsfw: 'Unable to upload file/s due to NSFW status',
+    },
+    textile: {
+      friend_not_found: 'Friend not found',
+      mailbox_manager_not_found: 'Mailbox manager not found',
+      mailbox_manager_not_initialized: 'Mailbox manager not initialized',
+    },
+  },
+  search: {
+    search: 'Search... ',
+    input: {
+      search_options: 'SEARCH OPTIONS',
+      search_for: 'SEARCH FOR',
+    },
+    result: {
+      search_results: 'Search results',
+      more_filters: 'More filters',
+      results: 'Results',
+      users: 'Users',
+      conversations: 'Conversations',
+      select_date: 'Select Date',
+    },
+  },
+  media: {
+    settings: {
+      audio_quality: 'Audio Quality',
+      bitrate: 'Bitrate',
+      sample_size: 'Sample size',
+      video_quality: 'Video Quality',
+      no_options: 'No options',
+      stream_options: 'Stream Options',
+    },
+  },
+  user: {
+    registration: {
+      create: 'Registration',
+      sign_in: 'Sign in',
+      photo_text:
+        'Customize how the world sees you, choose something memorable.',
+      username: 'Username',
+      username_placeholder: 'Neil Spaceman...',
+      username_error: 'Username must be at least 5 characters.',
+      status: 'Status',
+      status_placeholder: 'Ready for launch...',
+      reg_status: {
+        unknown: 'Not registered',
+        in_progress: "We're transporting your data to the Realm...",
+        funding_account: 'Making contact, requesting entry to Realm...',
+        sending_transaction: 'Request Approved! Now entering the Realm...',
+        registered: 'Grats! Your a brand new citizen on the Realm.',
+      },
+    },
+    loading: {
+      loading_account: 'Aligning satellites to retrieve your account...',
+    },
+  },
+  friends: {
+    friends: 'Friends',
+    self_add: "You can't add yourself you silly goose.",
+    already_friend: "You're already friends with this user.",
+    not_found: "Hmm, we couldn't find a user at that address",
+    invalid_id: 'Invalid account ID',
+    request_sent: 'Friend request successfully sent!',
+    requests: 'Friends request',
+    blocked: 'Blocked friends',
+    search_placeholder: 'Some User...',
+    add: 'Add Friend',
+    add_description:
+      "Enter your friend's account ID or nickname. Account IDs are case sensitive.",
+    add_via_qr: 'Add Via QR',
+    add_qrcode_description:
+      'You can also add a friend by having them scan your QR code, or by scanning theirs.',
+    scan_code: 'Scan a Friend Code',
+    camera_scan: 'Allow Camera Scan',
+    friend_code: 'Your Friend Code',
+    accept: 'Accept',
+    decline: 'Decline',
+    unblock: 'Unblock',
+    send: 'Send',
+    message: 'Message',
+    options: 'Options',
+  },
+  market_place: {
+    title: 'Marketplace',
+    glyphs: 'Glyphs',
+    themes: 'Themes',
+    nfts: 'NFTs',
+  },
+  glyphs: {
+    view_pack: 'View Glyph Pack',
+    back: 'Back',
+    new: 'New',
+    sale: 'Sale',
+    shop_all: 'Shop All Glyphs',
+    shop_now: 'Shop Now',
+    trending: 'Trending',
+    try: 'Try using some glyphs',
+  },
+  alerts: {
+    caught_up: "You're all caught up!",
+  },
+  modal: {
+    update_modal: {
+      update_required:
+        "This update requires a refresh of the Alpha application. Click 'Update Now' below to start fresh on the new update.",
+      got_it: 'Got It!',
+    },
+    profile: {
+      about: {
+        tab: 'About',
+        me: 'About Me',
+        location: 'Location',
+        add_note: 'Add Note',
+        click_note: 'Click to add note',
+      },
+      accounts: 'Accounts',
+      activity: {
+        tab: 'Activity',
+        spotify: 'Listening to Spotify',
+        currently: 'Currently Playing',
+      },
+      mutual: {
+        tab: 'Mutual',
+        servers: 'Servers',
+        friends: 'Friends',
+      },
+    },
+  },
+}
