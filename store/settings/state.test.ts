ab56eedd10d1683c8b0c66e3488761109a1a3af0
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-02-08 15:11:39 +0000

    chore(jest): delete test (#1384)

--- store/settings/state.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from '@jest/globals'
-import * as state from '~/store/settings/state'
-
-describe('state.default', () => {
-  test('0', () => {
-    const result: any = state.default()
-    expect(result).toMatchSnapshot()
-  })
-})
