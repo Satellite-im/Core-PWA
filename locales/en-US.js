de1e3d97242c29814da56407c9f83717c0b385e5
Chris Hogan <chris@hogan.io>
2022-02-09 16:16:19 -0700

    fixing text

--- locales/en-US.js
+++ locales/en-US.js
@@ -371,8 +371,9 @@ export default {
           title: 'Clear Storage',
           subtitle:
             'Reset Satellite. This will clear any saved accounts. Do not do this without backing up your account first.',
-          subtitle_warning:
-            'Are you absolutely sure? If you do not have your recovery seed youraccount will be gone forever?',
+          subtitle_warning1: 'Are you absolutely sure?',
+          subtitle_warning2:
+            'If you do not have your recovery seed your account will be gone forever?',
           button: 'Clear Local Storage',
           confirm_button: 'Yes, Really, Clear Local Storage',
           message: 'Local storage cleared successfully',
