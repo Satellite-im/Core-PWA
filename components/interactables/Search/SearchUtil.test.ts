fe725ea459d6e9fbef4021f23df34e60d656e321
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-02-13 21:16:04 +0000

    add(jest): add and update tests (#1470)
    * add(jest): add and update tests
    
    * add(jest): linting + update snapshots
    
    * add(jest): linting + update snapshots

--- components/interactables/Search/SearchUtil.test.ts
+++ components/interactables/Search/SearchUtil.test.ts
@@ -1,303 +1,147 @@
-import SearchUtil from './SearchUtil'
+import * as SearchUtil from '~/components/interactables/Search/SearchUtil'
+import * as search from '~/types/search/search'
 
-describe('SearchUtil.getTextCommandMap', () => {
+describe('SearchUtil.default.getTextCommandMap', () => {
   test('0', () => {
-    const result: any = SearchUtil.getTextCommandMap()
+    let result: any = SearchUtil.default.getTextCommandMap()
     expect(result).toMatchSnapshot()
   })
 })
 
-describe('SearchUtil.getCommandTypeParams', () => {
+describe('SearchUtil.default.getCommandTypeParams', () => {
   test('0', () => {
-    const result: any = SearchUtil.getCommandTypeParams()
+    let result: any = SearchUtil.default.getCommandTypeParams()
     expect(result).toMatchSnapshot()
   })
 })
 
-describe('SearchUtil.getCommandMetaList', () => {
+describe('SearchUtil.default.getCommandMetaList', () => {
   test('0', () => {
-    const result: any = SearchUtil.getCommandMetaList()
+    let result: any = SearchUtil.default.getCommandMetaList()
     expect(result).toMatchSnapshot()
   })
 })
 
-describe('SearchUtil.getSearchResultGroupList', () => {
+describe('SearchUtil.default.getSearchResultGroupList', () => {
   test('0', () => {
-    const result: any = SearchUtil.getSearchResultGroupList()
+    let result: any = SearchUtil.default.getSearchResultGroupList()
     expect(result).toMatchSnapshot()
   })
 })
 
-describe('SearchUtil.getSearchOrderTypeList', () => {
+describe('SearchUtil.default.getSearchOrderTypeList', () => {
   test('0', () => {
-    const result: any = SearchUtil.getSearchOrderTypeList()
+    let result: any = SearchUtil.default.getSearchOrderTypeList()
     expect(result).toMatchSnapshot()
   })
 })
 
-describe('SearchUtil.filterSearchRecommendResult', () => {
+describe('SearchUtil.default.getCommandMeta', () => {
   test('0', () => {
-    const object: any = [
-      {
-        name: undefined,
-        address: undefined,
-        avatar: undefined,
-        key: 'elio@example.com',
-        value: 'elio@example.com',
-      },
-      {
-        name: undefined,
-        address: undefined,
-        avatar: undefined,
-        key: 'elio@example.com',
-        value: 'Elio',
-      },
-      {
-        name: undefined,
-        address: undefined,
-        avatar: undefined,
-        key: 'Dillenberg',
-        value: 'elio@example.com',
-      },
-    ]
-    const result: any = SearchUtil.filterSearchRecommendResult(
-      { key1: object, key3: [], key0: [], key2: [] },
-      null,
+    let result: any = SearchUtil.default.getCommandMeta(
+      search.SearchCommand.Before,
     )
     expect(result).toMatchSnapshot()
   })
 
   test('1', () => {
-    const object: any = [
-      {
-        name: undefined,
-        address: undefined,
-        avatar: undefined,
-        key: 'Elio',
-        value: 'Dillenberg',
-      },
-      {
-        name: undefined,
-        address: undefined,
-        avatar: undefined,
-        key: 'Dillenberg',
-        value: 'Elio',
-      },
-      {
-        name: undefined,
-        address: undefined,
-        avatar: undefined,
-        key: 'Dillenberg',
-        value: 'Elio',
-      },
-    ]
-    const result: any = SearchUtil.filterSearchRecommendResult(
-      { key1: object, key3: [], key0: [], key2: [] },
-      null,
-    )
-    expect(result).toMatchSnapshot()
-  })
-
-  test('2', () => {
-    const object: any = [
-      {
-        name: undefined,
-        address: undefined,
-        avatar: undefined,
-        key: 'elio@example.com',
-        value: 'elio@example.com',
-      },
-    ]
-    const result: any = SearchUtil.filterSearchRecommendResult(
-      { key1: object, key3: [], key0: [], key2: [] },
-      null,
-    )
-    expect(result).toMatchSnapshot()
-  })
-
-  test('3', () => {
-    const object: any = [
-      {
-        name: 'Jean-Philippe',
-        address: '192.168.1.5',
-        avatar: 'https://cdn.fakercloud.com/avatars/joannefournier_128.jpg',
-        key: 'Elio',
-        value: 'Elio',
-      },
-      {
-        name: 'Edmond',
-        address: '192.168.1.5',
-        avatar: 'https://cdn.fakercloud.com/avatars/akmalfikri_128.jpg',
-        key: 'Dillenberg',
-        value: 'Elio',
-      },
-      {
-        name: 'Edmond',
-        address: '192.168.1.5',
-        avatar: 'https://cdn.fakercloud.com/avatars/akmalfikri_128.jpg',
-        key: 'Elio',
-        value: 'Elio',
-      },
-      {
-        name: 'Edmond',
-        address: '0.0.0.0',
-        avatar: 'https://cdn.fakercloud.com/avatars/weavermedia_128.jpg',
-        key: 'Dillenberg',
-        value: 'elio@example.com',
-      },
-      {
-        name: 'Edmond',
-        address: '192.168.1.5',
-        avatar: 'https://cdn.fakercloud.com/avatars/joannefournier_128.jpg',
-        key: 'Dillenberg',
-        value: 'Elio',
-      },
-    ]
-    const result: any = SearchUtil.filterSearchRecommendResult(
-      { key0: object, key2: [], key4: [], key3: [], key1: [] },
-      null,
+    let result: any = SearchUtil.default.getCommandMeta(
+      search.SearchCommand.Empty,
     )
     expect(result).toMatchSnapshot()
   })
 })
 
-describe('SearchUtil.getCommandMetaList', () => {
+describe('SearchUtil.default.getCommandTypeParam', () => {
   test('0', () => {
-    const result: any = SearchUtil.getCommandMetaList()
-    expect(result).toMatchSnapshot()
-  })
-})
-
-describe('SearchUtil.getTextCommandMap', () => {
-  test('0', () => {
-    const result: any = SearchUtil.getTextCommandMap()
-    expect(result).toMatchSnapshot()
-  })
-})
-
-describe('SearchUtil.getSearchResultGroupList', () => {
-  test('0', () => {
-    const result: any = SearchUtil.getSearchResultGroupList()
-    expect(result).toMatchSnapshot()
-  })
-})
-
-describe('SearchUtil.getSearchOrderTypeList', () => {
-  test('0', () => {
-    const result: any = SearchUtil.getSearchOrderTypeList()
-    expect(result).toMatchSnapshot()
-  })
-})
-
-describe('SearchUtil.filterSearchRecommendResult', () => {
-  test('0', () => {
-    const object: any = [
-      {
-        name: 'Michael',
-        address: '192.168.1.5',
-        avatar: undefined,
-        key: 'Elio',
-        value: 'Dillenberg',
-      },
-      {
-        name: 'Jean-Philippe',
-        address: '0.0.0.0',
-        avatar: undefined,
-        key: 'elio@example.com',
-        value: 'elio@example.com',
-      },
-    ]
-    const result: any = SearchUtil.filterSearchRecommendResult(
-      { key0: object, key2: [], key4: [], key3: [], key1: [] },
-      null,
+    let result: any = SearchUtil.default.getCommandTypeParam(
+      search.SearchCommand.Empty,
     )
     expect(result).toMatchSnapshot()
   })
 
   test('1', () => {
-    const object: any = [
-      {
-        name: 'Edmond',
-        address: '0.0.0.0',
-        avatar: undefined,
-        key: 'Elio',
-        value: 'elio@example.com',
-      },
-      {
-        name: 'Michael',
-        address: '0.0.0.0',
-        avatar: undefined,
-        key: 'elio@example.com',
-        value: 'Elio',
-      },
-    ]
-    const result: any = SearchUtil.filterSearchRecommendResult(
-      { key0: object, key2: [], key4: [], key3: [], key1: [] },
-      null,
+    let result: any = SearchUtil.default.getCommandTypeParam(
+      search.SearchCommand.During,
     )
     expect(result).toMatchSnapshot()
   })
 
   test('2', () => {
-    const object: any = [
+    let result: any = SearchUtil.default.getCommandTypeParam(
+      search.SearchCommand.Has,
+    )
+    expect(result).toMatchSnapshot()
+  })
+})
+
+describe('SearchUtil.default.filterSearchRecommendResult', () => {
+  test('0', () => {
+    let result: any = SearchUtil.default.filterSearchRecommendResult(
+      ['Expressway', 'Extensions', 'Lights', 'Extensions'],
       {
-        name: 'George',
-        address: '192.168.1.5',
-        avatar: undefined,
-        key: 'Elio',
-        value: 'elio@example.com',
+        command: 'parse mobile firewall',
+        Empty: ' ',
+        value: { toLowerCase: () => 'Michael' },
       },
+    )
+    expect(result).toMatchSnapshot()
+  })
+
+  test('1', () => {
+    let result: any = SearchUtil.default.filterSearchRecommendResult(
+      ['Harbors', 'Expressway', 'Expressway', 'Extensions'],
       {
-        name: 'Anas',
-        address: '192.168.1.5',
-        avatar: undefined,
-        key: 'Dillenberg',
-        value: 'Dillenberg',
+        command: 'parse mobile firewall',
+        Empty: '',
+        value: { toLowerCase: () => 'Anas' },
       },
-    ]
-    const result: any = SearchUtil.filterSearchRecommendResult(
-      { key0: object, key2: [], key4: [], key3: [], key1: [] },
-      null,
+    )
+    expect(result).toMatchSnapshot()
+  })
+
+  test('2', () => {
+    let result: any = SearchUtil.default.filterSearchRecommendResult(
+      ['Port', 'Port', 'Port', 'Lights'],
+      {
+        command: 'transmit bluetooth bus',
+        Empty: ' ',
+        value: { toLowerCase: () => 'Edmond' },
+      },
     )
     expect(result).toMatchSnapshot()
   })
 
   test('3', () => {
-    const object: any = [
+    let result: any = SearchUtil.default.filterSearchRecommendResult(
+      ['Port', 'Port', 'Lights', 'Expressway'],
       {
-        name: 'Edmond',
-        address: '192.168.1.5',
-        avatar: 'https://cdn.fakercloud.com/avatars/akmalfikri_128.jpg',
-        key: 'elio@example.com',
-        value: 'Elio',
+        command: 'transmit bluetooth bus',
+        Empty: ' ',
+        value: { toLowerCase: () => 'Michael' },
       },
-      {
-        name: 'George',
-        address: '0.0.0.0',
-        avatar: 'https://cdn.fakercloud.com/avatars/zvchkelly_128.jpg',
-        key: 'elio@example.com',
-        value: 'Dillenberg',
-      },
-      {
-        name: 'Pierre Edouard',
-        address: '192.168.1.5',
-        avatar: 'https://cdn.fakercloud.com/avatars/zvchkelly_128.jpg',
-        key: 'Elio',
-        value: 'elio@example.com',
-      },
-    ]
-    const result: any = SearchUtil.filterSearchRecommendResult(
-      { key1: object, key0: [] },
-      null,
     )
     expect(result).toMatchSnapshot()
   })
 
   test('4', () => {
-    const result: any = SearchUtil.filterSearchRecommendResult(
-      { key0: [], key2: [], key4: [], key3: [], key1: [] },
-      null,
+    let result: any = SearchUtil.default.filterSearchRecommendResult(
+      ['Extensions', 'Port', 'Harbors', 'Port'],
+      {
+        command: 'parse mobile firewall',
+        Empty: '',
+        value: { toLowerCase: () => 'Michael' },
+      },
     )
+    expect(result).toMatchSnapshot()
+  })
+
+  test('5', () => {
+    let result: any = SearchUtil.default.filterSearchRecommendResult([], {
+      command: '',
+      Empty: '',
+      value: { toLowerCase: () => '' },
+    })
     expect(result).toMatchSnapshot()
   })
 })
