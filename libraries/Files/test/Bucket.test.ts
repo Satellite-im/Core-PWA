342e242acbc9c7e4c2310c70a66b1b565b605ab5
Joe McGrath <josephmcg01@gmail.com>
2022-02-15 17:36:58 +0900

    fix(test): update bucket test to match new structure

--- libraries/Files/test/Bucket.test.ts
+++ libraries/Files/test/Bucket.test.ts
@@ -1,39 +1,35 @@
-// import { Fil } from '../Fil'
-// import { FilSystem } from '../FilSystem'
-// import { Bucket } from '../remote/textile/Bucket'
-// import { FileSystemExport } from '../types/filesystem'
+import { Fil } from '../Fil'
+import { FilSystem } from '../FilSystem'
+import { Bucket } from '../remote/textile/Bucket'
+import { FileSystemExport } from '../types/filesystem'
 
-// const mockFileData = {
-//   _name: 'TestFile.png',
-//   _descrption: 'Test file description',
-//   hash: '0x0aef',
-// }
+const mockFileData = {
+  _name: 'TestFile.png',
+  _descrption: 'Test file description',
+  hash: '0x0aef',
+}
 
-// const file = new Fil(...Object.values(mockFileData))
-// const file2 = new Fil(
-//   ...Object.values({ name: 'testPng2.png', ...mockFileData }),
-// )
+const file = new Fil(...Object.values(mockFileData))
+const file2 = new Fil(
+  ...Object.values({ name: 'testPng2.png', ...mockFileData }),
+)
 
-// describe('Test FileSystem Directory', () => {
-//   it('Fetch index and import it into the fileSystem', () => {
-//     const bucket = new Bucket(new FilSystem())
-//     const fsToImport = new Bucket(new FilSystem())
-
-//     fsToImport.fileSystem.addChild(file)
-//     fsToImport.fileSystem.createDirectory('dir')
-//     fsToImport.fileSystem.openDirectory('dir')
-//     fsToImport.fileSystem.addChild(file2)
-
-//     // need to place in variable or it returns empty after the first time
-//     const ex: FileSystemExport = fsToImport.index
-
-//     bucket.updateIndex(ex)
-//     expect(bucket.index).toEqual(ex)
-//   })
-// })
-
-describe('Test ', () => {
+describe('Test FileSystem Directory', () => {
   it('Fetch index and import it into the fileSystem', () => {
-    expect('').toEqual('')
+    const bucket = new Bucket()
+    const fs = new FilSystem()
+
+    fs.addChild(file)
+    fs.createDirectory('dir')
+    fs.openDirectory('dir')
+    fs.addChild(file2)
+
+    fs.goBackToDirectory('root')
+
+    // need to place in variable or it returns empty after the first time
+    const ex: FileSystemExport = fs.export
+
+    bucket.updateIndex(ex)
+    expect(bucket.index).toEqual(ex)
   })
 })
