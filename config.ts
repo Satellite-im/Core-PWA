42a92ae773b9548b1462e915110986b45dd9b847
jekrimo <jekrimo@gmail.com>
2022-02-16 12:50:16 -0700

    fix(groupChat): glyphs working in groups now

--- config.ts
+++ config.ts
@@ -124,6 +124,7 @@ export const Config = {
     spotify: /^https?:\/\/([a-z0-9-]+[.])spotify[.]com\/(playlist|embed)?/g,
     uuidv4:
       /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i,
+    collectionID: /^[0-9-]+$/,
   },
   webrtc: {
     constraints: {
