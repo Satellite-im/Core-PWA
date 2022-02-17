c507c6bdb40b7ac0ad1a994140fdc42143863038
Chris Hogan <chris@hogan.io>
2022-02-17 13:30:11 -0700

    overlay addition

--- components/ui/ComingSoon/ComingSoon.vue
+++ components/ui/ComingSoon/ComingSoon.vue
@@ -3,20 +3,34 @@
 <script lang="ts">
 import Vue from 'vue'
 
+declare module 'vue/types/vue' {
+  interface Vue {
+    overlayText: string
+  }
+}
+
 export default Vue.extend({
   name: 'ComingSoon',
   props: {
     tooltipText: {
       type: String,
       default: 'Coming Soon',
+      required: false,
     },
     tooltipPosition: {
       type: String,
       default: 'right',
+      required: false,
     },
     marginTopOverride: {
       type: String,
       default: '0px',
+      required: false,
+    },
+    areaCover: {
+      type: Boolean,
+      default: false,
+      required: false,
     },
   },
   methods: {
