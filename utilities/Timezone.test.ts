fe3594ff2960b5388c470f8bcc458f309f4e9748
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-02-17 20:27:40 +0700

    fix(jest): Patch daylight saving timezone issue (#1530)

--- utilities/Timezone.test.ts
+++ utilities/Timezone.test.ts
@@ -3,8 +3,14 @@ import * as Timezone from './Timezone'
 describe('init', () => {
   test('getTimezoneDropdowns', () => {
     const result = Timezone.getTimezoneDropdowns()
+    const nonDaylightTimezone = result.find((obj) => {
+      return obj.alternativeName === 'Western Indonesia Time'
+    })
 
-    expect(result).toMatchSnapshot()
+    expect(result.length).toEqual(316) // As of 17 Feb 2022, there are 316 timezones on @vvo/tzdb.
+    expect(nonDaylightTimezone.currentTimeFormat).toEqual(
+      '+07:00 Western Indonesia Time - Jakarta, Surabaya, Medan, Bandung',
+    ) // Make sure that the nonDaylightTimezone exists.
   })
   test('getUtfOffsetInMins for an existing timezone', () => {
     const result = Timezone.getUtfOffsetInMins('Asia/Jakarta')
