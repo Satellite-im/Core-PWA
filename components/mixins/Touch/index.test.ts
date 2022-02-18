2392bcce653f0ff8a52534c0dc77d369a5f01aa6
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-02-18 21:19:30 +0700

    feat(jest): 100% coverage for components/mixins/Touch

--- components/mixins/Touch/index.test.ts
+++ components/mixins/Touch/index.test.ts
@@ -7,7 +7,82 @@ describe('index.Touch.created', () => {
   })
 })
 
-describe('index.Touch.methods.sidebarSwipeHandler', () => {
+describe('index.Touch.methods.sidebarSwipeHandler human created', () => {
+  test('showSettings false and left direction', () => {
+    const local = {
+      $store: {
+        state: {
+          ui: {
+            showSettings: false,
+          },
+        },
+      },
+      $device: {
+        isMobile: true,
+      },
+      sidebar: false,
+    }
+    const givenFunction: any = index.Touch.methods.sidebarSwipeHandler(local)
+    givenFunction('left')
+    expect(local.sidebar).toBeFalsy()
+  })
+  test('showSettings false and right direction', () => {
+    const local = {
+      $store: {
+        state: {
+          ui: {
+            showSettings: false,
+          },
+        },
+      },
+      $device: {
+        isMobile: true,
+      },
+      sidebar: false,
+    }
+    const givenFunction: any = index.Touch.methods.sidebarSwipeHandler(local)
+    givenFunction('right')
+    expect(local.sidebar).toBeTruthy()
+  })
+  test('showSettings true and left direction', () => {
+    const local = {
+      $store: {
+        state: {
+          ui: {
+            showSettings: true,
+          },
+        },
+      },
+      $device: {
+        isMobile: true,
+      },
+      sidebar: false,
+    }
+    const givenFunction: any = index.Touch.methods.sidebarSwipeHandler(local)
+    givenFunction('left')
+    expect(local.sidebar).toBeFalsy()
+  })
+  test('showSettings true and right direction', () => {
+    const local = {
+      $store: {
+        state: {
+          ui: {
+            showSettings: true,
+          },
+        },
+      },
+      $device: {
+        isMobile: true,
+      },
+      sidebar: false,
+    }
+    const givenFunction: any = index.Touch.methods.sidebarSwipeHandler(local)
+    givenFunction('right')
+    // expect(local.sidebar).toBeTruthy()
+    expect(local.sidebar).toBeFalsy() // Will be false because no mutation is done to the local, because showSettings is not false.
+  })
+})
+describe.skip('index.Touch.methods.sidebarSwipeHandler', () => {
   test('0', () => {
     const result: any = index.Touch.methods.sidebarSwipeHandler({
       $store: { state: 'Florida' },
@@ -68,14 +143,14 @@ describe('index.Touch.methods.sidebarSwipeHandler', () => {
   })
 })
 
-describe('index.Touch.created', () => {
+describe.skip('index.Touch.created', () => {
   test('0', () => {
     const result: any = index.Touch.created()
     expect(result).toMatchSnapshot()
   })
 })
 
-describe('index.Touch.methods.sidebarSwipeHandler', () => {
+describe.skip('index.Touch.methods.sidebarSwipeHandler', () => {
   test('0', () => {
     const result: any = index.Touch.methods.sidebarSwipeHandler({
       $store: { state: 'Abruzzo' },
