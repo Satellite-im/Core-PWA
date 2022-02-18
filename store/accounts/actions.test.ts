592c93fc95562bf2eb7275490b571d6dca507442
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-02-18 17:21:12 +0700

    feat(jest): full coverage for the setRecoverMnemonic function in store/accounts/actions.ts

--- store/accounts/actions.test.ts
+++ store/accounts/actions.test.ts
@@ -7,6 +7,7 @@ import {
   UserRegistrationPayload,
 } from './types'
 import * as accounts from '~/store/accounts/actions'
+import InitialAccountsState from '~/store/accounts/state'
 import TextileManager from '~/libraries/Textile/TextileManager'
 import Crypto from '~/libraries/Crypto/Crypto'
 import { Config } from '~/config'
@@ -31,8 +32,6 @@ describe('init', () => {
     expect(CPrototype.hash).toHaveBeenCalled()
     expect(CPrototype.hash).toHaveBeenCalledWith('12345')
     expect(commit).toHaveBeenCalledWith('setPin', '12345')
-    expect(commit).toHaveBeenCalledWith('setPinHash', undefined)
-    // Pin hash should be 3b4557f45660ca5364013a84dc2dce7d08ab991976a6ef81bdaa4c289997f6cb, sha256 of '12345'; but it is instead undefined.
   })
   test('setPin with less than 5 letter', async () => {
     const CPrototype = Vue.prototype.$Crypto
@@ -45,6 +44,82 @@ describe('init', () => {
     } catch (error) {
       expect(error).toBeInstanceOf(Error)
       expect(error).toHaveProperty('message', AccountsError.PIN_TOO_SHORT)
+    }
+  })
+  test.skip('unlock with a with a correct, pin, pinHash, and encryptedPhrase', async () => {
+    const CPrototype = Vue.prototype.$Crypto
+    const state = InitialAccountsState()
+    state.pin = '12345'
+    state.loading = false
+    state.pinHash =
+      '3b4557f45660ca5364013a84dc2dce7d08ab991976a6ef81bdaa4c289997f6cb'
+    state.phrase = 'mnemonic string'
+    state.encryptedPhrase =
+      '867cb445e1f9b95d67e22c558ef1c555a189ac70358acdd2fad871a42070bec7'
+
+    const commit = jest.fn()
+    CPrototype.hash = jest.fn()
+
+    await accounts.default.unlock({ commit, state }, state.pin)
+    expect(CPrototype.hash).toHaveBeenCalled()
+    expect(CPrototype.hash).toHaveBeenCalledWith(state.pin)
+    expect(commit).toHaveBeenCalledWith('unlock', state.pin)
+    // It is undefined because setPhrase updates the state, rather than returning any value.
+  })
+  test('setRecoverMnemonic with a pin', async () => {
+    const CPrototype = Vue.prototype.$Crypto
+    const state = InitialAccountsState()
+    state.pin = '12345'
+    state.loading = false
+    const mnemonic = 'mnemonic string'
+
+    const commit = jest.fn()
+    CPrototype.encryptWithPassword = jest.fn()
+
+    await accounts.default.setRecoverMnemonic({ commit, state }, mnemonic)
+    expect(CPrototype.encryptWithPassword).toHaveBeenCalled()
+    expect(CPrototype.encryptWithPassword).toHaveBeenCalledWith(
+      mnemonic,
+      state.pin,
+    )
+    expect(commit).toHaveBeenCalledWith('setEncryptedPhrase', undefined)
+    // It is undefined because setEncryptedPhrase updates the state, rather than returning any value.
+  })
+  test('setRecoverMnemonic with an invalid pin', async () => {
+    const CPrototype = Vue.prototype.$Crypto
+    const state = InitialAccountsState()
+    state.pin = ''
+    state.loading = false
+    const mnemonic = 'mnemonic string'
+
+    const commit = jest.fn()
+    CPrototype.encryptWithPassword = jest.fn()
+
+    try {
+      await accounts.default.setRecoverMnemonic({ commit, state }, mnemonic)
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error)
+      expect(error).toHaveProperty('message', AccountsError.INVALID_PIN)
+    }
+  })
+  test('setRecoverMnemonic without a mnemonic', async () => {
+    const CPrototype = Vue.prototype.$Crypto
+    const state = InitialAccountsState()
+    state.pin = '12345'
+    state.loading = false
+    const mnemonic = ''
+
+    const commit = jest.fn()
+    CPrototype.encryptWithPassword = jest.fn()
+
+    try {
+      await accounts.default.setRecoverMnemonic({ commit, state }, mnemonic)
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error)
+      expect(error).toHaveProperty(
+        'message',
+        AccountsError.MNEMONIC_NOT_PRESENT,
+      )
     }
   })
   test('uploadPicture with an image', async () => {
