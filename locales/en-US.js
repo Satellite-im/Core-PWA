5ffe33cf7913e86cc8534a5a5fb2924c1f5f69da
Sasha Zakablukov <alexz@ein-des-ein.com>
2022-02-21 16:01:41 +0200

    fix(chat): show toast with error of permissions instead of app crash

--- locales/en-US.js
+++ locales/en-US.js
@@ -493,6 +493,9 @@ export default {
     storage: {
       database_not_cleared: 'Could not clear database',
     },
+    webRTC: {
+      permission_denied: 'Permission denied',
+    },
   },
   search: {
     search: 'Search... ',
