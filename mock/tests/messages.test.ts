5b8dfa7e3b2a0e17fa7ca5a6d19410ca3284c7fb
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-02-24 18:23:17 +0700

    feat(jest): 100% coverage fro mock/messages.ts

---
+++ mock/tests/messages.test.ts
@@ -0,0 +1,11 @@
+import * as Marketplace from '../marketplace'
+import * as Messages from '../messages'
+
+describe('init', () => {
+  it('gets the Messages constant', () => {
+    expect(Messages.Messages).toMatchSnapshot()
+  })
+  it('gets the PinnedMessages constant', () => {
+    expect(Messages.PinnedMessages).toMatchSnapshot()
+  })
+})
