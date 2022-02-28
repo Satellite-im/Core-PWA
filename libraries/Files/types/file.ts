27ada357e24a650a6ff4ee413ca7bf49369d9273
Joe McGrath <josephmcg01@gmail.com>
2022-02-28 13:21:00 +0900

    fix(fileType): setup template and support a few more types

--- libraries/Files/types/file.ts
+++ libraries/Files/types/file.ts
@@ -1,10 +1,24 @@
 /* Describes all supported filetypes
  * this will be useful for applying icons to the tree later on
  * if we don't have a filetype supported, we can just default to generic.
+ * todo AP-931 - fill this out following ticket instructions
  */
 export enum FILE_TYPE {
   GENERIC = 'generic',
-  // images
+
+  // application - todo
+  ARC = 'application/x-freearc',
+  BZ = 'application/x-bzip',
+  BZTWO = 'application/x-bzip2',
+  GZIP = 'application/gzip',
+  JAR = 'application/java-archive',
+  PDF = 'application/pdf',
+  RAR = 'application/vnd.rar',
+  TAR = 'application/x-tar',
+  ZIP = 'application/zip',
+  SEVENZIP = 'application/x-7z-compressed',
+
+  // image - embeddable
   APNG = 'image/apng',
   AVIF = 'image/avif',
   GIF = 'image/gif',
@@ -12,8 +26,10 @@ export enum FILE_TYPE {
   PNG = 'image/png',
   SVG = 'image/svg+xml',
   WEBP = 'image/webp',
-  // archives
-  ZIP = 'application/zip',
-  RAR = 'application/vnd.rar',
-  SEVENZIP = 'application/x-7z-compressed',
+
+  // image - non-embeddable
+  BMP = 'image/bmp',
+  HEIC = 'image/heic',
+  ICO = 'image/vnd.microsoft.icon',
+  TIFF = 'image/tiff',
 }
