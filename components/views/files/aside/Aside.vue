d45e0f42b169244f0ee73e534d0fdc158ba846d9
Joe McGrath <josephmcg01@gmail.com>
2022-03-01 09:31:35 +0900

    fix(progress): fill progress bar properly on load

--- components/views/files/aside/Aside.vue
+++ components/views/files/aside/Aside.vue
@@ -5,7 +5,7 @@ import Vue from 'vue'
 export default Vue.extend({
   data() {
     return {
-      progress: 0 as number,
+      progress: this.$FileSystem.percentStorageUsed as number,
     }
   },
   computed: {
