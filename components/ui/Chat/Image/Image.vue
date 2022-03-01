494b98e6e9501015088da6a623d3473eed902717
Joe McGrath <33670767+josephmcg@users.noreply.github.com>
2022-03-01 09:46:47 +0900

    fix(fileSize): standardize filesize conversion and general component cleanup (#1744)

--- components/ui/Chat/Image/Image.vue
+++ components/ui/Chat/Image/Image.vue
@@ -14,10 +14,6 @@ export default Vue.extend({
       type: String,
       default: 'Image',
     },
-    url: {
-      type: String,
-      default: 'url',
-    },
     full: {
       type: Boolean,
       default: false,
@@ -30,36 +26,7 @@ export default Vue.extend({
   data() {
     return {
       showfull: false,
-      fileSize: '',
     }
-  },
-  computed: {
-    getFileSize() {
-      return this.bytesToSize(this.image.size)
-    },
-  },
-  methods: {
-    /**
-     * @method bytesToSize
-     * @description converts bytes to display easily readable file size
-     * @param bytes bytes of current file
-     * @example bytesToSize(this.file.size)
-     */
-    bytesToSize(bytes: number) {
-      const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
-      if (bytes === 0) return '0 Bytes'
-      const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
-      return `${Math.round(bytes / Math.pow(1024, i), 2)} ${sizes[i]}`
-    },
-    async downloadImage(imageURL: string) {},
-    /**
-     * @method openImage DocsTODO
-     * @description
-     * @example
-     */
-    openImage() {
-      window?.open(this.$props.image.url, '_blank').focus()
-    },
   },
 })
 </script>
