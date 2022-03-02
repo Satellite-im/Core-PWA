9e2f933cdac7a3ab39873ca833b2564d5db794ac
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-03-02 17:06:09 +0000

    chore(typo): fix typo

--- config.ts
+++ config.ts
@@ -93,7 +93,7 @@ export const Config = {
   },
   chat: {
     defaultMessageLimit: 50,
-    defalutLoadMoreLimit: 20,
+    defaultLoadMoreLimit: 20,
     messageMaxChars: 256,
     timestampUpdateInterval: 60 * 1000,
     maxChars: 256,
