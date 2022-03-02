73a1e9891d39e684e46a9286c050f0e5672d42b6
pavlzk <pavel.lozko@brymonsoft.com>
2022-03-02 13:42:59 +0300

    chore(groupChats): update groupChats store

--- store/groups/mutations.ts
+++ store/groups/mutations.ts
@@ -2,14 +2,15 @@ import { GroupsState, Group } from './types'
 
 const mutations = {
   setGroups(state: GroupsState, groups: Group[]) {
-    console.log('setGroups', groups)
     state.all = groups
   },
   addGroup(state: GroupsState, group: Group) {
     state.all.push(group)
   },
   updateGroup(state: GroupsState, group: Group) {
-    state.all = state.all.map((item) => (item.id !== group.id ? item : group))
+    state.all = state.all.map((item) =>
+      item.id !== group.id ? item : { ...item, ...group },
+    )
   },
 }
 
