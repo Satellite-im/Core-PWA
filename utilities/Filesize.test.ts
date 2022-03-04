466905d249e8df3595aaa2ba1646fd51c69a11b1
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-03-04 21:35:06 +0700

    feat(jest): 100% coverage for Filesize.ts (#1798)

---
+++ utilities/Filesize.test.ts
@@ -0,0 +1,13 @@
+import { filesize } from './Filesize'
+
+describe('init', () => {
+  let inst: any
+
+  beforeEach(() => {
+    inst = filesize
+  })
+
+  it('should return the initial return value', () => {
+    expect(inst(8)).toMatchSnapshot()
+  })
+})
