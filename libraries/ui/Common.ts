8330d5f7c35222bf73e1f677c612551d5f69a687
davidchan1219 <nkristic0@gmail.com>
2022-03-08 12:35:07 -0500

    fix(privacy): fix ui and logic issue

---
+++ libraries/ui/Common.ts
@@ -0,0 +1,12 @@
+export function validURL(text: string): boolean {
+  const pattern = new RegExp(
+    '^((ftp|ws|rtp|rtmp|srt|udp|http|https)?:\\/\\/)?' +
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' +
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+      '(\\?[;&a-z\\d%_.~+=-]*)?' +
+      '(\\#[-a-z\\d_]*)?$',
+    'i',
+  )
+  return !!pattern.test(text)
+}
