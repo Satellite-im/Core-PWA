985ef3964be92e8a72ae0447fb66abd7ef3e0509
davidchan1219 <nkristic0@gmail.com>
2022-03-08 10:07:32 -0500

    change the title of the privacy page and add some components

--- components/views/settings/pages/privacy/index.vue
+++ components/views/settings/pages/privacy/index.vue
@@ -4,11 +4,33 @@
 import Vue from 'vue'
 import { mapState } from 'vuex'
 import { Themes, Flairs, ThemeNames } from '~/store/ui/types.ts'
+import { ServerTypes } from '~/components/views/settings/pages/privacy/options'
 export default Vue.extend({
   name: 'PrivacySettings',
   layout: 'settings',
+  data() {
+    return {
+      ServerTypes,
+    }
+  },
   computed: {
     ...mapState(['ui', 'accounts', 'settings']),
+    serverType: {
+      set(state) {
+        this.$store.commit('settings/setServerType', state)
+      },
+      get() {
+        return this.settings.serverType
+      },
+    },
+    ownInfo: {
+      set(state) {
+        this.$store.commit('settings/setOwnInfo', state)
+      },
+      get() {
+        return this.settings.ownInfo
+      },
+    },
     registry: {
       get() {
         return !this.accounts ? false : this.accounts.registry
