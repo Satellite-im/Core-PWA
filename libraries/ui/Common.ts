764f62e7caebd8297393532feb8bc7e7d00ccf9f
davidchan1219 <nkristic0@gmail.com>
2022-03-09 05:01:30 -0500

    fix(common): fix validate problem and style issue

--- libraries/ui/Common.ts
+++ libraries/ui/Common.ts
@@ -2,11 +2,12 @@ export function validURL(text: string): boolean {
   const pattern = new RegExp(
     '^((ftp|ws|wss|rtp|rtmp|srt|udp|http|https)?:\\/\\/)?' +
       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      'localhost|' +
       '((\\d{1,3}\\.){3}\\d{1,3}))' +
       '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
       '(\\?[;&a-z\\d%_.~+=-]*)?' +
       '(\\#[-a-z\\d_]*)?$',
     'i',
   )
-  return !!pattern.test(text)
+  return !!pattern.test(text) || !text
 }
