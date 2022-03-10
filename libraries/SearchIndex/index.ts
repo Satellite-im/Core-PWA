ec90c9c5540d103202cb26f033aa2dc6cbe58efa
Drew Ewing <drew.ewing@satellite.im>
2022-03-10 05:16:38 -0700

    fix(searchIndex): temporarily disable search, add comments, pr cleanup

--- libraries/SearchIndex/index.ts
+++ libraries/SearchIndex/index.ts
@@ -5,10 +5,28 @@ export type SearchIndexSchema = {
   fields: string[]
 }
 
+/**
+ * Class representing a SearchIndex
+ * @class SearchIndex
+ */
 export default class SearchIndex {
   schema: SearchIndexSchema
   index?: lunr.Index
 
+  /**
+   * @constructs SearchIndex
+   * @param schema the schema to use for the index
+   */
+  constructor(schema: SearchIndexSchema) {
+    this.schema = schema
+  }
+
+  /**
+   * Builds a search index from a list of documents.
+   * @param schema the schema of the index
+   * @param data the data to index
+   * @returns lunr.Index
+   */
   static build(schema: SearchIndexSchema, data: any[]) {
     return lunr(function () {
       this.ref(schema.ref)
@@ -17,14 +35,18 @@ export default class SearchIndex {
     })
   }
 
-  constructor(schema: SearchIndexSchema) {
-    this.schema = schema
-  }
-
+  /**
+   * Updates the index with new data.
+   * @param data the data to index
+   */
   update(data: any[]) {
     this.index = SearchIndex.build(this.schema, data)
   }
 
+  /**
+   * Serializes the index to JSON.
+   * @returns JSON
+   */
   serialize() {
     return JSON.stringify({
       schema: this.schema,
@@ -32,6 +54,11 @@ export default class SearchIndex {
     })
   }
 
+  /**
+   * Deserialize an index from JSON.
+   * @param serialized the serialized index
+   * @returns SearchIndex
+   */
   static deserialize(serialized: string) {
     const { schema, index } = JSON.parse(serialized)
     const searchIndex = new SearchIndex(schema)
@@ -39,6 +66,12 @@ export default class SearchIndex {
     return searchIndex
   }
 
+  /**
+   * Searches the index for a query string.
+   * @param query the query to search for
+   * @param retry is this a retry?
+   * @returns an array of results
+   */
   search(query: string, retry = false): lunr.Index.Result[] | undefined {
     try {
       return this.index?.search(query)
@@ -52,6 +85,11 @@ export default class SearchIndex {
     }
   }
 
+  /**
+   * Subscribe to an observable that emits data to be indexed
+   * @param observable the observable to subscribe to
+   * @param callback the callback to call when data is emitted
+   */
   subscribe(observable: any, callback: any) {
     return observable.subscribe((data: any[]) => {
       this.update(data)
@@ -59,6 +97,10 @@ export default class SearchIndex {
     })
   }
 
+  /**
+   * Unsubscribe from an observable that emits data to be indexed
+   * @param observable the observable to unsubscribe from
+   */
   unsubscribe(observable: any) {
     return observable.unsubscribe()
   }
--- plugins/thirdparty/dexie.ts
+++ plugins/thirdparty/dexie.ts
@@ -15,30 +15,11 @@ export class SatelliteDB extends Dexie {
   public constructor() {
     super('SatelliteDB')
     this.version(1).stores({
-      conversations: 'key',
+      conversations: 'key, lastInbound',
+      conversationMessages:
+        'id, conversation, from, to, at, readAt, type, payload',
     })
-    this.version(2)
-      .stores({
-        conversations: 'key, lastInbound',
-        conversationMessages:
-          'id, conversation, from, to, at, readAt, type, payload',
-      })
-      .upgrade((tx) => {
-        return tx
-          .table('conversations')
-          .toCollection()
-          .modify(async (chat: any) => {
-            await Promise.all(
-              chat?.conversation?.map((conversation: any) => {
-                console.info('conversation', conversation)
-                return tx
-                  .table('conversationMessages')
-                  .add({ ...conversation, conversation: chat.key })
-              }),
-            )
-            delete chat.conversation
-          })
-      })
+
     this.conversations = this.table('conversations')
     this.conversationMessages = this.table('conversationMessages')
   }
