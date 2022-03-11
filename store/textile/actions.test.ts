4b547f93d7d1a6e9c71193959b9bca03647f79dd
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-03-11 10:03:15 +0800

    fix(jest): resolved Skaler error import jest (#2283)

--- store/textile/actions.test.ts
+++ store/textile/actions.test.ts
@@ -1,32 +1,59 @@
 import Vue from 'vue'
 import { TextileError } from './types'
+import { DIRECTORY_TYPE } from '~/libraries/Files/types/directory'
 import * as actions from '~/store/textile/actions'
 import * as Logger from '~/utilities/Logger'
 import TextileManager from '~/libraries/Textile/TextileManager'
 import { TextileFileSystem } from '~/libraries/Files/TextileFileSystem'
 import { Config } from '~/config'
+import { Fil } from '~/libraries/Files/Fil'
+import { FilSystem } from '~/libraries/Files/FilSystem'
 
 Vue.prototype.$Config = Config
 
 const DefaultLogger = Logger.default
 Vue.prototype.$Logger = new DefaultLogger(Vue.prototype.$Config.debug)
 Vue.prototype.$TextileManager = new TextileManager()
-// Vue.prototype.$FileSystem = new TextileFileSystem()
+Vue.prototype.$FileSystem = new TextileFileSystem()
 
 describe('actions.default.initialize', () => {
   test('', async () => {
     try {
+      // Mocks so we have a fs.export
+      const mockFileData = {
+        name: 'TestFile.png',
+        hash: '0x0aef',
+        size: 42345,
+        descrption: 'Test file description',
+      }
+
+      const mockDirectoryData = {
+        name: 'dir',
+        liked: false,
+        shared: false,
+        type: DIRECTORY_TYPE.DEFAULT,
+      }
+
+      const file = new Fil(mockFileData)
+      const file2 = new Fil({ ...mockFileData, name: 'testPng2.png' })
+      const fs = new FilSystem()
+
+      fs.addChild(file)
+      fs.createDirectory(mockDirectoryData)
+      fs.openDirectory('dir')
+      fs.addChild(file2)
+
+      // Mocks for the arguments
       const commit = jest.fn()
       const JestTextileManager = Vue.prototype.$TextileManager
       JestTextileManager.init = jest.fn()
       JestTextileManager.getIdentityPublicKey = jest.fn()
       JestTextileManager.getIdentityPublicKey.mockReturnValueOnce('public key')
       JestTextileManager.bucket = jest.fn()
-      JestTextileManager.bucket.index = jest.fn()
-      JestTextileManager.bucket.index.mockReturnValueOnce('index')
-      // const JestFS = Vue.prototype.$FileSystem
-      // JestFS.import = jest.fn()
+      JestTextileManager.bucket.index = fs.export
+
       await actions.default.initialize({ commit }, { id: 'id', pass: 'pass' })
+
       expect(JestTextileManager.init).toHaveBeenCalled()
       expect(JestTextileManager.init).toHaveBeenCalledWith({
         id: 'id',
@@ -39,7 +66,6 @@ describe('actions.default.initialize', () => {
         'public key',
         { root: true },
       )
-      // expect(JestFS.import).toHaveBeenCalled()
     } catch (error) {
       console.error(error)
     }
