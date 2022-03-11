932538078c33b7bbb8e2bc5fa579ae92487b7085
Chris Hogan <chris@hogan.io>
2022-03-11 15:20:46 -0700

    chore(signaling): ui updates for signaling server settings

--- libraries/ui/Common.ts
+++ libraries/ui/Common.ts
@@ -9,5 +9,5 @@ export function validURL(text: string): boolean {
       '(\\#[-a-z\\d_]*)?$',
     'i',
   )
-  return (!!text.match(pattern) && !(text.length > 2048)) || !text
+  return !!pattern.test(text) || !text
 }
