b698e5d7133405f9b9fb7b94ec89c64f0b7cb949
pavlzk <pavel.lozko@brymonsoft.com>
2022-03-11 20:17:38 +0300

    chore(groupChats): add files

---
+++ libraries/Solana/GroupchatsProgram/GroupCrypto.ts
@@ -0,0 +1,61 @@
+import base58 from 'micro-base58'
+import { Keypair } from '@solana/web3.js'
+import { Invitation } from '~/libraries/Solana/GroupchatsProgram/GroupchatsProgram.types'
+import Crypto from '~/libraries/Crypto/Crypto'
+
+export default class GroupCrypto {
+  protected crypto: Crypto
+
+  constructor(keyPair: Keypair) {
+    this.crypto = new Crypto()
+    this.crypto.init(keyPair)
+  }
+
+  generateEncryptionKey() {
+    // TODO consider to change random key generation
+    return base58(Keypair.generate().secretKey).slice(0, 16) // becomes 64 length after encryption
+  }
+
+  async encryptInvite(invite: Invitation): Promise<Invitation> {
+    await this.crypto.initializeRecipient(invite.recipient)
+
+    const encryptionKey = await this.crypto.encryptFor(
+      invite.recipient.toString(),
+      invite.encryptionKey,
+    )
+
+    const groupId = await this.crypto.encryptWithPassword(
+      invite.groupId,
+      invite.encryptionKey,
+    )
+
+    return {
+      ...invite,
+      groupId,
+      encryptionKey,
+    }
+  }
+
+  async decryptInvite(invite: Invitation): Promise<Invitation> {
+    await this.crypto.initializeRecipient(invite.sender)
+
+    const encryptionKey = await this.crypto.decryptFrom(
+      invite.sender.toString(),
+      invite.encryptionKey,
+    )
+
+    const groupId = await this.crypto.decryptWithPassword(
+      invite.groupId,
+      encryptionKey,
+    )
+    if (!groupId) {
+      throw new Error('Unable to decrypt group id')
+    }
+
+    return {
+      ...invite,
+      groupId,
+      encryptionKey,
+    }
+  }
+}
