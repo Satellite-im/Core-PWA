3efc96d5deb6c09cd424cb2aa34cec06e4aa0fa3
Chris Hogan <chris@hogan.io>
2022-03-11 16:16:44 -0700

    simplify regex for acceptable url

--- libraries/ui/Common.ts
+++ libraries/ui/Common.ts
@@ -1,13 +1,4 @@
 export function validURL(text: string): boolean {
-  const pattern = new RegExp(
-    '^((ftp|ws|wss|rtp|rtmp|srt|udp|http|https)?:\\/\\/)?' +
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,63}|' +
-      'localhost|' +
-      '((\\d{1,3}\\.){3}\\d{1,3}))' +
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
-      '(\\?[;&a-z\\d%_.~+=-]*)?' +
-      '(\\#[-a-z\\d_]*)?$',
-    'i',
-  )
+  const pattern = /^(http|https|ws|wss|udp):\/\//
   return !!pattern.test(text) || !text
 }
