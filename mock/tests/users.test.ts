78555830d5360e5d8159aaaf78dfe169711008c2
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-03-16 01:46:15 +0700

    feat(jest): 100% coverage for mock/tests/users.ts (#2329)

---
+++ mock/tests/users.test.ts
@@ -0,0 +1,17 @@
+import * as Users from '../users'
+
+Users.CallUsers
+Users.User
+Users.Users
+
+describe('init', () => {
+  it('gets the CallUsers constant', () => {
+    expect(Users.CallUsers).toMatchSnapshot()
+  })
+  it('gets the User constant', () => {
+    expect(Users.User).toMatchSnapshot()
+  })
+  it('gets the Users constant', () => {
+    expect(Users.Users).toMatchSnapshot()
+  })
+})
