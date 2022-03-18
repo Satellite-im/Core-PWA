2ab76f0733589212e6ca5168b6626066f891235f
Eric Lee <eric.lee6174@gmail.com>
2022-03-18 06:46:57 -0600

    chore: use settings privacy component in setup

--- pages/setup/privacy/index.vue
+++ pages/setup/privacy/index.vue
@@ -7,92 +7,16 @@ import { validURL } from '~/libraries/ui/Common'
 
 export default Vue.extend({
   name: 'PrivacyScreen',
-  data() {
-    return {
-      formatError: false,
-      lengthError: false,
-      serverTypes: [
-        {
-          text: this.$t('pages.privacy.ownInfo.satelliteServer'),
-          value: 'satellite',
-        },
-        {
-          text: this.$t('pages.privacy.ownInfo.publicServer'),
-          value: 'public',
-        },
-        {
-          text: this.$t('pages.privacy.ownInfo.userDefinedServer'),
-          value: 'own',
-        },
-      ],
-    }
-  },
   computed: {
-    ...mapState(['ui', 'accounts', 'settings']),
-    serverType: {
-      set(state) {
-        this.$store.commit('settings/setServerType', state)
-      },
-      get() {
-        return this.settings.serverType
-      },
-    },
-    ownInfo: {
-      set(state) {
-        if (validURL(state) && state.length < 2049) {
-          this.formatError = false
-          this.lengthError = false
-          this.$store.commit('settings/setOwnInfo', state)
-          return
-        }
-        if (state.length > 2048) {
-          this.lengthError = true
-        }
-        if (!validURL(state)) {
-          this.formatError = true
-        }
-      },
-      get() {
-        return this.settings.ownInfo
-      },
-    },
-    registry: {
-      set(state) {
-        this.$store.commit('accounts/setRegistry', state)
-      },
-      get() {
-        return !this.accounts ? false : this.accounts.registry
-      },
-    },
-    storePin: {
-      set(state) {
-        this.$store.commit('accounts/setStorePin', state)
-      },
-      get() {
-        return !this.accounts ? false : this.accounts.storePin
-      },
-    },
-    embeddedLinks: {
-      set(state) {
-        this.$store.commit('settings/embeddedLinks', state)
-      },
-      get() {
-        return this.settings.embeddedLinks
-      },
-    },
-    displayCurrentActivity: {
-      set(state) {
-        this.$store.commit('settings/displayCurrentActivity', state)
-      },
-      get() {
-        return this.settings.displayCurrentActivity
-      },
-    },
+    ...mapState(['settings']),
   },
   methods: {
     async generateWallet() {
       await this.$store.dispatch('accounts/generateWallet')
       this.$router.push('phrase')
+    },
+    isDisabled() {
+      return !this.settings.ownInfo && this.settings.serverType === 'own'
     },
   },
 })
