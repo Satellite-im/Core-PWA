7cea0641c96ffaa4595b09a55eaabc87402dc713
Samir Vimercati <vimercati.samir@gmail.com>
2022-03-22 12:17:47 +0100

    fix(chat): scroll to bottom on new messages

--- layouts/chat.vue
+++ layouts/chat.vue
@@ -62,7 +62,6 @@
               :fullscreen-max-viewable-users="20"
             />
             <UiChatScroll
-              :contents="ui.messages"
               :prevent-scroll-offset="500"
               :class="
                 $store.state.friends.all.find(
@@ -72,6 +71,7 @@
                   : 'media-unopen'
               "
               enable-wrap
+              :user="recipient"
             >
               <Nuxt />
             </UiChatScroll>
