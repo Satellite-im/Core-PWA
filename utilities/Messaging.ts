adec772e8971384cd6d7f82eb97f4e3fdf165eda
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-03-22 17:33:49 +0000

    chore(update): add hash jira

--- utilities/Messaging.ts
+++ utilities/Messaging.ts
@@ -115,7 +115,7 @@ export function groupMessages(
         type: MessagingTypesEnum.GROUP,
         at: currentMessage.at,
         from: currentMessage.from,
-        sender: currentMessage.sender, // TODO add types
+        sender: currentMessage.sender, // TODO add types - AP-1128
         to: currentMessage.to,
         messages: [
           {
