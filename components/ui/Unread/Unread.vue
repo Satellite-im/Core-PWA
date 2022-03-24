019e56175e511b7ca7d43c11dc9b51c22d827a44
Sasha Zakablukov <alex@MacBook-Pro-Sasha.local>
2022-03-24 12:52:32 +0200

    fix(slimbar): add functionality to context menu and move sound emit to mount

--- components/ui/Unread/Unread.vue
+++ components/ui/Unread/Unread.vue
@@ -4,11 +4,14 @@
 import Vue, { PropType } from 'vue'
 import { User } from '~/types/ui/user'
 import ContextMenu from '~/components/mixins/UI/ContextMenu'
+import { Sounds } from '~/libraries/SoundManager/SoundManager'
 
 declare module 'vue/types/vue' {
   interface Vue {
     testFunc: () => void
     navigateToUser: () => void
+    handleShowProfile: () => void
+    removeUser: () => void
   }
 }
 
@@ -30,12 +33,13 @@ export default Vue.extend({
   data() {
     return {
       contextMenuValues: [
-        { text: this.$t('context.send'), func: this.testFunc },
+        { text: this.$t('context.send'), func: this.navigateToUser },
         { text: this.$t('context.voice'), func: this.testFunc },
         { text: this.$t('context.video'), func: this.testFunc },
-        { text: this.$t('context.profile'), func: this.testFunc },
-        { text: this.$t('context.remove'), func: this.testFunc },
+        { text: this.$t('context.profile'), func: this.handleShowProfile },
+        { text: this.$t('context.remove'), func: this.removeUser },
       ],
+      isLoading: false,
     }
   },
   computed: {
@@ -44,9 +48,28 @@ export default Vue.extend({
       return hash ? `${this.$Config.textile.browser}/ipfs/${hash}` : ''
     },
   },
+  mounted() {
+    this.$store.dispatch('sounds/playSound', Sounds.NEW_MESSAGE)
+  },
   methods: {
     testFunc() {
       this.$Logger.log('User Context', 'Test func')
+    },
+    async removeUser() {
+      this.isLoading = true
+      try {
+        await this.$store.dispatch('friends/removeFriend', this.user)
+        this.$router.replace('/chat/direct')
+      } catch (e) {
+        this.$toast.success(
+          this.$t('errors.friends.friend_not_removed') as string,
+        )
+      } finally {
+        this.isLoading = false
+      }
+    },
+    async handleShowProfile() {
+      this.$store.dispatch('ui/showProfile', this.user)
     },
     navigateToUser() {
       if (
