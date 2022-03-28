83a3b798e6824fe8d1ec0d10ef17dcd1972e97a3
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-03-28 19:43:17 +0700

    feat(jest): 100% coverage for Fil.test.ts

--- libraries/Files/test/Fil.test.ts
+++ libraries/Files/test/Fil.test.ts
@@ -26,8 +26,23 @@ describe('Test FileSystem File', () => {
     expect(clonedFile.size).toEqual(file.size)
     expect(clonedFile.id).not.toEqual(file.id)
   })
+  it(`Correctly returns a file object`, () => {
+    const clonedFile: Fil = file.copy
+    expect(clonedFile.file).toEqual(file.file)
+  })
+  it(`Correctly returns a file url`, () => {
+    const clonedFile: Fil = file.copy
+    expect(clonedFile.url).toEqual(file.url)
+  })
   it('Correctly sets a file description', () => {
     file.description = 'setter'
     expect(file.description).toEqual('setter')
+  })
+  it("Correctly sets a file's file", () => {
+    const testFile = new File(['hello'], 'test_fil.txt', {
+      type: 'text/plain',
+    })
+    file.file = testFile
+    expect(file.file).toEqual(testFile)
   })
 })
