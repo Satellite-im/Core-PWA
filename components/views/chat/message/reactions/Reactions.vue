3377322a88e00264b41ac9438462da37004d502f
Eric Lee <eric.lee6174@gmail.com>
2022-03-29 10:36:23 -0600

    fix(reaction): fix issues when try to add reaction

--- components/views/chat/message/reactions/Reactions.vue
+++ components/views/chat/message/reactions/Reactions.vue
@@ -3,7 +3,7 @@
 // eslint-disable-next-line import/named
 import Vue, { PropType } from 'vue'
 import { SmileIcon } from 'satellite-lucide-icons'
-import { Group, Reply, UIMessage } from '~/types/messaging'
+import { Group, UIReply, UIMessage } from '~/types/messaging'
 import { getUsernameFromState } from '~/utilities/Messaging'
 
 export default Vue.extend({
@@ -12,7 +12,7 @@ export default Vue.extend({
   },
   props: {
     reply: {
-      type: Object as PropType<Reply>,
+      type: Object as PropType<UIReply>,
       default: () => ({
         id: '',
         at: 1620515543000,
@@ -81,12 +81,16 @@ export default Vue.extend({
      * @example
      */
     quickReaction(emoji: String) {
-      this.$store.dispatch('ui/addReaction', {
+      const myTextilePublicKey = this.$TextileManager.getIdentityPublicKey()
+      this.$store.dispatch('textile/sendReactionMessage', {
+        to:
+          this.$props.message.to === myTextilePublicKey
+            ? this.$props.message.from
+            : this.$props.message.to,
         emoji,
-        reactor: this.$mock.user.name,
-        groupID: this.$props.group.id,
-        messageID: this.$props.message.id,
-        replyID: this.$props.reply.id,
+        reactTo: this.$props.reply.id
+          ? this.$props.reply.id
+          : this.$props.message.id,
       })
     },
     /**
