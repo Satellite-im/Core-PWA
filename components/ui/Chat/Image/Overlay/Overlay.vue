a7d043a433bb0fc91c5416cffd4b7c6d0d8e2edb
Eric Lee <eric.lee6174@gmail.com>
2022-04-01 05:36:08 -0600

    feat(preview): add chat image overlay component

---
+++ components/ui/Chat/Image/Overlay/Overlay.vue
@@ -0,0 +1,22 @@
+<template src="./Overlay.html" />
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import { mapState } from 'vuex'
+
+export default Vue.extend({
+  computed: {
+    ...mapState(['ui']),
+  },
+  methods: {
+    /**
+     * @method closeChatImageOverlay
+     * @description Close Chat Image Overlay
+     * @example
+     */
+    closeChatImageOverlay() {
+      this.$store.commit('ui/setChatImageOverlay', undefined)
+    },
+  },
+})
+</script>
+<style scoped lang="less" src="./Overlay.less"></style>
