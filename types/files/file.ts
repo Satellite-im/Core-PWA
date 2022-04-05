a3663490256d2d4da7e066760cc8262cd16557b7
Joe McGrath <33670767+josephmcg@users.noreply.github.com>
2022-04-05 22:29:28 +0900

    chore(mock): remove mock file info (#2713)

--- types/files/file.ts
+++ types/files/file.ts
@@ -1,21 +1,3 @@
-export type FileType = {
-  name: string
-  modified: number
-  type: string
-  size: number
-  location: string
-  meta: {
-    liked: boolean
-    shared: boolean
-  }
-}
-
-export type Folder = {
-  name: string
-  modified: number
-  children: Array<FileType | Folder>
-}
-
 export type UploadDropItemType = {
   file: File
   url: string
