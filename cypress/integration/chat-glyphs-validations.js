6781815a29506a74f444f3f8f52647414c77f6c7
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2022-04-06 06:23:12 -0500

    test(cypress): add tests for glyphs (#2734)

---
+++ cypress/integration/chat-glyphs-validations.js
@@ -0,0 +1,53 @@
+const faker = require('faker')
+const randomPIN = faker.internet.password(7, false, /[A-Z]/, 'test') // generate random PIN
+const recoverySeed =
+  'useful wedding venture reopen forest lawsuit essence hamster kitchen bundle level tower{enter}'
+
+describe('Chat - Sending Glyphs Tests', () => {
+  it('Send a glyph on chat', () => {
+    //Import account
+    cy.importAccount(randomPIN, recoverySeed)
+
+    //Validate profile name displayed
+    cy.contains('cypress', { timeout: 180000 }).should('be.visible')
+
+    //Validate message is sent
+    cy.goToConversation('cypress friend')
+
+    //Send first glyph from Astrobunny pack
+    cy.chatFeaturesSendGlyph()
+
+    //Assert glyph received
+    cy.goToLastGlyphOnChat()
+  })
+
+  it('Send a glyph after scrolling in the selection screen', () => {
+    //Send fourth glyph from Genshin Impact 2 pack
+    cy.chatFeaturesSendGlyph(6, 3)
+
+    //Assert glyph received
+    cy.goToLastGlyphOnChat()
+  })
+
+  it('Send a glyph from the recents section', () => {
+    //Send a glyph from recents section
+    cy.get('#glyph-toggle').click()
+    cy.get('#glyphs').should('be.visible')
+    cy.get('[data-cy=recent-glyph-item]').first().click()
+    cy.get('[data-cy=send-message]').click() //sending glyph message
+
+    //Assert glyph received
+    cy.goToLastGlyphOnChat()
+  })
+
+  it('Validate glyph pack screen appears', () => {
+    cy.goToLastGlyphOnChat().click()
+    cy.validateGlyphsModal()
+  })
+
+  it('Validate coming soon modal is displayed on glyph pack screen', () => {
+    cy.contains('View Glyph Pack').click()
+    cy.get('[data-cy=modal-cta]').should('be.visible')
+    cy.closeModal('[data-cy=modal-cta]')
+  })
+})
