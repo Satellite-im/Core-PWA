5f8bc7a1513ddfcfc9df54a8ac0475be1f6e8703
drepram <caxvis@gmail.com>
2022-04-07 20:15:41 +0700

    feat(jest): 39% coverage for FileType.ts

--- utilities/FileType.test.ts
+++ utilities/FileType.test.ts
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import * as FileType from '~/utilities/FileType'
 
 describe('FileType.isMimeArchive', () => {
@@ -46,5 +47,40 @@ describe('FileType.isMimeEmbeddableImage', () => {
   test('4', () => {
     const result: any = FileType.isMimeEmbeddableImage('')
     expect(result).toMatchSnapshot()
+  })
+})
+
+describe('FileType.isHeic', () => {
+  test('heic function check heic imag', async () => {
+    const buffer = fs.readFileSync('utilities/assets/heic-image.heic', {
+      flag: 'r',
+    })
+    const file = new Blob([buffer], {
+      type: 'image/heic',
+    })
+    const result: any = await FileType.isHeic(file)
+
+    expect(result).toBeTruthy()
+  })
+  test('heic function check non heic image', async () => {
+    const buffer = fs.readFileSync('utilities/assets/non-heic-image.png', {
+      flag: 'r',
+    })
+    const file = new Blob([buffer], {
+      type: 'image/heic', // Wrong type and the isHeic function still works
+    })
+    const result: any = await FileType.isHeic(file)
+
+    expect(result).toBeFalsy()
+  })
+
+  test('heic function check a non buffer response', async () => {
+    const buffer = Buffer.from('Hello, World', 'utf8') // buffer.length is 12
+    const file = new Blob([buffer], {
+      type: 'image/heic', // Wrong type and the isHeic function still works
+    })
+    const result: any = await FileType.isHeic(file)
+
+    expect(result).toBeFalsy()
   })
 })
