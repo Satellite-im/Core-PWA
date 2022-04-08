69c338e3772b5364363331166ecd9c44be92ca94
Joe McGrath <33670767+josephmcg@users.noreply.github.com>
2022-04-09 03:19:33 +0900

    fix(files): reactivity issue on files operations (#2783)

--- libraries/Files/Directory.ts
+++ libraries/Files/Directory.ts
@@ -1,8 +1,10 @@
 import { isEqual } from 'lodash'
+import { FileSortEnum } from '../Enums/enums'
 import { Item } from './abstracts/Item.abstract'
 import { FileSystemErrors } from './errors/Errors'
 import { Fil } from './Fil'
 import { DIRECTORY_TYPE } from './types/directory'
+import { FileSort } from '~/store/ui/types'
 
 export class Directory extends Item {
   private _children = new Map()
@@ -88,9 +90,38 @@ export class Directory extends Item {
   }
 
   /**
+   * @method sortedContent
+   * @param {FileSort} sort current sort key and asc/desc boolean
+   * @returns {Item[]} array of sorted content within the CURRENT directory
+   */
+  sortedContent(sort: FileSort): Item[] {
+    const key = sort.category
+    if (key === FileSortEnum.SIZE) {
+      return this.content.sort(
+        sort.asc
+          ? (a: Item, b: Item) => a[key] - b[key]
+          : (a: Item, b: Item) => b[key] - a[key],
+      )
+    }
+    if (key === FileSortEnum.MODIFIED) {
+      return this.content.sort(
+        sort.asc
+          ? (a: Item, b: Item) => b[key] - a[key]
+          : (a: Item, b: Item) => a[key] - b[key],
+      )
+    }
+
+    return this.content.sort(
+      sort.asc
+        ? (a: Item, b: Item) => a[key].localeCompare(b[key])
+        : (a: Item, b: Item) => b[key].localeCompare(a[key]),
+    )
+  }
+
+  /**
    * @method hasChild
    * @param {string} childName the name of the child to search for
-   * @returns {boolean} returns true or false depending on if a child exists in the directory
+   * @returns {boolean} true or false depending on if a child exists in the directory
    */
   hasChild(childName: string): boolean {
     return this._children.has(childName.toLowerCase())
@@ -99,7 +130,7 @@ export class Directory extends Item {
   /**
    * @method addChild
    * @param {Item} child the child to add to the parent directory
-   * @returns {boolean} returns true or false depending on if a child exists in the directory
+   * @returns {boolean} true or false depending on if a child exists in the directory
    */
   addChild(child: Item): boolean {
     if (this.hasChild(child.name)) {
@@ -128,7 +159,7 @@ export class Directory extends Item {
   /**
    * @method getChild
    * @param {string} childName the name of the child to fetch
-   * @returns {Item} returns the child if it exists, otherwise returns null
+   * @returns {Item | null} returns the child if it exists, otherwise returns null
    */
   getChild(childName: string): Item {
     return this._children.get(childName.toLowerCase()) || null
@@ -137,7 +168,7 @@ export class Directory extends Item {
   /**
    * @method removeChild
    * @param {string} childName the name of the child to remove
-   * @returns {Item} returns true if the child has been successfully removed
+   * @returns {boolean} true if the child has been successfully removed
    */
   removeChild(childName: string): boolean {
     if (this.getChild(childName) === null) return false
