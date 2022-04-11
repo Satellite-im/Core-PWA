03b866221c313210bbf888f0422c8451b6804a87
ThomBos <thombos@MBP-di-ThomBos.station>
2022-04-11 15:19:23 +0200

    chore(scrollbar): new scrollbar component created

---
+++ components/ui/SimpleScroll/SimpleScroll.vue
@@ -0,0 +1,32 @@
+<template src="./SimpleScroll.html" />
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import Simplebar from 'simplebar-vue'
+import 'simplebar/dist/simplebar.min.css'
+
+type ScrollMode = 'vertical' | 'horizontal'
+type ScrollShow = 'always' | 'scroll'
+
+export default Vue.extend({
+  name: 'ScrollBar',
+  components: {
+    Simplebar,
+  },
+  props: {
+    scrollMode: {
+      type: String as PropType<ScrollMode>,
+      default: 'vertical',
+      required: false,
+    },
+    scrollShow: {
+      type: String as PropType<ScrollShow>,
+      default: 'scroll',
+      required: false,
+    },
+  },
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less" src="./SimpleScroll.less"></style>
