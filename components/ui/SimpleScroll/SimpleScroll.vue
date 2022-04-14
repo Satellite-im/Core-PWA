4a36a345ea918e42ec6796e9245d9ec5f498a84c
Thomas Bosatelli <58808132+ThomBos@users.noreply.github.com>
2022-04-14 16:09:08 +0200

    chore(scrollbar): created new scrollbar component (#2856)

---
+++ components/ui/SimpleScroll/SimpleScroll.vue
@@ -0,0 +1,28 @@
+<template src="./SimpleScroll.html" />
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import { throttle } from 'lodash'
+
+type ScrollMode = 'vertical' | 'horizontal'
+type ScrollShow = 'always' | 'scroll'
+
+export default Vue.extend({
+  name: 'ScrollBar',
+  props: {
+    scrollMode: {
+      type: String as PropType<ScrollMode>,
+      default: 'vertical',
+      required: false,
+    },
+    scrollShow: {
+      type: String as PropType<ScrollShow>,
+      default: 'scroll',
+      required: false,
+    },
+  },
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less" src="./SimpleScroll.less"></style>
