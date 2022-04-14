2d8f20e31b19763daaedbbb4f1905aa99e4527c2
David Lee <david.lee4015@gmail.com>
2022-04-14 15:58:55 -0600

    chore(ui): refactor view/message/Friend (#2841)

--- components/views/message/Friend.vue
+++ components/views/message/Friend.vue
@@ -2,11 +2,9 @@
 
 <script lang="ts">
 import Vue, { PropType } from 'vue'
-import { mapState } from 'vuex'
 
 import ContextMenu from '~/components/mixins/UI/ContextMenu'
 import { User } from '~/types/ui/user'
-import { Conversation } from '~/store/textile/types'
 
 declare module 'vue/types/vue' {
   interface Vue {
@@ -37,27 +35,18 @@ export default Vue.extend({
   },
   data() {
     return {
-      existConversation: false,
       isLoading: false,
       address: '',
       checked: false,
     }
   },
   computed: {
-    ...mapState(['textile']),
     src(): string {
       const hash = this.user?.profilePicture
       return hash ? `${this.$Config.textile.browser}/ipfs/${hash}` : ''
     },
   },
   watch: {
-    'textile.conversations': {
-      handler(newValue) {
-        this.existMessage(newValue)
-      },
-      deep: true,
-      immediate: true,
-    },
     /**
      * Watch checked state
      * if false, set selected as ''
@@ -73,43 +62,6 @@ export default Vue.extend({
     },
   },
   methods: {
-    getLastUpdate() {
-      const currentUserInfo =
-        this.$store.state.textile.conversations[this.user.address]
-
-      const lastMessageAt = currentUserInfo?.messages
-        ? Math.max.apply(
-            null,
-            Object.values(currentUserInfo.messages).map((msg: any) => msg.at),
-          )
-        : 0
-
-      const uLastUpdate =
-        (this.user.lastUpdate ||
-          currentUserInfo?.lastUpdate ||
-          lastMessageAt) ??
-        0
-
-      const today = new Date().setHours(0, 0, 0, 0)
-
-      if (uLastUpdate) {
-        const uDay = new Date(uLastUpdate).setHours(0, 0, 0, 0)
-
-        if (today === uDay) {
-          return this.$dayjs(uLastUpdate).format('HH:mm')
-        }
-        return this.$dayjs(uLastUpdate).format('YYYY-MM-DD')
-      }
-
-      return 'No message'
-    },
-    existMessage(textileObj: Conversation) {
-      const currentUserInfo = textileObj[this.user.address]
-
-      this.$data.existConversation = !(
-        !currentUserInfo || currentUserInfo?.lastUpdate <= 0
-      )
-    },
     toggleSelect() {
       this.checked = !this.checked
     },
