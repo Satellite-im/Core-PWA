d97450797b2f7efbde015e176504c3f79965d209
Sasha Zakablukov <alex@MacBook-Pro-Sasha.local>
2022-04-27 11:31:20 +0300

    fix(fonts): add bold fonts

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,10 +27,22 @@ export default Vue.extend({
   src: url('~assets/fonts/SpaceMono-Regular.ttf');
 }
 @font-face {
+  font-family: 'SpaceMono';
+  font-style: normal;
+  font-weight: 700;
+  src: url('~assets/fonts/SpaceMono-Bold.ttf');
+}
+@font-face {
   font-family: @secondary-font;
   font-style: normal;
   font-weight: 400;
   src: url('~assets/fonts/Poppins-Regular.ttf');
+}
+@font-face {
+  font-family: @secondary-font;
+  font-style: normal;
+  font-weight: 700;
+  src: url('~assets/fonts/Poppins-Bold.ttf');
 }
 p {
   font-family: @secondary-font;
