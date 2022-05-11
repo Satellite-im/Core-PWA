d5e140be5853212c9476cfa573930deb87ede870
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-05-11 22:18:12 +0700

    feat(jest): 9% coverage for store/textile/actions

--- store/textile/actions.test.ts
+++ store/textile/actions.test.ts
@@ -17,7 +17,7 @@ Vue.prototype.$TextileManager = new TextileManager()
 Vue.prototype.$FileSystem = new TextileFileSystem()
 
 describe('actions.default.initialize', () => {
-  test('', async () => {
+  test('initialize', async () => {
     try {
       // Mocks so we have a fs.export
       const mockFileData = {
@@ -65,6 +65,130 @@ describe('actions.default.initialize', () => {
         'public key',
         { root: true },
       )
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error)
+    }
+  })
+  test('initialize without record.consent_scan', async () => {
+    try {
+      // Mocks so we have a fs.export
+      const mockFileData = {
+        name: 'TestFile1.png',
+        hash: '0x0aef',
+        size: 42345,
+        descrption: 'Test file description',
+      }
+
+      const mockDirectoryData = {
+        name: 'dir1',
+        liked: false,
+        shared: false,
+        type: DIRECTORY_TYPE.DEFAULT,
+      }
+
+      const file = new Fil(mockFileData)
+      const file2 = new Fil({ ...mockFileData, name: 'testPng3.png' })
+      const fs = new FilSystem()
+
+      fs.addChild(file)
+      fs.createDirectory(mockDirectoryData)
+      fs.openDirectory('dir')
+      fs.addChild(file2)
+
+      // Mocks for the arguments
+      const commit = jest.fn()
+      const JestTextileManager = Vue.prototype.$TextileManager
+      JestTextileManager.init = jest.fn()
+      JestTextileManager.getIdentityPublicKey = jest.fn()
+      JestTextileManager.getIdentityPublicKey.mockReturnValueOnce('public key')
+      JestTextileManager.bucket = jest.fn()
+      JestTextileManager.bucket.index = fs.export
+      JestTextileManager.userInfoManager = jest.fn()
+      JestTextileManager.userInfoManager.getUserRecord = jest
+        .fn()
+        .mockReturnValueOnce({
+          consent_scan: true,
+        })
+
+      await actions.default.initialize({ commit }, { id: 'id', pass: 'pass' })
+
+      expect(JestTextileManager.init).toHaveBeenCalled()
+      expect(JestTextileManager.init).toHaveBeenCalledWith({
+        id: 'id',
+        pass: 'pass',
+      })
+      expect(JestTextileManager.getIdentityPublicKey).toHaveBeenCalled()
+      expect(commit).toHaveBeenCalledWith(
+        'accounts/updateTextilePubkey',
+        'public key',
+        { root: true },
+      )
+      expect(commit).toHaveBeenCalledWith('settings/setConsentScan', true, {
+        root: true,
+      })
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error)
+    }
+  })
+  test('initialize without record.block_nsfw', async () => {
+    try {
+      // Mocks so we have a fs.export
+      const mockFileData = {
+        name: 'TestFile2.png',
+        hash: '0x0aef',
+        size: 42345,
+        descrption: 'Test file description',
+      }
+
+      const mockDirectoryData = {
+        name: 'dir2',
+        liked: false,
+        shared: false,
+        type: DIRECTORY_TYPE.DEFAULT,
+      }
+
+      const file = new Fil(mockFileData)
+      const file2 = new Fil({ ...mockFileData, name: 'testPng4.png' })
+      const fs = new FilSystem()
+
+      fs.addChild(file)
+      fs.createDirectory(mockDirectoryData)
+      fs.openDirectory('dir')
+      fs.addChild(file2)
+
+      // Mocks for the arguments
+      const commit = jest.fn()
+      const JestTextileManager = Vue.prototype.$TextileManager
+      JestTextileManager.init = jest.fn()
+      JestTextileManager.getIdentityPublicKey = jest.fn()
+      JestTextileManager.getIdentityPublicKey.mockReturnValueOnce('public key')
+      JestTextileManager.bucket = jest.fn()
+      JestTextileManager.bucket.index = fs.export
+      JestTextileManager.userInfoManager = jest.fn()
+      JestTextileManager.userInfoManager.getUserRecord = jest
+        .fn()
+        .mockReturnValueOnce({
+          block_nsfw: true,
+        })
+
+      await actions.default.initialize({ commit }, { id: 'id', pass: 'pass' })
+
+      expect(JestTextileManager.init).toHaveBeenCalled()
+      expect(JestTextileManager.init).toHaveBeenCalledWith({
+        id: 'id',
+        pass: 'pass',
+      })
+      expect(JestTextileManager.getIdentityPublicKey).toHaveBeenCalled()
+      expect(commit).toHaveBeenCalledWith(
+        'accounts/updateTextilePubkey',
+        'public key',
+        { root: true },
+      )
+      expect(commit).toHaveBeenCalledWith('settings/setBlockNsfw', true, {
+        root: true,
+      })
     } catch (error) {
       // eslint-disable-next-line no-console
       console.error(error)
