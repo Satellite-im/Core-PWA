da7dfc748d22f4ffbdbb070aba0ed71fc2c5be48
Joe McGrath <josephmcg01@gmail.com>
2022-05-18 08:49:25 +0900

    fix(bundle): remove v clipboard

--- components/views/navigation/sidebar/status/Status.vue
+++ components/views/navigation/sidebar/status/Status.vue
@@ -1,15 +1,24 @@
-<template src="./Status.html" />
+<template src="./Status.html"></template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { mapState } from 'vuex'
+import { RootState } from '~/types/store/store'
 
 export default Vue.extend({
   computed: {
-    ...mapState(['accounts']),
+    ...mapState({
+      accounts: (state) => (state as RootState).accounts,
+    }),
     src(): string {
       const hash = this.accounts.details.profilePicture
       return hash ? `${this.$Config.textile.browser}/ipfs/${hash}` : ''
+    },
+  },
+  methods: {
+    copyId() {
+      navigator.clipboard.writeText(this.accounts.active)
+      this.$toast.show(this.$t('ui.copied') as string)
     },
   },
 })
