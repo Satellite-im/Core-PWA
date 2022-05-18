7361d25e1813b031f46355bfe90fb96a83fd9177
Joe McGrath <33670767+josephmcg@users.noreply.github.com>
2022-05-19 00:13:12 +0900

    fix(bundle): remove v clipboard (#3214)

--- nuxt.config.js
+++ nuxt.config.js
@@ -81,7 +81,6 @@ export default defineNuxtConfig({
   plugins: [
     // Third Party
     { src: '~/plugins/thirdparty/vscrolllock.ts', ssr: false },
-    { src: '~/plugins/thirdparty/clipboard.ts' },
     { src: '~/plugins/thirdparty/clickoutside.ts' },
     { src: '~/plugins/thirdparty/filesize.ts' },
     { src: '~/plugins/thirdparty/persist.ts', ssr: false },
