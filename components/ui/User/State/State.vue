c222954f37b745595b4a1b4d1b5dbafde302059d
Mauro Molinari <mauro.molinari@knobs.it>
2022-05-23 14:46:26 +0200

    fix(user): state

--- components/ui/User/State/State.vue
+++ components/ui/User/State/State.vue
@@ -3,62 +3,83 @@
     class="user-state"
     data-cy="user-state"
     :style="`width:${size}px; height:${size}px`"
-    :class="{ 'is-large': size > 36 }"
   >
-    <UiCircle
-      data-cy="satellite-circle-profile"
-      :type="src ? 'image' : 'random'"
-      :seed="user.address"
-      :size="size"
-      :source="src"
-      @click="clickHandler"
-    />
-    <div
-      v-if="user.state !== 'mobile' && !isTyping"
-      class="status"
-      :class="{ [`is-${user.state}`]: user.state }"
-    />
-    <smartphone-icon
-      v-else-if="user.state === 'mobile'"
-      size="1x"
-      :class="`mobile-status is-${user.state}`"
-    />
-    <UiChatTypingIndicator v-else />
+    <svg width="44" height="40" viewBox="0 0 44 40" class="mask">
+      <foreignObject
+        x="0"
+        y="0"
+        width="40"
+        height="40"
+        :mask="`url(#${finalMask}-mask)`"
+      >
+        <UiCircle
+          data-cy="satellite-circle-profile"
+          :type="src ? 'image' : 'random'"
+          :seed="user.address"
+          :size="40"
+          :source="src"
+          @click="$emit('click', $event)"
+        />
+      </foreignObject>
+      <svg width="28" height="18" x="12" y="22" viewBox="0 0 28 18">
+        <rect
+          :class="`status is-${state}`"
+          width="28"
+          height="18"
+          :mask="`url(#mask-state-${state})`"
+        />
+        <foreignObject
+          v-if="state === 'typing'"
+          x="3"
+          y="9"
+          width="25"
+          height="6"
+        >
+          <div id="typing-loader-container">
+            <div id="typing-loader" />
+          </div>
+        </foreignObject>
+      </svg>
+    </svg>
   </div>
 </template>
 
-<script lang="ts">
-import Vue, { PropType } from 'vue'
+<script setup lang="ts">
+import { PropType } from 'vue'
+import { User, UserState } from '~/types/ui/user'
 
-import { SmartphoneIcon } from 'satellite-lucide-icons'
-import { User } from '~/types/ui/user'
+const props = defineProps({
+  user: { type: Object as PropType<User>, required: true },
+  isTyping: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+  src: { type: String, default: '', required: false },
+  size: {
+    type: Number,
+    default: 36,
+    required: false,
+  },
+})
 
-export default Vue.extend({
-  components: {
-    SmartphoneIcon,
-  },
-  props: {
-    user: {
-      type: Object as PropType<User>,
-      required: true,
-    },
-    isTyping: {
-      type: Boolean,
-      default: false,
-      required: false,
-    },
-    src: { type: String, default: '', required: false },
-    size: {
-      type: Number,
-      default: 36,
-      required: false,
-    },
-    clickHandler: {
-      type: Function,
-      required: false,
-      default: () => {},
-    },
-  },
+const state: UserState | 'typing' = computed(() => {
+  // Intentionally disable isTyping indicator in case of offline state
+  if (props.user.state === 'offline') return 'offline'
+  if (props.isTyping) return 'typing'
+
+  return props.user.state
+})
+
+const finalMask = computed(() => {
+  if (
+    state.value === 'online' ||
+    state.value === 'offline' ||
+    state.value === 'idle'
+  )
+    return 'circle'
+
+  return state.value
 })
 </script>
 
