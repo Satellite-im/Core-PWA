88ae5abdadf860056eca0cc4ee1eafa73706a7a1
Samir Vimercati <vimercati.samir@gmail.com>
2022-05-26 15:46:55 +0200

    chore: revert capacitor implementation

--- utilities/EnvInfo.ts
+++ utilities/EnvInfo.ts
@@ -1,19 +1,5 @@
+import { Capacitor } from '@capacitor/core'
 import { PlatformTypeEnum } from '~/libraries/Enums/enums'
-
-const getPlatform = () => {
-  const userAgent =
-    navigator.userAgent || navigator.vendor || (window as any).opera
-
-  if (/android/i.test(userAgent)) {
-    return 'android'
-  }
-
-  if (/iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream) {
-    return 'ios'
-  }
-
-  return 'web'
-}
 
 export const EnvInfo = class EnvInfo {
   currentPlatform: PlatformTypeEnum = PlatformTypeEnum.ANDROID
@@ -22,11 +8,10 @@ export const EnvInfo = class EnvInfo {
   debugInfo: any = undefined
   process: any = undefined
   navigator: Navigator = navigator
-
   constructor() {
     // https://capacitorjs.com/docs/v2/basics/utilities#getplatform
     // their doc has return type of string but lets use an enum
-    this.currentPlatform = getPlatform() as PlatformTypeEnum
+    this.currentPlatform = Capacitor.getPlatform() as PlatformTypeEnum
     this.navigator = navigator
 
     if (window.navigator.product === 'Gecko') {
