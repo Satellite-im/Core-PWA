41739a3825d00e2576a707edd083d1659781c49c
Joe McGrath <josephmcg01@gmail.com>
2022-06-01 14:58:44 +0900

    fix(groups): prevent submit if groupname too short

--- components/views/friends/quick/Quick.vue
+++ components/views/friends/quick/Quick.vue
@@ -1,8 +1,9 @@
-<template src="./Quick.html" />
+<template src="./Quick.html"></template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { mapState } from 'vuex'
+import { ModalWindows } from '~/store/ui/types'
 
 export default Vue.extend({
   name: 'Quick',
@@ -17,54 +18,46 @@ export default Vue.extend({
   computed: {
     ...mapState(['ui']),
   },
-  watch: {
-    'ui.modals.quickchat'(val) {
-      if (!val) {
-        this.friends = []
-        this.isLoading = false
-        this.name = ''
-        this.error = ''
-      }
-    },
-  },
   methods: {
     async confirm() {
-      /* Create Group and Start Chat.  For now Group Messages needs some changes on Solana & IPFS so leave this blank  */
-
-      if (this.$data.friends.length === 1) {
-        this.$router.push(`/chat/direct/${this.$data.friends[0].address}`)
+      if (!this.name || this.name.length < 3) {
+        this.error = this.$t('errors.chat.group_name') as string
+        return
+      }
+      // if only 1 friend, direct to DM instead
+      if (this.friends.length === 1) {
+        this.$router.push(`/chat/direct/${this.friends[0].address}`)
         this.$store.commit('ui/toggleModal', {
           name: 'quickchat',
           state: false,
         })
-      } else {
-        // logic for creating group chat
-        try {
-          this.isLoading = true
-          this.error = ''
-          const groupId = await this.$store.dispatch('groups/createGroup', {
-            name: this.name,
-          })
+      }
+      // create group
+      try {
+        this.isLoading = true
+        this.error = ''
+        const groupId = await this.$store.dispatch('groups/createGroup', {
+          name: this.name,
+        })
 
-          await Promise.all(
-            this.$data.friends.map(({ address }: { address: string }) =>
-              this.$store.dispatch('groups/sendGroupInvite', {
-                group: { id: groupId },
-                recipient: address,
-              }),
-            ),
-          )
-          // close quickchat modal after redirecting to chat
-          this.$store.commit('ui/toggleModal', {
-            name: 'quickchat',
-            state: false,
-          })
-          this.$router.push(`/chat/groups/${groupId}`)
-        } catch (e: any) {
-          this.error = `Failed to create group: ${e.message}`
-        } finally {
-          this.isLoading = false
-        }
+        await Promise.all(
+          this.friends.map(({ address }: { address: string }) =>
+            this.$store.dispatch('groups/sendGroupInvite', {
+              group: { id: groupId },
+              recipient: address,
+            }),
+          ),
+        )
+        // close quickchat modal after redirecting to chat
+        this.$store.commit('ui/toggleModal', {
+          name: ModalWindows.QUICK_CHAT,
+          state: false,
+        })
+        this.$router.push(`/chat/groups/${groupId}`)
+      } catch (e: any) {
+        this.error = `Failed to create group: ${e.message}`
+      } finally {
+        this.isLoading = false
       }
     },
   },
