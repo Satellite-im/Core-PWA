01c451f4dcb7801138936f6c7811582e7fac74c1
Joe McGrath <josephmcg01@gmail.com>
2022-06-06 18:53:48 +0900

    docs(getters): add jsdoc

--- store/settings/getters.ts
+++ store/settings/getters.ts
@@ -4,7 +4,19 @@ import { RootState } from '~/types/store/store'
 import { SettingsState } from '~/store/settings/types'
 
 export interface SettingsGetters {
+  /**
+   * @description return localized timestamp
+   * @argument {number} timestamp unix timestamp
+   * @returns {string} formatted time
+   * @example (23423424) => 2:49 PM
+   */
   getTimestamp(state: SettingsState): (timestamp: number) => string
+  /**
+   * @description return localized timestamp, including date
+   * @argument {number} timestamp unix timestamp
+   * @returns {string} formatted time and date
+   * @example (23423424) => 6/6/22 2:49 PM
+   */
   getFullTimestamp(state: SettingsState): (timestamp: number) => string
 }
 
