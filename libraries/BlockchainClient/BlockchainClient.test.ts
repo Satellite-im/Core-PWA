75ffe95175fd974ce0ab09b267a53d97042d8c6f
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-06-10 16:34:54 +0100

    chore(jest): add jest tests (#3569)

---
+++ libraries/BlockchainClient/BlockchainClient.test.ts
@@ -0,0 +1,8 @@
+import * as BlockchainClient from '~/libraries/BlockchainClient/BlockchainClient'
+
+describe.skip('BlockchainClient.default.getInstance', () => { //skipped due to the local env
+  test('0', () => {
+    const result: any = BlockchainClient.default.getInstance()
+    expect(result).toMatchSnapshot()
+  })
+})
