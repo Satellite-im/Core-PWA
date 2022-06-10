60600ee5ac7500c98f047584da9797f34f60d08f
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-06-10 16:36:43 +0100

    chore(lint): lint

--- libraries/BlockchainClient/BlockchainClient.test.ts
+++ libraries/BlockchainClient/BlockchainClient.test.ts
@@ -1,6 +1,7 @@
 import * as BlockchainClient from '~/libraries/BlockchainClient/BlockchainClient'
 
-describe.skip('BlockchainClient.default.getInstance', () => { //skipped due to the local env
+describe.skip('BlockchainClient.default.getInstance', () => {
+  // skipped due to the local env
   test('0', () => {
     const result: any = BlockchainClient.default.getInstance()
     expect(result).toMatchSnapshot()
