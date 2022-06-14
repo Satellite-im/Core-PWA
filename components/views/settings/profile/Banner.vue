4f5c65731e29ffa4377c5a197cba4add05894d06
Joe McGrath <33670767+josephmcg@users.noreply.github.com>
2022-06-15 07:32:38 +0900

    fix(profile): simplify profile, minor refactor (#3610)

---
+++ components/views/settings/profile/Banner.vue
@@ -0,0 +1,23 @@
+<template>
+  <img :src="sampleProfileInfo.imageUrl" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { sampleProfileInfo } from '~/mock/profile'
+
+export default Vue.extend({
+  computed: {
+    sampleProfileInfo: () => sampleProfileInfo,
+  },
+})
+</script>
+
+<style scoped lang="less">
+img {
+  height: 330px;
+  width: 100%;
+  object-fit: cover;
+  border-radius: 0 @corner-rounding;
+}
+</style>
