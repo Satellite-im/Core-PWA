5ac1b1b67d11003d3035c68ad3e1b1b64f4e2a67
Joe McGrath <josephmcg01@gmail.com>
2022-06-28 11:49:35 +0900

    fix(friends): add friend picture shrinking

--- components/views/friends/friend/Friend.vue
+++ components/views/friends/friend/Friend.vue
@@ -57,7 +57,6 @@ export default Vue.extend({
     }
   },
   computed: {
-    ...mapGetters('friends', ['friendExists']),
     ...mapState(['accounts']),
     src(): string {
       const hash =
@@ -68,12 +67,6 @@ export default Vue.extend({
     },
     contextMenuValues(): ContextMenuItem[] {
       return [{ text: this.$t('context.remove'), func: this.removeFriend }]
-    },
-    showStatus(): boolean {
-      return (
-        this.friendExists(this.$props.friend.address) ||
-        this.$props.friend.address === this.accounts.active
-      )
     },
   },
   beforeDestroy() {
