8361e61f33563605c7642c1849b79fe921bb34af
Joe McGrath <josephmcg01@gmail.com>
2022-06-28 10:34:14 +0900

    fix(notifications): text overflow for notifications

--- components/views/navigation/toolbar/alerts/Alerts.vue
+++ components/views/navigation/toolbar/alerts/Alerts.vue
@@ -3,10 +3,13 @@
 <script lang="ts">
 import Vue from 'vue'
 import { mapGetters, mapState } from 'vuex'
+import { FlaskConicalIcon } from 'satellite-lucide-icons'
 import { AlertState } from '~/libraries/ui/Alerts'
 import { RootState } from '~/types/store/store'
-
 export default Vue.extend({
+  components: {
+    FlaskConicalIcon,
+  },
   computed: {
     ...mapState({
       notifications: (state) => (state as RootState).ui.notifications,
