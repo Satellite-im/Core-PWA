d0ea5e17a42793dabd9a2f462a469e0b88eb238a
Luis Cardeña <35935591+luisecm@users.noreply.github.com>
2022-06-29 13:12:07 -0500

    chore(cypress): update cypress to 10.3 version (#3868)

---
+++ cypress-first.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  fixturesFolder: false,
+  defaultCommandTimeout: 15000,
+  numTestsKeptInMemory: 0,
+  pageLoadTimeout: 120000,
+  watchForFileChanges: false,
+  videosFolder: 'cypress/videos-chat-pair/first-user',
+  screenshotsFolder: 'cypress/screenshots-chat-pair/first-user',
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/integration-pair-chat/**/chat-first-user.js',
+    supportFile: 'cypress/support/first-user/index.js',
+  },
+})
---
+++ cypress-second.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  fixturesFolder: false,
+  defaultCommandTimeout: 15000,
+  numTestsKeptInMemory: 0,
+  pageLoadTimeout: 120000,
+  watchForFileChanges: false,
+  videosFolder: 'cypress/videos-chat-pair/second-user',
+  screenshotsFolder: 'cypress/screenshots-chat-pair/second-user',
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/integration-pair-chat/**/chat-second-user.js',
+    supportFile: 'cypress/support/second-user/index.js',
+  },
+})
