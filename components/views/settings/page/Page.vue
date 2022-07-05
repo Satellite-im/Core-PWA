3582f1e4aa6130453286fea19b47a11c2d29c7fe
Jason Woodland <me@jasonwoodland.com>
2022-07-05 17:57:20 +1000

    feat(settings): add page and section components

---
+++ components/views/settings/page/Page.vue
@@ -0,0 +1,10 @@
+PagePage
+<template src="./Page.html"></template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style scoped lang="less" src="./Page.less"></style>
