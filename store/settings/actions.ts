74d42020a6863ced3aa21db4999143d0ec0fe714
Mauro Molinari <mauro.molinari@knobs.it>
2022-07-08 11:23:17 +0200

    chore(dexie): clear all dbs from settings

--- store/settings/actions.ts
+++ store/settings/actions.ts
@@ -1,14 +1,18 @@
 import { Dexie } from 'dexie'
-import { db } from '~/libraries/SatelliteDB/SatelliteDB'
 import { SettingsError } from '~/store/settings/types'
-import { Config } from '~/config'
+
+async function deleteAllDexieDbs() {
+  const databases = await Dexie.getDatabaseNames()
+
+  databases.forEach((name) => {
+    new Dexie(name).delete()
+  })
+}
 
 export default {
   async clearLocalStorage() {
     try {
-      if (await Dexie.exists(Config.indexedDbName)) {
-        await db.delete()
-      }
+      await deleteAllDexieDbs()
       localStorage.clear()
     } catch (e) {
       throw new Error(SettingsError.DATABASE_NOT_CLEARED)
