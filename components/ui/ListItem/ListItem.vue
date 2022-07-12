d96322e0b05726523fca94300bfe92e3213e49b0
Nathan Power <Nathan.c.power@icloud.com>
2022-07-12 13:56:35 +1000

    feat(settings-profile): account link component/markup

---
+++ components/ui/ListItem/ListItem.vue
@@ -0,0 +1,37 @@
+<template src="./ListItem.html"></template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import { XIcon } from 'satellite-lucide-icons'
+
+type Account = {
+  name: string
+  icon: string
+  username: string
+  isVerified: boolean
+  displayOnProfile: boolean
+  verifyUrl: string
+}
+
+export default Vue.extend({
+  components: {
+    XIcon,
+  },
+  props: {
+    account: {
+      type: Object as PropType<Account>,
+      required: true,
+      default: () => ({
+        name: '',
+        icon: '',
+        username: '',
+        isVerified: false,
+        displayOnProfile: false,
+        verifyUrl: 'https://www.reddit.com/',
+      }),
+    },
+  },
+})
+</script>
+
+<style scoped lang="less" src="./ListItem.less"></style>
---
+++ components/views/settings/profile/accounts/List.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="list">
+    <UiListItem
+      v-for="account in accounts"
+      :key="account.name"
+      :account="account"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { TwitchIcon, YoutubeIcon, TwitterIcon } from 'satellite-lucide-icons'
+
+type Account = {
+  name: string
+  icon: string
+  isVerified: boolean
+  displayOnProfile: boolean
+}
+export default Vue.extend({
+  data() {
+    return {
+      accounts: {
+        reddit: {
+          name: 'Reddit',
+          // TODO: Add icon
+          icon: 'reddit',
+          username: '',
+          isVerified: false,
+          displayOnProfile: true,
+          verifyUrl: 'https://www.reddit.com/',
+        },
+        twitch: {
+          name: 'Twitch',
+          icon: TwitchIcon,
+          username: 'Sophia_Kalindi',
+          isVerified: true,
+          displayOnProfile: true,
+          verifyUrl: 'https://www.twitch.com/',
+        },
+        youtube: {
+          name: 'Youtube',
+          icon: YoutubeIcon,
+          username: 'Sophia_Kalindi94',
+          isVerified: false,
+          displayMusic: false,
+          displayOnProfile: true,
+          verifyUrl: 'https://www.youtube.com/',
+        },
+        twitter: {
+          name: 'Twitter',
+          icon: TwitterIcon,
+          username: 'Sophia_Kalindi94',
+          isVerified: true,
+          displayOnProfile: true,
+          verifyUrl: 'https://www.twitter.com/',
+        },
+        spotify: {
+          name: 'Spotify',
+          // TODO: Add icon
+          icon: 'spotify',
+          username: '',
+          isVerified: false,
+          displayMusic: true,
+          displayOnProfile: true,
+          verifyUrl: 'https://www.spotify.com/',
+        },
+      },
+    }
+  },
+  methods: {
+    toggleAccount(account: Account) {
+      account.displayOnProfile = !account.displayOnProfile
+    },
+  },
+})
+</script>
+
+<style scoped lang="less" src="./List.less"></style>
