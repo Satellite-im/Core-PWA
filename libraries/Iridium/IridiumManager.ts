97d9291c6eb3b359c62e2b0d221fce369af9314e
Joe McGrath <josephmcg01@gmail.com>
2022-07-22 16:05:45 +0900

    fix: remove iridium global

--- libraries/Iridium/IridiumManager.ts
+++ libraries/Iridium/IridiumManager.ts
@@ -29,7 +29,6 @@ export class IridiumManager extends Emitter {
     this.chat = new ChatManager(this)
     this.files = new FilesManager(this)
     this.settings = new SettingsManager(this)
-    window.iridium = this
   }
 
   /**
