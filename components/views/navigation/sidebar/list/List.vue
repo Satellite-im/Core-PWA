f8c1d7f2b49e5a8e52aad52c9bc25a228fc65eaa
vimercati-samir <vimercati.samir@gmail.com>
2022-07-25 17:47:56 +0200

    fix(friends): fix crash when removing a friend

--- components/views/navigation/sidebar/list/List.vue
+++ components/views/navigation/sidebar/list/List.vue
@@ -4,6 +4,7 @@
 import Vue from 'vue'
 import { UserPlusIcon } from 'satellite-lucide-icons'
 import iridium from '~/libraries/Iridium/IridiumManager'
+import { Conversation } from '~/libraries/Iridium/chat/types'
 
 export default Vue.extend({
   components: {
@@ -11,7 +12,29 @@ export default Vue.extend({
   },
   data: () => ({
     conversations: iridium.chat.state.conversations,
+    friends: iridium.friends.state,
   }),
+  computed: {
+    conversationsWithFriends(): Array<Conversation> {
+      return Object.values(this.conversations).filter((conversation) => {
+        const { participants } = conversation
+
+        let doesConversationsContainsActualFriends = true
+
+        participants.forEach((participant) => {
+          if (participant === iridium.connector?.id) {
+            return
+          }
+
+          if (!this.friends.list.find((friend) => friend.did === participant)) {
+            doesConversationsContainsActualFriends = false
+          }
+        })
+
+        return doesConversationsContainsActualFriends
+      })
+    },
+  },
   methods: {
     navigateAddFriends() {
       if (this.$route.name?.includes('friends-list')) {
