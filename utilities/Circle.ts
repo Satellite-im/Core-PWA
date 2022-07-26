e957991ab7b357c721d5ab635532251c3a125b00
Nathan Power <Nathan.c.power@icloud.com>
2022-07-26 16:40:38 +1000

    fix(circle): render different default colors based on provided did string (#4067)

--- utilities/Circle.ts
+++ utilities/Circle.ts
@@ -10,3 +10,36 @@ export function toHex(str: string) {
   }
   return res
 }
+
+export function stringToColor(str: string): string {
+  const hash = stringToHash(str)
+  return colors[hash]
+}
+
+function stringToHash(str: string): number {
+  return (
+    str.split('').reduce((p, c) => p + c.charCodeAt(0), 0) % (colors.length - 1)
+  )
+}
+
+const colors = [
+  '#fc5c65',
+  '#fd9644',
+  '#fed330',
+  '#26de81',
+  '#2bcbba',
+  '#eb3b5a',
+  '#fa8231',
+  '#f7b731',
+  '#20bf6b',
+  '#0fb9b1',
+  '#45aaf2',
+  '#4b7bec',
+  '#a55eea',
+  '#778ca3',
+  '#2d98da',
+  '#3867d6',
+  '#8854d0',
+  '#a5b1c2',
+  '#4b6584',
+]
