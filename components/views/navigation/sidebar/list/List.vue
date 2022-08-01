82b9671ea362f5d9bf64bf3663a639b322b15910
Nathan Power <Nathan.c.power@icloud.com>
2022-08-01 15:14:49 +1000

    feat(chat): sort sidebar items

--- components/views/navigation/sidebar/list/List.vue
+++ components/views/navigation/sidebar/list/List.vue
@@ -4,6 +4,7 @@
 import Vue from 'vue'
 import { UserPlusIcon } from 'satellite-lucide-icons'
 import iridium from '~/libraries/Iridium/IridiumManager'
+import { Conversation } from '~/libraries/Iridium/chat/types'
 
 export default Vue.extend({
   components: {
@@ -11,9 +12,19 @@ export default Vue.extend({
   },
   data: () => ({
     conversations: iridium.chat.state.conversations,
+    messages: iridium.chat.messages,
   }),
+  computed: {
+    sortedConversations(): Conversation[] {
+      const conversations = Object.values(this.conversations)
+      conversations.sort(
+        (a, b) => this.lastMessageTimestamp(b) - this.lastMessageTimestamp(a),
+      )
+      return conversations
+    },
+  },
   methods: {
-    navigateAddFriends() {
+    navigateAddFriends(): void {
       if (this.$route.name?.includes('friends-list')) {
         if (this.$device.isMobile) {
           this.$store.commit('ui/showSidebar', false)
@@ -21,6 +32,14 @@ export default Vue.extend({
       } else {
         this.$router.push({ path: '/friends' })
       }
+    },
+    lastMessageTimestamp(conversation: Conversation): number {
+      const messages = this.messages[conversation.id]
+      if (messages.length === 0) {
+        return conversation.updatedAt || 0
+      }
+      const lastMessage = messages[messages.length - 1]
+      return lastMessage.at || 0
     },
   },
 })
