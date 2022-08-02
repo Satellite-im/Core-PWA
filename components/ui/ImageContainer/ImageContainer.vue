f0dd968a765a22b215902aff173151fd55895df5
Nathan Power <Nathan.c.power@icloud.com>
2022-08-02 13:14:41 +1000

    feat(images): create and implement ImageContainer component

---
+++ components/ui/ImageContainer/ImageContainer.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="image-container" :class="{ 'image-container-nsfw': nsfw }">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    nsfw: {
+      type: Boolean,
+      required: false,
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.image-container {
+  overflow: hidden;
+  background-color: #fff;
+  background-image: linear-gradient(
+      45deg,
+      #eee 25%,
+      transparent 25%,
+      transparent 75%,
+      #eee 75%
+    ),
+    linear-gradient(45deg, #eee 25%, transparent 25%, transparent 75%, #eee 75%);
+  background-size: 16px 16px;
+  background-position: 0 0, 8px 8px;
+  filter: brightness(0.75);
+
+  &-nsfw {
+    background-color: @background;
+    background-image: none;
+
+    img {
+      filter: blur(40px);
+    }
+  }
+}
+</style>
