29b5af24b558ca45f60d6f4f384a1b9b4cb0478e
Sasha Zakablukov <38879804+AlexZakablukov@users.noreply.github.com>
2022-08-03 16:15:41 +0300

    fix(calls): fix call preview

--- components/views/media/previewCall/PreviewCall.vue
+++ components/views/media/previewCall/PreviewCall.vue
@@ -2,9 +2,13 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import { CornerUpLeftIcon } from 'satellite-lucide-icons'
 import iridium from '~/libraries/Iridium/IridiumManager'
 
 export default Vue.extend({
+  components: {
+    CornerUpLeftIcon,
+  },
   data() {
     return {
       chat: iridium.chat,
@@ -19,9 +23,21 @@ export default Vue.extend({
       return this.chat.getOtherParticipants(this.conversationId)[0]
     },
   },
-  beforeDestroy() {},
-  async mounted() {},
-  methods: {},
+  methods: {
+    async navigateToActiveConversation() {
+      if (!this.conversationId) {
+        return
+      }
+
+      if (this.$device.isMobile) {
+        // mobile, show slide 1 which is chat slide, set showSidebar flag false as css related
+        this.$store.commit('ui/setSwiperSlideIndex', 1)
+        this.$store.commit('ui/showSidebar', false)
+      }
+
+      this.$router.push(`/chat/${this.conversationId}`)
+    },
+  },
 })
 </script>
 
