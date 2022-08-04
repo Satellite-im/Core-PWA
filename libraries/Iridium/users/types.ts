a3d2c16b481452adabd438349f6a287c2e1fd182
Nathan Power <Nathan.c.power@icloud.com>
2022-08-04 11:41:04 +1000

    feat(group-manager): create manager to handle iridium user data (#4123)

---
+++ libraries/Iridium/users/types.ts
@@ -0,0 +1,14 @@
+// TODO: Replace User (friends/types.ts) and GroupMemberDetails (groups/types.ts) with User (users/types.ts)
+export type User = {
+  did: string
+  name: string
+  peerId?: string
+  status?: 'online' | 'offline' | 'busy' | 'away'
+  seen?: number
+  photoHash?: string
+}
+
+export const UsersError = {
+  NETWORK_ERROR: 'errors.users.network',
+  USER_NOT_FOUND: 'errors.users.not_found',
+}
