ddb5fc80c19a370b940bceefbba19f49a2c5f8a0
Nathan Power <Nathan.c.power@icloud.com>
2022-08-08 15:51:11 +1000

    feat(friends): build 'add friend' functionality for mobile (#4144)
    * feat(friends): build 'add friend' functionality for mobile
    
    * fix(friends): stop friends-list column from over-expanding
    
    * Revert "fix(friends): stop friends-list column from over-expanding"
    
    This reverts commit da342d6e33c11cd9207f2c295b0647da2bf1a426.
    
    * fix(friends): prevent friends-list from becoming squashed
    
    * fix(friends): adjust page spacing
    
    * fix(friends): add title to friends list

--- components/views/friends/add/Add.vue
+++ components/views/friends/add/Add.vue
@@ -1,21 +1,11 @@
 <template src="./Add.html"></template>
 <script lang="ts">
-// @ts-ignore
-import QrcodeVue from 'qrcode.vue'
-import { UserPlusIcon } from 'satellite-lucide-icons'
-
 import Vue from 'vue'
 import { mapState } from 'vuex'
-import { debounce } from 'lodash'
-import iridium from '~/libraries/Iridium/IridiumManager'
 import { FriendRequest, User } from '~/libraries/Iridium/friends/types'
 import { RootState } from '~/types/store/store'
 
 export default Vue.extend({
-  components: {
-    QrcodeVue,
-    UserPlusIcon,
-  },
   data() {
     return {
       error: '',
@@ -29,57 +19,6 @@ export default Vue.extend({
     ...mapState({
       showSidebar: (state) => (state as RootState).ui.showSidebar,
     }),
-  },
-  async mounted() {
-    if (this.$route.params && this.$route.params.id) {
-      this.$data.friendId = this.$route.params.id
-      this._searchFriend()
-    }
-    iridium.friends?.on('request/error', (err) => {
-      this.error = err
-    })
-  },
-  methods: {
-    _searchFriend: debounce(async function (this: any) {
-      if (!this.friendId.length) {
-        this.error = ''
-        this.user = null
-        this.searching = false
-        return
-      }
-      await this.searchFriend()
-      this.searching = false
-    }, 500),
-    async searchFriend() {
-      this.user = null
-      this.error = ''
-      this.searching = true
-      const friendId = this.friendId.trim()
-      if (friendId === iridium.connector?.id) {
-        this.error = this.$t('friends.self_add') as string
-        return
-      }
-      const hasFriend = iridium.friends.isFriend(friendId)
-      if (hasFriend) {
-        this.error = this.$t('friends.already_friend') as string
-      }
-
-      this.user = {
-        did: friendId,
-        name: friendId,
-        status: 'offline',
-      }
-      this.searching = false
-    },
-    onFriendRequestSent() {
-      this.request = null
-      this.user = null
-      this.friendId = ''
-      // @ts-ignore
-      const input = this.$refs.input.$refs.input as HTMLInputElement
-      input.value = ''
-      this.$toast.show(this.$t('friends.request_sent') as string)
-    },
   },
 })
 </script>
