43dade21a405cf1c904c27839244316466062147
ThomBos <thombos@MacBook-Pro-di-ThomBos.local>
2022-08-08 15:31:14 +0200

    refactor(buttons): rebase

---
+++ components/interactables/Button/HoverShader/HoverGlow.vue
@@ -0,0 +1,37 @@
+<template src="./HoverGlow.html"></template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  methods: {
+    mouseLeave(e: any) {
+      const target = e.target
+      target.style = ''
+    },
+    mouseEnter(e: any) {
+      const target = e.target
+      const rect = target.getBoundingClientRect()
+      target.style.setProperty('--circle-size', `${rect.height}px`)
+    },
+    mouseMove(e: any) {
+      const target = e.target
+
+      const rect = target.getBoundingClientRect()
+
+      const x = e.clientX - rect.left
+      const y = e.clientY - rect.top
+
+      target.style.setProperty('--mouse-x', `${x}px`)
+      target.style.setProperty('--mouse-y', `${y}px`)
+    },
+  },
+})
+</script>
+<style scoped lang="less" src="./HoverGlow.less"></style>
