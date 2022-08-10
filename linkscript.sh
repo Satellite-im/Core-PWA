cc093867c105083be277d3bde82b3023e5025fc5
Chris Hogan <chris@hogan.io>
2022-08-10 11:48:30 -0600

    chore(linkscript): updating iridium build node location (#4190)

--- linkscript.sh
+++ linkscript.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/Satellite-im/iridium.git
 cp .env iridium/.env
 cd iridium
-export SYNC_NODE_ADDR=/dns4/sync-ny.satellite.im/tcp/4003/ws,/dns4/sync-ny.satellite.im/tcp/4002
+export SYNC_NODE_ADDR=/ip4/138.197.229.159/tcp/4003/ws,/ip4/138.197.229.159/tcp/4002
 corepack enable
 corepack prepare pnpm@6.34.0 --activate
 pnpm i --unsafe-perm
