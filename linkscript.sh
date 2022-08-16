f5a6612c0d5bcc9a01cc37fbec7c48dd5ae47492
Chris Hogan <chris@hogan.io>
2022-08-16 11:19:35 -0600

    chore(linkscript): update pnpm in linkscript (#4303)

--- linkscript.sh
+++ linkscript.sh
@@ -4,7 +4,7 @@ cp .env iridium/.env
 cd iridium
 export SYNC_NODE_ADDR=/ip4/138.197.229.159/tcp/4003/ws,/ip4/138.197.229.159/tcp/4002
 corepack enable
-corepack prepare pnpm@6.34.0 --activate
+corepack prepare pnpm@7.9.1 --activate
 pnpm i --unsafe-perm
 pnpm build:ipfs
 pnpm build:browser
