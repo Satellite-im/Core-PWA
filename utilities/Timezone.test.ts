7b0b8692fc591d99d8f73e1061fe1661b1361af5
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-08-19 18:20:49 +0700

    chore(jest): update Timezone test

--- utilities/Timezone.test.ts
+++ utilities/Timezone.test.ts
@@ -10,7 +10,7 @@ describe('init', () => {
     expect(result.length).not.toBeNull()
     expect(result.length).toBeGreaterThan(0)
     expect(nonDaylightTimezone.currentTimeFormat).toEqual(
-      '+08:00 Singapore Time - Singapore, Woodlands, Marine Parade',
+      '+08:00 Singapore Time - Singapore, Woodlands, Geylang, Marine Parade',
     ) // Make sure that the nonDaylightTimezone exists.
   })
 
