422fb391c42c3c0abfb903c95c5d5b3286587db8
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-08-22 13:12:15 +0100

    chore(lint): lint jest

--- libraries/Iridium/friends/types.test.ts
+++ libraries/Iridium/friends/types.test.ts
@@ -1,14 +1,14 @@
-import { FriendsError } from './types';
+import { FriendsError } from './types'
 
-    describe('FriendsError', () => {
-        test('check FriendsError export type', () => {
-            expect(FriendsError).toEqual({
-                NETWORK_ERROR: 'errors.friends.network',
-                FRIEND_EXISTS: 'errors.friends.exists',
-                FRIEND_NOT_FOUND: 'errors.friends.not_found',
-                REQUEST_NOT_FOUND: 'errors.friends.request_not_found',
-                REQUEST_NOT_SENT: 'errors.friends.request_not_sent',
-                REQUEST_ALREADY_SENT: 'errors.friends.request_already_sent',
-            })
-          })
-        }) 
+describe('FriendsError', () => {
+  test('check FriendsError export type', () => {
+    expect(FriendsError).toEqual({
+      NETWORK_ERROR: 'errors.friends.network',
+      FRIEND_EXISTS: 'errors.friends.exists',
+      FRIEND_NOT_FOUND: 'errors.friends.not_found',
+      REQUEST_NOT_FOUND: 'errors.friends.request_not_found',
+      REQUEST_NOT_SENT: 'errors.friends.request_not_sent',
+      REQUEST_ALREADY_SENT: 'errors.friends.request_already_sent',
+    })
+  })
+})
--- libraries/Iridium/webrtc/hooks.test.disabled.ts
+++ libraries/Iridium/webrtc/hooks.test.disabled.ts
@@ -2,18 +2,18 @@ import { useCallElapsedTime, useUserStreams, useWebRTC } from './hooks'
 
 describe('useCallElapsedTime', () => {
   it('should expose a function', () => {
-		expect(useCallElapsedTime).toBeDefined()
-	})
+    expect(useCallElapsedTime).toBeDefined()
+  })
 })
 
 describe('useUserStreams', () => {
   it('should expose a function', () => {
-		expect(useUserStreams).toBeDefined()
-	})
+    expect(useUserStreams).toBeDefined()
+  })
 })
 
 describe('useWebRTC', () => {
   it('should expose a function', () => {
-		expect(useWebRTC).toBeDefined()
-	})
+    expect(useWebRTC).toBeDefined()
+  })
 })
