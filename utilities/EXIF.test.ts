1cdd51b546f37965207898602ce6bc5b238cfb30
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-08-24 00:30:40 +0100

    Update EXIF.test.ts

--- utilities/EXIF.test.ts
+++ utilities/EXIF.test.ts
@@ -3,36 +3,17 @@ import * as exif from './EXIF'
 describe('', () => {
   test('file is jpeg', async () => {
     const jpegBlob = new Blob(['testing'], { type: 'image/jpeg' })
-
-    // const file2 = new Fil({ ...mockFileData, name: 'testPng2.png' })
     const result = await exif.stripEXIF(jpegBlob)
     expect(result).toBe(jpegBlob)
   })
   test.skip('file is jpeg but promise rejects', async () => {
     const jpegBlob = new Blob([], { type: 'image/jpeg' })
-
-    // const file2 = new Fil({ ...mockFileData, name: 'testPng2.png' })
     const result = await exif.stripEXIF(jpegBlob)
-    // expect(result).rejects.toMatchObject({})
     expect(result).toBe(jpegBlob)
   })
 
   test('dv.getUint16 mocked ', async () => {
-    // global.DataView = class MockDataView {
-    //   filename: string
-    //   constructor(
-    //     parts: (string | Blob | ArrayBuffer | ArrayBufferView)[],
-    //     filename: string,
-    //     properties?: FilePropertyBag,
-    //   ) {
-    //     this.filename = filename
-    //   }
-    // }
-    // global.DataView = jest.fn()
-    // global.DataView.getUint16 = jest.fn().mockImplementationOnce(() => 42)
     const jpegBlob = new Blob(['testing'], { type: 'image/jpeg' })
-
-    // const file2 = new Fil({ ...mockFileData, name: 'testPng2.png' })
     const result = await exif.stripEXIF(jpegBlob)
     expect(result).toBe(jpegBlob)
   })
