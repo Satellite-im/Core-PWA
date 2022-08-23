f8f2c2c74424647d74c2a0861a14330b02de7a65
Thomas Bosatelli <58808132+ThomBos@users.noreply.github.com>
2022-08-23 02:26:06 +0200

    fix(call-ui): fixed loader ui (#4397)
    * fix(call): fixed loader ui
    
    * fix(call): rebase
    
    Co-authored-by: ThomBos <thombos@MBP-di-ThomBos.station>

---
+++ components/ui/Loaders/ScaleLoader/ScaleLoader.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="scale-loader">
+    <span />
+    <span />
+    <span />
+    <span />
+    <span />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'ScaleLoader',
+})
+</script>
+
+<style scoped lang="less">
+.scale-loader {
+  display: flex;
+  span {
+    background-color: @white;
+    width: 4px;
+    height: 20px;
+    margin: 2px;
+    border-radius: 2px;
+    display: inline-block;
+    animation: 1s cubic-bezier(0.2, 0.68, 0.18, 1.08) 0.1s infinite normal both
+      running scale-loader;
+    &:nth-child(2) {
+      animation-delay: 0.2s;
+    }
+    &:nth-child(3) {
+      animation-delay: 0.3s;
+    }
+    &:nth-child(4) {
+      animation-delay: 0.4s;
+    }
+    &:nth-child(5) {
+      animation-delay: 0.5s;
+    }
+  }
+}
+@keyframes scale-loader {
+  0% {
+    transform: scaleY(1);
+  }
+  50% {
+    transform: scaleY(0.4);
+  }
+  100% {
+    transform: scaleY(1);
+  }
+}
+</style>
