d1d5c32c1618da28f44e67fd9fbfc211dacb885b
Drew Ewing <drew.ewing@satellite.im>
2022-09-02 13:12:14 -0700

    fix(init): linkscript git pull

--- linkscript.sh
+++ linkscript.sh
@@ -2,8 +2,11 @@
 if [ ! -d ./linked-iridium ]; then
   git clone https://github.com/Satellite-im/iridium.git ./linked-iridium
   cp .env ./linked-iridium/.env
+  cd ./.linked-iridium
+else
+  cd ./linked-iridium
+  git pull -f
 fi
-cd ./linked-iridium
 export SYNC_NODE_ADDR=/ip4/138.197.229.159/tcp/4003/ws,/ip4/138.197.229.159/tcp/4002
 corepack enable
 corepack prepare pnpm@latest --activate
