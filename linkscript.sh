39c73e80eae68a362df34c03750153286351afcb
Chris Hogan <chris@hogan.io>
2022-09-07 13:29:37 -0600

    moving checkout

--- linkscript.sh
+++ linkscript.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 export PNPM_HOME=./.pnpm-links/
 if [ ! -d ./linked-iridium ]; then
-  if [[ -z "${DEPLOY_ENV}" ]]; then
-    git clone https://github.com/Satellite-im/iridium.git#dev ./linked-iridium
-  else
-    git clone https://github.com/Satellite-im/iridium.git#${DEPLOY_ENV} ./linked-iridium
-  fi
+  git clone https://github.com/Satellite-im/iridium.git ./linked-iridium
   cp .env ./linked-iridium/.env
   cd ./linked-iridium
 else
   cd ./linked-iridium
   git pull -f
 fi
+
+if [[ -z "${DEPLOY_ENV}" ]]; then
+  git checkout dev
+else
+  git checkout ${DEPLOY_ENV}
+fi
+
 export SYNC_NODE_ADDR=/ip4/138.197.229.159/tcp/4003/ws,/ip4/138.197.229.159/tcp/4002
 corepack enable
 corepack prepare pnpm@latest --activate
