567b6f4146ab19ef5887749cbe5e667d1c0a72c0
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-09-07 19:30:18 +0700

    chore(jest): remove unnecessary test

--- store/files/state.test.ts
+++ store/files/state.test.ts
@@ -1,10 +1,6 @@
 import InitialFilesState from './state'
 
 describe('Test files/state', () => {
-  it('should match the initial state snapshot', () => {
-    expect(InitialFilesState).toMatchSnapshot()
-  })
-
   test('the function should match the initial state snapshot', () => {
     expect(InitialFilesState()).toMatchSnapshot()
   })
