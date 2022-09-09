3bc538b5828124e3e5c1c84f1b25fb6a5a2493c7
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-09-09 23:46:33 +0100

    chore(test): add test jest

---
+++ utilities/animation.test.ts
@@ -0,0 +1,121 @@
+import * as animation from '~/utilities/animation'
+
+describe('animation.easeOutBack', () => {
+  test('easeOutBack 2', () => {
+    const result: any = animation.easeOutBack(2)
+    expect(result).toMatchSnapshot()
+  })
+
+  test('easeOutBack 1', () => {
+    const result: any = animation.easeOutBack(1)
+    expect(result).toMatchSnapshot()
+  })
+
+  test('easeOutBack 1.0', () => {
+    const result: any = animation.easeOutBack(1.0)
+    expect(result).toMatchSnapshot()
+  })
+
+  test('easeOutBack 0', () => {
+    const result: any = animation.easeOutBack(0)
+    expect(result).toMatchSnapshot()
+  })
+
+  test('easeOutBack 70', () => {
+    const result: any = animation.easeOutBack(70)
+    expect(result).toMatchSnapshot()
+  })
+
+  test('easeOutBack NaN', () => {
+    const result: any = animation.easeOutBack(NaN)
+    expect(result).toMatchSnapshot()
+  })
+})
+
+describe('animation.lerp', () => {
+  test('90, 90, 0', () => {
+    const result: any = animation.lerp(90, 90, 0)
+    expect(result).toMatchSnapshot()
+  })
+
+  test('100, 550, 0', () => {
+    const result: any = animation.lerp(100, 550, 0)
+    expect(result).toMatchSnapshot()
+  })
+
+  test('410, 550, 1', () => {
+    const result: any = animation.lerp(410, 550, 1)
+    expect(result).toMatchSnapshot()
+  })
+
+  test('1, 320, 1', () => {
+    const result: any = animation.lerp(1, 320, 1)
+    expect(result).toMatchSnapshot()
+  })
+
+  test('350, 380, 1', () => {
+    const result: any = animation.lerp(350, 380, 1)
+    expect(result).toMatchSnapshot()
+  })
+
+  test('Infinity, Infinity, Infinity', () => {
+    const result: any = animation.lerp(Infinity, Infinity, Infinity)
+    expect(result).toMatchSnapshot()
+  })
+})
+
+describe('animation.animate', () => {
+  test('timing 4, duration 60', () => {
+    const result: any = animation.animate({
+      timing: () => 4,
+      draw: () => undefined,
+      duration: 60,
+    })
+    expect(result).toMatchSnapshot()
+  })
+
+  test('timing 200, duration 15', () => {
+    const result: any = animation.animate({
+      timing: () => 200,
+      draw: () => undefined,
+      duration: 15,
+    })
+    expect(result).toMatchSnapshot()
+  })
+
+  test('timing 1000, duration 0.0001', () => {
+    const result: any = animation.animate({
+      timing: () => 1000,
+      draw: () => undefined,
+      duration: 0.0001,
+    })
+    expect(result).toMatchSnapshot()
+  })
+
+  test('timing 4, duration 2500', () => {
+    const result: any = animation.animate({
+      timing: () => 4,
+      draw: () => undefined,
+      duration: 2500,
+    })
+    expect(result).toMatchSnapshot()
+  })
+
+  test('timing 1000, duration 2500', () => {
+    const result: any = animation.animate({
+      timing: () => 1000,
+      draw: () => undefined,
+      duration: 2500,
+    })
+    expect(result).toMatchSnapshot()
+  })
+
+  test('timing Infinity, duration Infinity', () => {
+    const result: any = animation.animate({
+      timing: () => Infinity,
+      draw: () => undefined,
+      duration: Infinity,
+    })
+    expect(result).toMatchSnapshot()
+  })
+})
