eccd7dddc90150325192dd95a7e90b7d6b4d878d
Joe McGrath <josephmcg01@gmail.com>
2022-09-12 16:18:15 +0900

    feat(chat): copy/save image context menu in chat

--- components/ui/Chat/Image/Image.vue
+++ components/ui/Chat/Image/Image.vue
@@ -5,6 +5,8 @@ import { ImageIcon, DownloadIcon } from 'satellite-lucide-icons'
 import placeholderImage from '~/assets/svg/mascot/sad_curious.svg'
 import { MessageAttachment } from '~/libraries/Iridium/chat/types'
 import iridium from '~/libraries/Iridium/IridiumManager'
+import { ContextMenuItem } from '~/store/ui/types'
+import { FILE_TYPE } from '~/libraries/Files/types/file'
 
 export default Vue.extend({
   components: {
@@ -18,14 +20,28 @@ export default Vue.extend({
     },
   },
   data: () => ({
-    placeholderSrc: placeholderImage,
     usePlaceholder: false,
     dataURL: '',
+    blob: null as Blob | null,
+    pngBlob: null as Blob | null,
   }),
+  computed: {
+    placeholderImage: () => placeholderImage,
+    contextMenuValues(): ContextMenuItem[] {
+      // remove copy from GIF because it copies a still png version
+      if (this.attachment.type === FILE_TYPE.GIF) {
+        return [{ text: this.$t('context.save_img'), func: this.saveImage }]
+      }
+      return [
+        { text: this.$t('context.copy_img'), func: this.copyImage },
+        { text: this.$t('context.save_img'), func: this.saveImage },
+      ]
+    },
+  },
   async mounted() {
     const { fileBuffer } = await iridium.connector?.load(this.attachment.cid)
-    const blob = new Blob([fileBuffer], { type: this.attachment.type })
-    this.dataURL = URL.createObjectURL(blob)
+    this.blob = new Blob([fileBuffer], { type: this.attachment.type })
+    this.dataURL = URL.createObjectURL(this.blob)
   },
   beforeDestroy() {
     URL.revokeObjectURL(this.dataURL)
@@ -37,8 +53,49 @@ export default Vue.extend({
         dataURL: this.dataURL,
       })
     },
-    onImageError() {
-      this.usePlaceholder = true
+    /**
+     * @method copyImage
+     * @description clipboard API only accepts png. if not png, convert via canvas
+     */
+    async copyImage() {
+      if (!this.blob) {
+        return
+      }
+      if (this.blob.type !== 'image/png') {
+        this.pngBlob = await this.toPng()
+      }
+      await navigator.clipboard.write([
+        new ClipboardItem({
+          'image/png': this.pngBlob || this.blob,
+        }),
+      ])
+      this.$toast.show(this.$t('ui.copied') as string)
+    },
+
+    async saveImage() {
+      const anchor = this.$refs.download as HTMLAnchorElement
+      anchor.click()
+    },
+    /**
+     * @method toPng
+     * @param {Blob} blob embeddable image blob
+     * @description helper function - convert image blob to png for Clipboard API
+     */
+    toPng() {
+      return new Promise<Blob | null>((resolve) => {
+        const canvas = document.createElement('canvas')
+        const ctx = canvas.getContext('2d')
+        const img = new Image()
+        img.src = this.dataURL
+        img.onload = () => {
+          canvas.width = img.naturalWidth
+          canvas.height = img.naturalHeight
+          ctx?.drawImage(img, 0, 0)
+          canvas.toBlob((newBlob: Blob | null) => {
+            resolve(newBlob)
+          })
+        }
+      })
     },
   },
 })
