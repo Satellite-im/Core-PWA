905573b86f1b08ed4c0160d0a583e0ff8b5cf15e
Joe McGrath <33670767+josephmcg@users.noreply.github.com>
2022-09-14 11:08:01 +0900

    feat(chat): new group member sidebar for group chats (#4747)
    * feat(sidebar): show group members in group chat
    
    * feat(chat): wip added typing status
    
    * fix: remove dummy text

---
+++ components/views/chat/UserList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="user-list hover-scroll">
+    <TypographyText>
+      {{ $t('pages.chat.members', { count: userDetails.length }) }}
+    </TypographyText>
+    <div v-for="user in userDetails" :key="user.did" class="user">
+      <UiUserState :user="user" :conversation-id="conversationId" />
+      <TypographyText class="ellipsis">
+        {{ user.name }}
+      </TypographyText>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { computed, ComputedRef, reactive } from 'vue'
+import iridium from '~/libraries/Iridium/IridiumManager'
+import { conversationHooks } from '~/components/compositions/conversations'
+import { User } from '~/libraries/Iridium/users/types'
+
+export default Vue.extend({
+  setup() {
+    const { conversation, conversationId } = conversationHooks()
+
+    const state = reactive({
+      users: iridium.users,
+    })
+
+    const userDetails: ComputedRef<User[]> = computed(() => {
+      if (!conversation.value) {
+        return []
+      }
+      const arr = conversation.value.participants.map((p) =>
+        state.users.getUser(p),
+      ) as User[]
+      return arr.sort((a, b) => a.name.localeCompare(b.name))
+    })
+
+    return { conversation, conversationId, userDetails }
+  },
+})
+</script>
+
+<style lang="less" scoped>
+.user-list {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  padding: 16px;
+  width: 240px;
+  overflow-y: auto;
+  background: @foreground-gradient;
+  box-shadow: @ui-shadow;
+  user-select: none;
+
+  .user {
+    display: flex;
+    gap: 8px;
+    align-items: center;
+  }
+}
+</style>
