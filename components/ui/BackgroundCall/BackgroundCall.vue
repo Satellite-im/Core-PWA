1baa0ff8a5160e2e2c7f4213f94790b00ff653e0
Joe McGrath <josephmcg01@gmail.com>
2022-09-15 18:05:11 +0900

    fix(mobile): call timestamp for mobile background call

--- components/ui/BackgroundCall/BackgroundCall.vue
+++ components/ui/BackgroundCall/BackgroundCall.vue
@@ -3,37 +3,36 @@
 <script lang="ts">
 import Vue from 'vue'
 import iridium from '~/libraries/Iridium/IridiumManager'
+import { User } from '~/libraries/Iridium/users/types'
 
 export default Vue.extend({
   data() {
     return {
       webrtc: iridium.webRTC.state,
-      callTime: iridium.webRTC.callTime,
+      callTimeString: '',
     }
   },
   computed: {
-    remoteParticipant() {
+    remoteParticipant(): User | undefined {
       return iridium.webRTC.remoteParticipants()?.[0] ?? undefined
+    },
+  },
+  watch: {
+    webrtc: {
+      handler() {
+        this.callTimeString = this.$dayjs(this.webrtc.callStartedAt).toNow(true)
+      },
+      deep: true,
     },
   },
   methods: {
     navigateToActiveConversation() {
-      if (!this.webrtc.activeCall) {
+      const conversationId = this.webrtc.activeCall?.callId
+      if (!conversationId) {
         return
       }
 
-      const id = iridium.chat?.directConversationIdFromDid(
-        this.webrtc.activeCall.did,
-      )
-      if (
-        !id ||
-        !iridium.chat?.hasConversation(id) ||
-        !this.remoteParticipant
-      ) {
-        return
-      }
-
-      this.$router.push(`/mobile/chat/${id}`)
+      this.$router.push(`/mobile/chat/${conversationId}`)
     },
   },
 })
