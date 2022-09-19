a3da266b79299205b9b2e1d70eaf5c48a0e34795
Jason Woodland <me@jasonwoodland.com>
2022-09-19 16:30:32 +1000

    feat(settings): settings header component

---
+++ components/views/settings/header/Header.vue
@@ -0,0 +1,23 @@
+<template src="./Header.html"></template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { ChevronLeftIcon } from 'satellite-lucide-icons'
+import { useSwiper, useSwiperSlide } from 'swiper/vue'
+import { Swiper } from 'swiper/types'
+
+export default Vue.extend({
+  components: {
+    ChevronLeftIcon,
+  },
+  methods: {
+    goBack() {
+      const swiper = (this.$el.closest('.swiper-initialized') as any)
+        .swiper as Swiper
+      swiper.slidePrev()
+    },
+  },
+})
+</script>
+
+<style scoped lang="less" src="./Header.less"></style>
