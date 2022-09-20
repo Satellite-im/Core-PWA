4bc23031d9f8738d86eba83ccebdde7efb5ce4df
Joe McGrath <josephmcg01@gmail.com>
2022-09-20 09:58:10 +0900

    fix(sidebar): user status reactivity bug

--- libraries/Iridium/profile/ProfileManager.ts
+++ libraries/Iridium/profile/ProfileManager.ts
@@ -77,10 +77,9 @@ export default class IridiumProfile extends Emitter {
 
   async updateUser(details: Partial<User>) {
     logger.info('iridium/profile', 'updating user', { details })
-    this.state = {
-      ...this.state,
-      ...details,
-    } as User
+    for await (const [key, value] of Object.entries(details)) {
+      this.state[key] = value
+    }
     await this.set('/', this.state)
     if (!this.state || !iridium.id) return
     // tell our peers via user announce
