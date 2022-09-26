99cf3a005cb166e1860a7752ff1fdc377426b6fc
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-09-26 22:45:08 +0100

    Update sign-up.js

--- cypress/e2e/sign-up.js
+++ cypress/e2e/sign-up.js
@@ -4,9 +4,9 @@ const randomPIN = faker.internet.password(7, false, /[A-Z]/, 'test') // generate
 describe('Sign up', () => {
   it('Sign up', { retries: 2 }, () => {
     cy.visit('/')
-    cy.get('[data-cy="input-group"]').should('be.visible').click().type('12345')
-    cy.get('[data-cy="submit-input"]').click()
-    cy.get('[data-cy="create-account-button"]').click()
+    cy.get('[data-cy=input-group]').should('be.visible').click().type('12345')
+    cy.get('[data-cy=submit-input]').click()
+    cy.get('[data-cy=create-account-button]').click()
     cy.contains('I Saved It').click()
     cy.contains('Neil Spaceman...').click().type('username')
     cy.contains('Sign in').click()
