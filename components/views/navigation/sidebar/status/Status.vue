b3a12363daa3b5edebbec1e3d5f94d8e3c3393fb
Joe McGrath <33670767+josephmcg@users.noreply.github.com>
2022-09-30 02:49:59 +0900

    fix(sidebarStatus): fix typography and accessiblity (#5082)

--- components/views/navigation/sidebar/status/Status.vue
+++ components/views/navigation/sidebar/status/Status.vue
@@ -3,12 +3,11 @@
 <script lang="ts">
 import Vue from 'vue'
 import iridium from '~/libraries/Iridium/IridiumManager'
-import { User } from '~/libraries/Iridium/users/types'
 
 export default Vue.extend({
-  data() {
-    return { status: 'online', profile: iridium.profile.state }
-  },
+  data: () => ({
+    profile: iridium.profile.state,
+  }),
   methods: {
     copyId() {
       if (!iridium.connector) return
