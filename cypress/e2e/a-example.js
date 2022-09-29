3efd7868629305c5857898b6443fc5dae280055e
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-09-29 01:39:52 +0100

    test

---
+++ cypress/e2e/a-example.js
@@ -0,0 +1,5 @@
+describe('Create two reusable accounts and validate friend request/accept flow', () => {
+  it('Create First Account and grab friend ID', { retries: 2 }, () => {
+    cy.visit('google.pt')
+  })
+})
