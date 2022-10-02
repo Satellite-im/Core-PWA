d8ac148c4cb4fd0de88cd02d28419a44a6164fd4
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2022-10-02 16:08:39 +0100

    Delete a-example.js

--- cypress/e2e/a-example.js
+++
@@ -1,5 +0,0 @@
-describe('Create two reusable accounts and validate friend request/accept flow', () => {
-  it('Create First Account and grab friend ID', { retries: 2 }, () => {
-    cy.visit('google.pt')
-  })
-})
