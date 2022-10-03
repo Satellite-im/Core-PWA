4552a07a0c6ca4587473d1bd1d0d51f26f634041
Andre Christoga Pramaditya <caxvis@gmail.com>
2022-10-03 22:47:01 +0700

    feat(jest): 67% coverage for DirectWebRTCBus (#5125)

--- libraries/WebRTC/bus/DirectWebRTCBus.test.ts
+++ libraries/WebRTC/bus/DirectWebRTCBus.test.ts
@@ -8,6 +8,7 @@ describe('Test DirectWebrtc', () => {
     const callConstructor = new DirectWebrtc('example_id', {
       on: jest.fn().mockReturnValueOnce(true), // Mocked to account for WebRTC error
     })
+
     try {
       //   const spy = jest.spyOn(Emitter, 'emit')
       callConstructor.onMessage({
@@ -24,6 +25,36 @@ describe('Test DirectWebrtc', () => {
       // expect(spy).toHaveBeenCalled()
     } catch (error) {
       // Related to #5092
+      expect(error).toBeInstanceOf(Error)
+      expect(error).toHaveProperty(
+        'message',
+        `No WebRTC support: Not a supported browser`,
+      )
+    }
+  })
+
+  test('isConnected return false', () => {
+    const mockedConnectedFunction = jest.fn().mockReturnValueOnce(false)
+    const callConstructor = new DirectWebrtc('example_id', {
+      on: jest.fn().mockReturnValueOnce(true),
+      connected: mockedConnectedFunction,
+    })
+
+    const result = callConstructor.isConnected()
+    expect(result).toBe(mockedConnectedFunction)
+  })
+
+  test('isConnected return true', () => {
+    const callConstructor = new DirectWebrtc('example_id', {
+      on: jest.fn().mockReturnValueOnce(true),
+      connected: jest.fn().mockReturnValueOnce(true),
+    })
+
+    try {
+      const result = callConstructor.isConnected()
+      expect(result).toBeTruthy()
+    } catch (error) {
+      // If error happens
       expect(error).toBeInstanceOf(Error)
       expect(error).toHaveProperty(
         'message',
