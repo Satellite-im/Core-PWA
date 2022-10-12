17059316088e4f7710d8079983672fc7dfc97ce7
molimauro <50131258+molimauro@users.noreply.github.com>
2022-10-12 20:16:11 +0200

    chore(scroll): better loader ux and logic (#5178)

--- components/ui/Chat/InfiniteScroll/InfiniteScroll.vue
+++ components/ui/Chat/InfiniteScroll/InfiniteScroll.vue
@@ -1,6 +1,13 @@
-<template src="./InfiniteScroll.html"></template>
+<template>
+  <div ref="root" class="infinite-scroll" />
+</template>
 
 <script lang="ts">
+import { throttle } from 'lodash'
+
+// the higher the sooner we trigger the 'loadMore' emit
+const LOADING_TOLERANCE = 0.25
+
 export default {
   name: 'InfiniteScroll',
   props: {
@@ -17,6 +24,15 @@ export default {
     parentElement(): HTMLElement {
       return (this.$refs.root as HTMLElement).parentElement as HTMLElement
     },
+    messageLoaderHeight(): number {
+      return (
+        (
+          document.querySelector(
+            `[data-id="message-scroll-loader"]`,
+          ) as HTMLElement
+        )?.offsetHeight ?? 0
+      )
+    },
   },
   mounted() {
     this.parentElement.addEventListener('wheel', this.scrollHandler)
@@ -27,14 +43,21 @@ export default {
     this.parentElement.removeEventListener('touchmove', this.scrollHandler)
   },
   methods: {
-    scrollHandler() {
-      if (this.isLoading || this.noMore || this.parentElement.scrollTop > 500) {
+    scrollHandler: throttle(function (this: any) {
+      const contentPart =
+        (this.parentElement.scrollHeight - this.messageLoaderHeight) *
+        LOADING_TOLERANCE
+
+      if (
+        this.isLoading ||
+        this.noMore ||
+        this.parentElement.scrollTop > this.messageLoaderHeight + contentPart
+      ) {
         return
       }
+
       this.$emit('loadMore')
-    },
+    }, 100),
   },
 }
 </script>
-
-<style scoped lang="less" src="./InfiniteScroll.less"></style>
