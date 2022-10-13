d3db52364311e9565c8658d865c4374e068e2aa4
Joe McGrath <josephmcg01@gmail.com>
2022-10-13 15:09:36 +0900

    fix: improve variable name

--- components/compositions/events.ts
+++ components/compositions/events.ts
@@ -1,9 +1,9 @@
 import { onMounted, onUnmounted } from 'vue'
 
-export function handleEsc(callback: Function) {
+export function handleEsc(func: Function) {
   function handleKeydown(event: KeyboardEvent) {
     if (event.key === 'Escape') {
-      callback()
+      func()
     }
   }
   onMounted(() => document.addEventListener('keydown', handleKeydown))
