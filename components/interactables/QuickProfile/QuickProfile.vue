43e5e826c840ed57704250e9b446d01b9fc7060c
Joe McGrath <josephmcg01@gmail.com>
2022-10-17 15:52:09 +0900

    refactor(quickProfile): refactor to comp api

--- components/interactables/QuickProfile/QuickProfile.vue
+++ components/interactables/QuickProfile/QuickProfile.vue
@@ -1,19 +1,16 @@
 <template src="./QuickProfile.html"></template>
 <script lang="ts">
-import Vue from 'vue'
-import { mapState } from 'vuex'
+import Vue, { computed, ComputedRef, onMounted, Ref, ref } from 'vue'
 import {
   ArrowRightIcon,
   ChevronRightIcon,
   EditIcon,
 } from 'satellite-lucide-icons'
-import { createFocusTrap, FocusTrap, Options } from 'focus-trap'
 import { User, UserStatus } from '~/libraries/Iridium/users/types'
-import { SettingsRoutes } from '~/store/ui/types'
+import { SettingsRoutes, UIState } from '~/store/ui/types'
 import iridium from '~/libraries/Iridium/IridiumManager'
-import { RootState } from '~/types/store/store'
 import { Conversation } from '~/libraries/Iridium/chat/types'
-import { handleEsc } from '~/components/compositions/events'
+import { handleEsc, handleFocusTrap } from '~/components/compositions/events'
 
 export default Vue.extend({
   components: {
@@ -21,122 +18,117 @@ export default Vue.extend({
     ChevronRightIcon,
     EditIcon,
   },
-  setup(props, { emit }) {
+  setup() {
     // @ts-ignore
-    const { $store } = useNuxtApp()
+    const { $store, $device, $router } = useNuxtApp()
+    const quickProfile: UIState['quickProfile'] = $store.state.ui.quickProfile
+
+    const quick: Ref<HTMLElement | null> = ref(null)
+    const text: Ref<string> = ref('')
+    const isStatusMenuVisible: Ref<boolean> = ref(false)
+
+    const user: ComputedRef<User | undefined> = computed(() => {
+      return quickProfile?.user
+    })
+
+    const isMe: ComputedRef<boolean> = computed(() => {
+      return iridium.id === user.value?.did
+    })
+
+    const status: ComputedRef<UserStatus> = computed(() => {
+      return 'online'
+    })
+
+    const conversationId: ComputedRef<Conversation['id'] | undefined> =
+      computed(() => {
+        if (!user.value) {
+          return
+        }
+        return iridium.chat.directConversationIdFromDid(user.value.did)
+      })
+
+    handleEsc(close)
+
+    onMounted(() => {
+      // non friend quick profiles do not have buttons. will add profile link later and remove check.value
+      if (conversationId.value || isMe.value) {
+        handleFocusTrap(quick)
+      }
+
+      if (!quickProfile || $device.isMobile || !quick.value) {
+        return
+      }
+
+      quick.value.style.top = `${quickProfile.position.y}px`
+      quick.value.style.left = `${quickProfile.position.x}px`
+
+      // handle overflow
+      const widthOverflow =
+        quickProfile.position.x + quick.value.clientWidth - window.innerWidth
+      const heightOverflow =
+        quickProfile.position.y + quick.value.clientHeight - window.innerHeight
+
+      if (widthOverflow > -8) {
+        quick.value.style.left = `${
+          quickProfile.position.x - widthOverflow - 16
+        }px`
+      }
+      if (heightOverflow > -8) {
+        quick.value.style.top = `${
+          quickProfile.position.y - heightOverflow - 16
+        }px`
+      }
+    })
+
     function close() {
       $store.commit('ui/setQuickProfile', undefined)
     }
 
-    handleEsc(close)
-
-    return {
-      close,
-    }
-  },
-  data: () => ({
-    text: '',
-    isStatusMenuVisible: false,
-    users: iridium.users,
-    trap: null as FocusTrap | null,
-  }),
-  computed: {
-    ...mapState({
-      quickProfile: (state) => (state as RootState).ui.quickProfile,
-    }),
-    user(): User | undefined {
-      return this.quickProfile?.user
-    },
-    isMe(): boolean {
-      return iridium.id === this.user?.did
-    },
-    conversationId(): Conversation['id'] | undefined {
-      if (!this.user) {
+    function sendMessage() {
+      if (!user.value || !conversationId.value) {
         return
       }
-      return iridium.chat.directConversationIdFromDid(this.user.did)
-    },
-    status(): UserStatus {
-      if (!this.user?.did) {
-        return 'offline'
-      }
-      return this.isMe
-        ? 'online'
-        : this.users?.ephemeral.status[this.user.did] ?? 'offline'
-    },
-  },
-  beforeDestroy() {
-    this.trap?.deactivate()
-  },
-  mounted() {
-    const quick = this.$refs.quick as HTMLElement
-
-    // non friend quick profiles do not have buttons. will add profile link later and remove this check
-    if (this.conversationId || this.isMe) {
-      const options: Options = {
-        allowOutsideClick: true,
-        escapeDeactivates: false,
-      }
-      this.trap = createFocusTrap(quick, options)
-      this.trap.activate()
-    }
-
-    if (this.$device.isDesktop && this.quickProfile) {
-      quick.style.top = `${this.quickProfile.position.y}px`
-      quick.style.left = `${this.quickProfile.position.x}px`
-    }
-    this.handleOverflow()
-  },
-  methods: {
-    handleOverflow() {
-      if (!this.quickProfile || this.$device.isMobile) {
-        return
-      }
-      const quick = this.$refs.quick as HTMLElement
-      const widthOverflow =
-        this.quickProfile.position.x + quick.clientWidth - window.innerWidth
-      const heightOverflow =
-        this.quickProfile.position.y + quick.clientHeight - window.innerHeight
-      if (widthOverflow > -8) {
-        quick.style.left = `${
-          this.quickProfile.position.x - widthOverflow - 16
-        }px`
-      }
-      if (heightOverflow > -8) {
-        quick.style.top = `${
-          this.quickProfile.position.y - heightOverflow - 16
-        }px`
-      }
-    },
-    sendMessage() {
-      if (!this.user || !this.conversationId) {
-        return
-      }
-      this.$router.push(
-        `${this.$device.isMobile ? '/mobile' : ''}/chat/${this.conversationId}`,
+      $router.value.push(
+        `${$device.isMobile ? '/mobile' : ''}/chat/${conversationId.value}`,
       )
       iridium.chat.sendMessage({
         at: Date.now(),
         type: 'text',
-        body: this.text,
-        conversationId: this.conversationId,
+        body: text.value,
+        conversationId: conversationId.value,
         attachments: [],
         payload: {},
       })
-      this.close()
-    },
-    openProfile() {},
-    openSettings() {
-      this.$store.commit('ui/setSettingsRoute', SettingsRoutes.PROFILE)
-      this.close()
+      close()
+    }
 
-      if (this.$device.isMobile) {
-        this.$router.push('/mobile/settings')
+    function openSettings() {
+      $store.value.commit('ui/setSettingsRoute', SettingsRoutes.PROFILE)
+      close()
+
+      if ($device.isMobile) {
+        $router.value.push('/mobile/settings')
       }
-    },
-    setMenuVis(val: boolean) {
-      this.isStatusMenuVisible = val
-    },
+    }
+
+    function setMenuVis(val: boolean) {
+      isStatusMenuVisible.value = val
+    }
+
+    return {
+      quick,
+      text,
+      isStatusMenuVisible,
+      quickProfile,
+      user,
+      isMe,
+      status,
+      conversationId,
+      close,
+      sendMessage,
+      openSettings,
+      setMenuVis,
+    }
   },
 })
 </script>
