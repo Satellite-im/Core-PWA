3510a9296eb9abf2667f92ee6af506a11fba03bb
Jason Woodland <me@jasonwoodland.com>
2022-10-18 15:07:12 +1100

    feat(android): overlay status and notification bars

--- android/app/src/main/java/im/satellite/core/MainActivity.java
+++ android/app/src/main/java/im/satellite/core/MainActivity.java
@@ -1,5 +1,19 @@
 package im.satellite.core;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Window window = getWindow();
+    window.setStatusBarColor(Color.TRANSPARENT);
+    window.setNavigationBarColor(Color.TRANSPARENT);
+    window.getDecorView().setSystemUiVisibility(window.getDecorView().getSystemUiVisibility() | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+  }
+}
