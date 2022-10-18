bbbbee0a159cd41af66a089772ce1cfe0b07d0c2
Joe McGrath <josephmcg01@gmail.com>
2022-10-18 15:13:54 +0900

    Revert "refactor(mobile): upgrade to composition api"
    This reverts commit 94618792f03c0dcb817ed39fccaf83b617829968.

--- components/views/navigation/mobile/toolbar/Toolbar.vue
+++ components/views/navigation/mobile/toolbar/Toolbar.vue
@@ -1,64 +1,63 @@
-<template>
-  <div class="toolbar">
-    <button v-if="conversationId" @click="$emit('slidePrev')">
-      <menu-icon class="font-color-flair" size="1.5x" alt="previous" />
-    </button>
-    <TypographyText as="h3">
-      {{ conversation?.name }}
-    </TypographyText>
-    <button
-      class="phone"
-      :class="{ disabled: !enableRTC || webrtc.activeCall || isGroup }"
-      :disabled="!enableRTC || Boolean(webrtc.activeCall)"
-      data-cy="toolbar-enable-audio"
-      @click="handleCall"
-    >
-      <phone-call-icon size="1.4x" alt="call" />
-    </button>
-    <!-- <button
-    title="video call"
-    :disabled="!enableRTC || webrtc.activeCall"
-    data-cy="toolbar-enable-audio"
-    @click="handleCall"
-  >
-    <video-icon size="1.4x" />
-  </button> -->
-    <template v-if="otherParticipants.length">
-      <UiUserState v-if="!isGroup" :user="otherParticipants[0]" />
-      <UiGroupIcon v-else :members="conversation?.participants" />
-    </template>
-  </div>
-</template>
+<template src="./Toolbar.html"></template>
 
-<script setup lang="ts">
-import { computed, ComputedRef, reactive } from 'vue'
-import { PhoneCallIcon, MenuIcon } from 'satellite-lucide-icons'
+<script lang="ts">
+import Vue, { computed, ComputedRef } from 'vue'
+import {
+  PhoneCallIcon,
+  UserPlusIcon,
+  MenuIcon,
+  VideoIcon,
+} from 'satellite-lucide-icons'
+
 import iridium from '~/libraries/Iridium/IridiumManager'
 import { conversationHooks } from '~/components/compositions/conversations'
 import { webrtcHooks } from '~/components/compositions/webrtc'
 
-// @ts-ignore
-const { $route } = useNuxtApp()
-const conversationId: ComputedRef<string | undefined> = computed(() => {
-  return $route.params.id
+export default Vue.extend({
+  components: {
+    PhoneCallIcon,
+    UserPlusIcon,
+    MenuIcon,
+    VideoIcon,
+  },
+  setup() {
+    // @ts-ignore
+    const $nuxt = useNuxtApp()
+    const conversationId: ComputedRef<string | undefined> = computed(() => {
+      return $nuxt.$route.params.id
+    })
+
+    const { conversation, isGroup, otherDids, otherParticipants } =
+      conversationHooks(conversationId.value)
+    const { enableRTC, call } = webrtcHooks(conversationId.value)
+
+    async function handleCall() {
+      if (isGroup.value || !enableRTC.value || !conversationId.value) {
+        return
+      }
+      await call({
+        recipient: otherDids.value[0],
+        conversationId: conversationId.value,
+        kinds: ['audio'],
+      })
+    }
+
+    return {
+      conversation,
+      isGroup,
+      enableRTC,
+      otherParticipants,
+      handleCall,
+    }
+  },
+
+  data() {
+    return {
+      webrtc: iridium.webRTC.state,
+      chat: iridium.chat.state,
+    }
+  },
 })
-
-const webrtc = reactive(iridium.webRTC.state)
-
-const { conversation, isGroup, otherDids, otherParticipants } =
-  conversationHooks(conversationId.value)
-const { enableRTC, call } = webrtcHooks(conversationId.value)
-
-async function handleCall() {
-  if (isGroup.value || !enableRTC.value || !conversationId.value) {
-    return
-  }
-  await call({
-    recipient: otherDids.value[0],
-    conversationId: conversationId.value,
-    kinds: ['audio'],
-  })
-}
 </script>
 
 <style scoped lang="less" src="./Toolbar.less"></style>
