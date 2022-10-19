f7f311d5277f25832283ec26439f5bf474ed526b
Joe McGrath <33670767+josephmcg@users.noreply.github.com>
2022-10-19 11:01:45 +0900

    refactor(button): upgrade to composition api (#5305)

--- components/interactables/Button/Button.vue
+++ components/interactables/Button/Button.vue
@@ -1,86 +1,85 @@
-<template src="./Button.html"></template>
+<template>
+  <button
+    ref="button"
+    class="button"
+    :class="`size-${size} color-${color}`"
+    :disabled="disabled || loading"
+    :type="type"
+    @click="handleClick"
+    @mousemove.stop="handleMouseMove"
+    @mouseenter="handleMouseEnter"
+    @mouseleave="handleMouseLeave"
+  >
+    <UiLoadersSpinner v-if="loading" class="spinner" spinning />
+    <div v-else class="content">
+      <slot></slot>
+      <span v-if="text.length">{{ text }}</span>
+    </div>
+    <div class="cursor-glow" :style="cursorGlowStyle"></div>
+  </button>
+</template>
 
-<script lang="ts">
-import Vue, { PropType } from 'vue'
+<script setup lang="ts">
+import { computed, ComputedRef, Ref, ref } from 'vue'
+import { ButtonColor, ButtonSize, ButtonType } from './types.d'
 
-import { ButtonType, ButtonSize } from './types.d'
+type CursorGlow = {
+  backgroundPosition: string
+  opacity: number
+}
 
-export default Vue.extend({
-  props: {
-    size: {
-      type: String as PropType<ButtonSize>,
-      default: 'md',
-    },
-    color: {
-      type: String as PropType<ButtonType>,
-      default: 'primary',
-    },
-    text: {
-      type: String,
-      default: '',
-    },
-    outlined: {
-      type: Boolean,
-      default: false,
-    },
-    loading: {
-      type: Boolean,
-      default: false,
-    },
-    type: {
-      type: String,
-      default: 'button',
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-    // aria label for accessibility
-    label: {
-      type: String,
-      default: '',
-    },
-  },
-  data() {
-    return {
-      cursorX: 0,
-      cursorY: 0,
-      showCursorGlow: false,
-    }
-  },
-  computed: {
-    cursorGlowStyle(): {
-      backgroundPosition: string
-      opacity: number
-    } {
-      return {
-        backgroundPosition: `left ${this.cursorX - 150}px top ${
-          this.cursorY - 150
-        }px`,
-        opacity: !this.disabled && this.showCursorGlow ? 1 : 0,
-      }
-    },
-  },
-  methods: {
-    handleClick(event: MouseEvent) {
-      this.$emit('click', event)
-    },
-    handleMouseEnter() {
-      this.showCursorGlow = true
-    },
-    handleMouseLeave() {
-      this.showCursorGlow = false
-    },
-    handleMouseMove(e: MouseEvent) {
-      const button = this.$refs.button as HTMLElement
-      if (!button) {
-        return
-      }
-      const bounds = button.getBoundingClientRect()
-      this.cursorX = e.clientX - bounds.left
-      this.cursorY = e.clientY - bounds.top
-    },
-  },
+interface Props {
+  size?: ButtonSize
+  color?: ButtonColor
+  text?: string
+  loading?: boolean
+  type?: ButtonType
+  disabled?: boolean
+}
+const props = withDefaults(defineProps<Props>(), {
+  size: 'md',
+  color: 'primary',
+  text: '',
+  loading: false,
+  type: 'button',
+  disabled: false,
 })
+
+interface Emits {
+  (e: 'click', event: MouseEvent): void
+}
+const emit = defineEmits<Emits>()
+
+const cursorX: Ref<number> = ref(0)
+const cursorY: Ref<number> = ref(0)
+const showCursorGlow: Ref<boolean> = ref(false)
+const button: Ref<HTMLButtonElement | null> = ref(null)
+
+const cursorGlowStyle: ComputedRef<CursorGlow> = computed(() => {
+  return {
+    backgroundPosition: `left ${cursorX.value - 150}px top ${
+      cursorY.value - 150
+    }px`,
+    opacity: !props.disabled && showCursorGlow.value ? 1 : 0,
+  }
+})
+
+function handleClick(event: MouseEvent) {
+  emit('click', event)
+}
+function handleMouseEnter() {
+  showCursorGlow.value = true
+}
+function handleMouseLeave() {
+  showCursorGlow.value = false
+}
+function handleMouseMove(event: MouseEvent) {
+  if (!button.value) {
+    return
+  }
+  const bounds = button.value.getBoundingClientRect()
+  cursorX.value = event.clientX - bounds.left
+  cursorY.value = event.clientY - bounds.top
+}
 </script>
 <style scoped lang="less" src="./Button.less"></style>
