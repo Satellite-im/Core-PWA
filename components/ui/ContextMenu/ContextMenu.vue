422cdf76ab58a9266451970dc859516f930d405d
Nathan Power <Nathan.c.power@icloud.com>
2022-10-19 18:35:18 +1100

    fix(context-menu): fix action sheet styles

--- components/ui/ContextMenu/ContextMenu.vue
+++ components/ui/ContextMenu/ContextMenu.vue
@@ -1,62 +1,23 @@
 <template>
   <!-- Desktop Context Menu -->
-  <div v-if="!$device.isMobile" @contextmenu="contextMenu" @click="handleClick">
+  <div
+    v-if="!$device.isMobile"
+    @contextmenu="contextMenu"
+    @click.stop.prevent="handleClick"
+  >
     <slot />
   </div>
 
   <!-- Mobile Context Menu -->
-  <div v-else v-contextmenu="showMenu" @click="handleClick">
+  <div v-else v-contextmenu="showMenu">
     <slot />
-    <div
-      v-if="isVisible"
-      ref="contextMenu"
-      v-click-outside="hideMenu"
-      class="context-menu-container"
-      data-cy="context-menu"
-    >
-      <!-- Quick Reactions -->
-      <div class="actions-group">
-        <template v-for="item in items">
-          <div
-            v-if="item.text === 'quickReaction' && mostUsedEmojis.length"
-            :key="String(item.text) + '-action'"
-            class="quick-reaction-container"
-          >
-            <div
-              v-for="reaction of mostUsedEmojis"
-              :key="reaction.content"
-              class="reaction"
-              data-cy="quick-reaction"
-              @click="
-                (e) =>
-                  handleAction(e, () => {
-                    item.func(reaction)
-                  })
-              "
-            >
-              {{ reaction.content }}
-            </div>
-          </div>
-        </template>
-      </div>
-
-      <!-- Item Buttons -->
-      <div class="actions-group">
-        <template v-for="item in items">
-          <button
-            v-if="item.text !== 'quickReaction'"
-            :key="String(item.text) + '-action'"
-            class="action-button"
-            :class="item?.type"
-            @click="(e) => handleAction(e, item.func)"
-          >
-            <TypographyText :color="item?.type">
-              {{ item.text }}
-            </TypographyText>
-          </button>
-        </template>
-      </div>
-    </div>
+    <ActionSheet
+      :items="items"
+      :is-visible="isVisible"
+      @click.stop.prevent="handleClick"
+      @action="handleAction"
+      @hide="hideMenu"
+    />
   </div>
 </template>
 
@@ -65,8 +26,10 @@ import Vue, { PropType } from 'vue'
 import { mapGetters } from 'vuex'
 import ContextMenu from '~/components/mixins/UI/ContextMenu'
 import { ContextMenuItem, EmojiUsage } from '~/store/ui/types'
+import ActionSheet from '~/components/ui/ActionSheet/ActionSheet.vue'
 
 export default Vue.extend({
+  components: { ActionSheet },
   mixins: [ContextMenu],
   props: {
     items: {
@@ -92,17 +55,16 @@ export default Vue.extend({
     showMenu(): void {
       this.isVisible = true
     },
-    hideMenu(e: Event): void {
-      e.stopPropagation()
+    hideMenu(): void {
       this.isVisible = false
     },
-    handleAction(e: Event, func: Function): void {
+    handleAction(func: Function): void {
       func()
-      this.hideMenu(e)
+      this.hideMenu()
     },
-    handleClick(e: Event): void {
+    handleClick(): void {
       if (this.isVisible) {
-        this.hideMenu(e)
+        this.hideMenu()
         return
       }
       this.$emit('click')
@@ -111,67 +73,4 @@ export default Vue.extend({
 })
 </script>
 
-<style lang="less" scoped>
-.context-menu-container {
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  flex-direction: column;
-  padding: 0 @normal-spacing @normal-spacing;
-  &:extend(.fourth-layer);
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  &:extend(.blur-less);
-  gap: @normal-spacing;
-  left: 0;
-  top: 0;
-
-  .actions-group {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    max-width: 600px;
-    width: 100%;
-    margin: 0 @normal-spacing;
-
-    &:extend(.background-semitransparent-dark);
-    &:extend(.round-corners);
-
-    .action-button {
-      height: 56px;
-      justify-content: center;
-      align-items: center;
-      width: 100%;
-
-      &.disabled {
-        opacity: 0.5;
-      }
-
-      &:not(:last-child) {
-        border-bottom: 0.5px solid @foreground;
-      }
-    }
-
-    .quick-reaction-container {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      flex-wrap: wrap;
-      width: 100%;
-      height: 64px;
-      overflow: hidden;
-
-      .reaction {
-        display: flex;
-        flex: 1 0 20%;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-        font-size: 24px;
-      }
-    }
-  }
-}
-</style>
+<style lang="less" scoped></style>
