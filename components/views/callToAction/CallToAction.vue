dcf3a7e198b8bad9d36e77302f2815b6f6a45cfc
Joe McGrath <33670767+josephmcg@users.noreply.github.com>
2022-10-19 23:28:18 +0900

    refactor(cta): add button props for anchor option, refactor cta modal (#5317)

--- components/views/callToAction/CallToAction.vue
+++ components/views/callToAction/CallToAction.vue
@@ -1,29 +1,36 @@
-<template src="./CallToAction.html"></template>
+<template>
+  <div class="cta" data-cy="modal-cta">
+    <satellite-icon class="sat-icon" size="3.5x" />
+    <TypographyText as="h1" class="heading">
+      {{ $t('modal.call_to_action.title') }}
+    </TypographyText>
+    <TypographyText> {{ $t('modal.call_to_action.subtitle') }} </TypographyText>
+    <TypographyText>
+      <ul>
+        <li>{{ $t('modal.call_to_action.item1') }}</li>
+        <li>{{ $t('modal.call_to_action.item2') }}</li>
+        <li>{{ $t('modal.call_to_action.item3') }}</li>
+        <li>{{ $t('modal.call_to_action.item4') }}</li>
+        <li>{{ $t('modal.call_to_action.item5') }}</li>
+      </ul>
+    </TypographyText>
+    <hr />
+    <TypographyText color="dark">
+      {{ $t('modal.call_to_action.description') }}
+    </TypographyText>
+    <InteractablesButton
+      class="button"
+      :text="$t('modal.call_to_action.btn')"
+      size="lg"
+      link="https://twitter.com/satellite_im"
+    >
+      <radio-icon size="1.5x" />
+    </InteractablesButton>
+  </div>
+</template>
 
-<script lang="ts">
-import Vue from 'vue'
-
+<script setup lang="ts">
 import { RadioIcon, SatelliteIcon } from 'satellite-lucide-icons'
-
-export default Vue.extend({
-  components: {
-    RadioIcon,
-    SatelliteIcon,
-  },
-
-  props: {
-    closeModal: {
-      type: Function,
-      default: () => {},
-    },
-  },
-
-  methods: {
-    showTwitter() {
-      window.open('https://twitter.com/satellite_im', '_blank')
-    },
-  },
-})
 </script>
 
 <style scoped lang="less" src="./CallToAction.less"></style>
