d6fa2dbf76b748c6b5b792ef792c42e1a1a0fe0e
Jason Woodland <me@jasonwoodland.com>
2022-10-20 17:11:25 +1100

    fix(safearea): dispatch keyboardheight event for ios/android

--- android/app/src/main/java/im/satellite/core/MainActivity.java
+++ android/app/src/main/java/im/satellite/core/MainActivity.java
@@ -2,18 +2,33 @@ package im.satellite.core;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.view.WindowInsetsAnimation;
 import android.view.WindowManager;
-
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
+  private WindowInsetsAnimation.Callback windowInsetsCallback;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
     Window window = getWindow();
+    float density = getResources().getDisplayMetrics().density;
+    View view = window.getDecorView();
+
     window.setStatusBarColor(Color.TRANSPARENT);
     window.setNavigationBarColor(Color.TRANSPARENT);
-    window.getDecorView().setSystemUiVisibility(window.getDecorView().getSystemUiVisibility() | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    view.setSystemUiVisibility(window.getDecorView().getSystemUiVisibility() | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+    view.setOnApplyWindowInsetsListener((v, insets) -> {
+      int keyboardHeight = insets.getSystemWindowInsetBottom();
+
+      getBridge().getWebView().evaluateJavascript("window.dispatchEvent(new CustomEvent('keyboardHeight', { detail: { height: " + (keyboardHeight / density) + " } }));", null);
+      return insets;
+    });
   }
+
 }
