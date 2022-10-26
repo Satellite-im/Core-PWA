f784ffbdc2f4a0c1f3d05f9960c221ec4565dcc5
Jason Woodland <me@jasonwoodland.com>
2022-10-26 12:42:35 +1100

    fix(ios): disable pinch to zoom

--- ios/App/App/ViewController.swift
+++ ios/App/App/ViewController.swift
@@ -8,23 +8,25 @@
 import UIKit
 import Capacitor
 
-class ViewController: CAPBridgeViewController {
-
+class ViewController: CAPBridgeViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-        self.webView?.evaluateJavaScript("window.app=document.querySelector('#app')")
+        self.webView?.scrollView.delegate = self
     }
     
     @objc func keyboardWillChange(notification: NSNotification) {
         guard let userInfo = notification.userInfo else { return }
-
+        
         let duration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
         let targetFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
-        var height = max(UIScreen.main.bounds.height - targetFrame.origin.y, self.view.safeAreaInsets.bottom)
+        let height = max(UIScreen.main.bounds.height - targetFrame.origin.y, self.view.safeAreaInsets.bottom)
         
         self.webView?.evaluateJavaScript("window.dispatchEvent(new CustomEvent('keyboardHeight', { detail: { height: \(height), duration: \(duration) } }));")
-
+    }
+    
+    func scrollViewWillBeginZooming(_ scrollView: UIScrollView, with view: UIView?) {
+      scrollView.pinchGestureRecognizer?.isEnabled = false
     }
 
 }
