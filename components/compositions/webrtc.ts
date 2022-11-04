213e8bfd9d4b643024a381ed35967145de862b90
Joe McGrath <josephmcg01@gmail.com>
2022-11-04 10:24:02 +0900

    refactor(webrtc): composable for active call timer

--- components/compositions/webrtc.ts
+++ components/compositions/webrtc.ts
@@ -1,10 +1,18 @@
-import { computed, ComputedRef, reactive } from 'vue'
+import {
+  computed,
+  ComputedRef,
+  reactive,
+  ref,
+  onMounted,
+  onUnmounted,
+} from 'vue'
 import { useNuxtApp } from '@nuxt/bridge/dist/runtime/app'
 import { Conversation } from '~/libraries/Iridium/chat/types'
 import iridium from '~/libraries/Iridium/IridiumManager'
 import { User } from '~/libraries/Iridium/users/types'
 import { TrackKind } from '~/libraries/WebRTC/types'
 import { conversationHooks } from '~/components/compositions/conversations'
+import { formatDuration } from '~~/utilities/duration'
 
 export function webrtcHooks(conversationId?: Conversation['id']) {
   const managers = reactive({
@@ -65,5 +73,25 @@ export function webrtcHooks(conversationId?: Conversation['id']) {
       }
     }
   }
-  return { enableRTC, isActiveCall, call }
+
+  function useDuration() {
+    const callDuration = ref<string>('')
+    let intervalId: NodeJS.Timer | undefined
+
+    const updateDuration = () => {
+      const durationOfCall = Date.now() - managers.webrtc.state.callStartedAt
+      callDuration.value = formatDuration(durationOfCall / 1000)
+    }
+
+    onMounted(() => {
+      updateDuration()
+      intervalId = setInterval(updateDuration, 1000)
+    })
+    onUnmounted(() => {
+      clearInterval(intervalId)
+    })
+    return callDuration
+  }
+
+  return { enableRTC, isActiveCall, call, useDuration }
 }
