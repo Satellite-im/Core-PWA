a6e5d074dcbb24dc3a4f72dc2f9919917406f73e
Joe McGrath <josephmcg01@gmail.com>
2022-11-04 14:05:14 +0900

    refactor(chat): upgrade new message divider to script setup

--- components/ui/CaretDivider/CaretDivider.vue
+++ components/ui/CaretDivider/CaretDivider.vue
@@ -1,19 +1,45 @@
-<template src="./CaretDivider.html" />
+<template>
+  <div class="caret_divider">
+    <TypographyText class="caret_text" size="xs">
+      {{ text }}
+      <div class="triangle"></div>
+    </TypographyText>
+  </div>
+</template>
 
-<script lang="ts">
-import Vue from 'vue'
+<script setup lang="ts">
+interface Props {
+  text: string
+}
 
-export default Vue.extend({
-  props: {
-    text: {
-      type: String,
-      default() {
-        return this.$t('internal.missing_text')
-      },
-    },
-  },
-})
+defineProps<Props>()
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="less" src="./CaretDivider.less"></style>
+<style scoped lang="less">
+.caret_divider {
+  border-bottom: 1px solid @red;
+  position: relative;
+  margin: 1.25rem 0;
+  pointer-events: none;
+  .caret_text {
+    background: @red;
+    border-radius: @corner-rounding-smaller;
+    position: absolute;
+    top: -9.5px;
+    padding: 0 0.5rem;
+    line-height: 20px;
+    .glow-danger();
+
+    .triangle {
+      position: absolute;
+      width: 20px;
+      height: 20px;
+      right: 1px;
+      top: 0px;
+      transform: translate(50%, 0%) rotate(45deg) scale(0.7);
+      border-radius: @corner-rounding-smaller;
+      background: linear-gradient(45deg, transparent 40%, @red 41%);
+    }
+  }
+}
+</style>
